9c48c690f4adb049b5eff83a7d42487613a8fdd5
Reece Hart <reecehart@gmail.com>
2002-11-26 01:14:54 +0000

    This commit was generated by cvs2svn to compensate for changes in r7, which included commits to RCS files with non-trunk default branches.

---
+++ misc/sql-scraps/pftype.sql
@@ -0,0 +1,7 @@
+create table pftype (
+	pftype_id		serial,
+	name			text
+	);
+
+create function pftype_id_lookup (text) returns integer as 
+	'select pftype_id from pftype where name = $1' language 'sql';
