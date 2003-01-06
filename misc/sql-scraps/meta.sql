fb5f94cd574e6f8e09aa7ff0e89ddf566cbd2f4e
Reece Hart <reecehart@gmail.com>
2003-01-06 23:07:24 +0000

    *** empty log message ***

--- misc/sql-scraps/meta.sql
+++
@@ -1,9 +0,0 @@
-\echo =======================================================================
-\echo $Id$
--- meta -- unison database meta information
-
-create table meta (
-	key		text		primary key not null default null,
-	value	text
-	);
-
