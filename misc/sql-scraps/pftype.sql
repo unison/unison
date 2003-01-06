666ff7df49d348285e0af0e8918d788cf3ed83da
rkh <rkh@localhost>
2003-01-06 23:07:24 +0000

    *** empty log message ***

--- misc/sql-scraps/pftype.sql
+++
@@ -1,7 +0,0 @@
-\echo =======================================================================
-\echo $Id: pftype.sql,v 1.3 2002/12/10 19:48:11 rkh Exp $
-
-create table pftype (
-	pftype_id		serial unique,
-	name			text
-	);
