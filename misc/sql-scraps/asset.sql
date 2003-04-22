0f43cf597d22991944959dcb0cbdb0b059815e0c
Reece Hart <reecehart@gmail.com>
2003-04-22 00:45:45 +0000

    *** empty log message ***

--- misc/sql-scraps/asset.sql
+++ misc/sql-scraps/asset.sql
@@ -1,4 +1,6 @@
-create or replace function set_append(text,text) returns text language plpgsql as '
+create or replace function public._asset(text,text)
+returns text
+language plpgsql as '
 BEGIN
 	if $1 is null or $1 = '''' then
 		return $2;
@@ -6,5 +8,7 @@ BEGIN
 		return $1 || '','' || $2;
 	end if;
 END;';
+comment on function public._asset(text,text) is 'see asset aggregate function';
 
-create aggregate asset (basetype=text, stype=text, sfunc=set_append );
+create aggregate public.asset (basetype=text, stype=text, sfunc=_asset);
+comment on aggregate asset(text) is 'aggregate to accumulate a comma-separated list of options';
