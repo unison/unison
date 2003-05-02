9da1a20e8c69edccbc97bd0bc5b0a6500b8deaea
rkh <rkh@localhost>
2003-05-02 06:08:04 +0000

    added best_alias, best_annotation, pseq_get_aliases methods

--- perl5/Unison/pseq.pm
+++ perl5/Unison/pseq.pm
@@ -1,7 +1,7 @@
 =head1 NAME
 
 Unison::pseq -- Unison pseq table utilities
-S<$Id: pm,v 1.2 2001/06/12 05:38:24 reece Exp $>
+S<$Id: pseq.pm,v 1.1 2003/04/28 20:52:00 rkh Exp $>
 
 =head1 SYNOPSIS
 
@@ -58,6 +58,87 @@ fetches a single protein sequence from the pseq table.
 =cut
   }
 
+
+sub best_alias
+  {
+  my $self = shift;
+  $self->is_open()
+	|| croak("Unison connection not established");
+  ($#_==0)
+	|| croak("exactly one porigin_id needed\n");
+  my $sth = $self->prepare_cached("select best_alias(?)");
+  $sth->execute(shift);
+  my $ba = $sth->fetchrow_array;
+  $sth->finish();
+  return( $ba );
+=pod
+
+=over
+
+=item B<::best_alias( C<pseq_id> )>
+
+return the `best_alias' as determined heuristically by Unison.
+Briefly, the best_alias is the one specified by the pseq.palias_id if
+not null, or the first preference-ordered list of aliases based on
+porigin.ann_pref ranking.  See also best_annotation.
+
+=back
+
+=cut
+  }
+
+
+sub best_annotation
+  {
+  my $self = shift;
+  $self->is_open()
+	|| croak("Unison connection not established");
+  ($#_==0)
+	|| croak("exactly one porigin_id needed\n");
+  my $sth = $self->prepare_cached("select best_annotation(?)");
+  $sth->execute(shift);
+  my $ba = $sth->fetchrow_array;
+  $sth->finish();
+  return( $ba );
+=pod
+
+=over
+
+=item B<::best_annotation( C<pseq_id> )>
+
+return the "best_annotation" as determined heuristically by Unison.
+Compare with the C<best_alias> method and see that for a definition of
+"best".
+
+=back
+
+=cut
+  }
+
+
+sub pseq_get_aliases
+  {
+  my $self = shift;
+  $self->is_open()
+	|| croak("Unison connection not established");
+  ($#_==0)
+	|| croak("exactly one porigin_id needed\n");
+  my $pseq_id = shift;
+  my $sql = "select origin||':'||alias from palias as a join porigin as o on a.porigin_id=o.porigin_id  where pseq_id=$pseq_id  order by o.ann_pref";
+  return( map {@$_} @{ $self->{'dbh'}->selectall_arrayref($sql) } );
+=pod
+
+=over
+
+=item B<::pseq_get_aliases( C<pseq_id> )>
+
+return a list of <origin>:<alias> annotations for a given pseq_id, ordered
+by porigin.ann_pref.
+
+=back
+
+=cut
+  }
 
 
 
