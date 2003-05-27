2cba6af1ff6b58ed9c8fc2a645d8df5eef19ec5c
rkh <rkh@localhost>
2003-05-27 22:28:06 +0000

    moved logos to csb-web/unison

--- misc/logo/index.html.pl
+++ misc/logo/index.html.pl
@@ -1,11 +1,20 @@
 #!/usr/bin/env perl
 
-print("<html><body>\n");
+print("<html><body>\n",
+	  "<hr>\n",
+	  imgs(undef,@ARGV),
+	  "<hr>\n",
+	  imgs('grey',@ARGV),
+	  "<hr>\n",
+	  imgs('yellow',@ARGV),
+	  "</body></html>\n");
 
-print('<span background="yellow">', "\n");
 
-print("hi there \n");
-
-print('</span>', "\n");
-
-print("</body></html>\n");
+sub imgs
+  {
+  my $c = shift;
+  $c = " bgcolor=\"$c\"" if defined $c;
+  return( "<table width=\"100%\"><tr><td$c>\n",
+		  (map { "<br>$_ <img src=\"$_\">\n" } @_),
+		  "</td></tr></table>\n");
+  }
