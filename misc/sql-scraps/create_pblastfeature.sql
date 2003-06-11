bc07eaaceb1f455a2b9ec7d5baf4754c2528ba8e
cavs <cavs@localhost>
2003-06-11 20:25:07 +0000

    dropped ins_pblastfeature(), using trigger pblastfeature_iu_trigger instead

--- misc/sql-scraps/create_pblastfeature.sql
+++ misc/sql-scraps/create_pblastfeature.sql
@@ -4,7 +4,7 @@
 -- PURPOSE: sql statements and PL/pgSQL commands for creating a
 --          blast feature table and associated procedures
 --
--- $Id: create_unison.pblastfeature.sql,v 1.2 2003/04/10 16:12:21 cavs Exp $
+-- $Id: create_pblastfeature.sql,v 1.3 2003/04/23 17:19:41 cavs Exp $
 --
 -- -----------------------------------------------------------------------------
 
@@ -16,17 +16,16 @@
 -- indices.
 DROP TABLE unison.pblastfeature;
 CREATE TABLE unison.pblastfeature (
-	t_pseq_id integer NOT NULL,
-	t_start integer NOT NULL,
-	t_stop integer NOT NULL,
-	e_value real NOT NULL,
-	p_value real NOT NULL,
-	hsp_length integer NOT NULL,
-	identities integer NOT NULL,
-	similarities integer NOT NULL,
-	pct_identity real NOT NULL,
-	pct_hsp_coverage real NOT NULL,
-	pct_coverage real NOT NULL
+  t_pseq_id integer NOT NULL,
+  t_start integer NOT NULL,
+  t_stop integer NOT NULL,
+  p_value double precision NOT NULL,
+  hsp_length integer NOT NULL,
+  identities integer NOT NULL,
+  similarities integer NOT NULL,
+  pct_identity real NOT NULL,
+  pct_hsp_coverage real NOT NULL,
+  pct_coverage real NOT NULL
 ) INHERITS (pfeature) WITHOUT OIDS;
 COMMENT ON TABLE unison.pblastfeature IS 'stores BLAST features and derived values';
 COMMENT ON COLUMN unison.pblastfeature.pseq_id IS 'query pseq_id';
@@ -35,7 +34,6 @@ COMMENT ON COLUMN unison.pblastfeature.stop IS 'ending position (1-based) on the
 COMMENT ON COLUMN unison.pblastfeature.t_pseq_id IS 'target pseq_id';
 COMMENT ON COLUMN unison.pblastfeature.t_start IS 'starting position (1-based) on the target sequence';
 COMMENT ON COLUMN unison.pblastfeature.t_stop IS 'ending position (1-based) on the target sequence';
-COMMENT ON COLUMN unison.pblastfeature.e_value IS 'HSP e-value';
 COMMENT ON COLUMN unison.pblastfeature.p_value IS 'HSP p-value';
 COMMENT ON COLUMN unison.pblastfeature.hsp_length IS 'length of HSP including gaps';
 COMMENT ON COLUMN unison.pblastfeature.identities IS 'number of identities';
@@ -63,75 +61,6 @@ GRANT INSERT,UPDATE ON TABLE unison.pblastfeature TO loader;
 
 -- -----------------------------------------------------------------------------
 --
--- ins_pblastfeature():
---   purpose: create a unison.pblastfeature record
---   arguments: porigin_id, alias, pseq_id, ref_pseq_id
---   returns: pfeature_id
---
-CREATE OR REPLACE FUNCTION ins_pblastfeature(integer, integer, integer, integer, integer, integer, real, real, integer, integer, integer)  RETURNS integer AS '
-DECLARE
-	v_q_pseq_id ALIAS FOR $1;
-	v_q_start ALIAS FOR $2;
-	v_q_stop ALIAS FOR $3;
-	v_t_pseq_id ALIAS FOR $4;
-	v_t_start ALIAS FOR $5;
-	v_t_stop ALIAS FOR $6;
-	v_e_value ALIAS FOR $7;
-	v_p_value ALIAS FOR $8;
-	v_hsp_length ALIAS FOR $9;
-	v_identities ALIAS FOR $10;
-	v_similarities ALIAS FOR $11;
-
-	v_pct_identity real;
-	v_pct_hsp_coverage real;
-	v_pct_coverage real;
-	v_pfeature_id integer;
-	v_pftype_id integer;
-	t_seq_len integer;
-	q_seq_len integer;
-	pftype_name varchar;
-BEGIN
-	pftype_name := ''BLAST'';
-	select into v_pfeature_id nextval(''pfeature_pfeature_id_seq'');
-	select into v_pftype_id pftype_id from pftype where name=pftype_name;
-
-	-- error handling
-	IF v_pftype_id is null THEN
-		RAISE EXCEPTION ''Unable to retrieve pftype_id for name=%'',pftype_name;
-	ELSIF v_hsp_length = 0 THEN
-		RAISE EXCEPTION ''hsp_length parameter is 0 - not valid'';
-	END IF;
-
-	-- get sequence lengths for derived values;
-	select into q_seq_len len from pseq where pseq_id=v_q_pseq_id;
-	select into t_seq_len len from pseq where pseq_id=v_t_pseq_id;
-	IF    q_seq_len is null or q_seq_len = 0 THEN
-		RAISE EXCEPTION ''length for pseq_id=% is null or zero'',v_q_pseq_id;
-	ELSIF t_seq_len is null or t_seq_len = 0 THEN
-		RAISE EXCEPTION ''length for pseq_id=% is null or zero'',v_t_pseq_id;
-	END IF;
-
-	-- calculate derived values
-	v_pct_identity := ( v_identities::real / v_hsp_length::real ) * 100.0;
-	v_pct_hsp_coverage := ( v_hsp_length::real / q_seq_len::real ) * 100.0;
-	v_pct_coverage := ( t_seq_len::real / q_seq_len::real ) * 100.0;
-
-	-- do insert
-	insert into unison.pblastfeature ( pfeature_id, pseq_id, pftype_id, start, stop, t_pseq_id, 
-	    t_start, t_stop, e_value, p_value, hsp_length, identities, similarities, pct_identity, 
-	    pct_hsp_coverage, pct_coverage ) values ( v_pfeature_id, v_q_pseq_id, v_pftype_id, v_q_start,
-		v_q_stop, v_t_pseq_id, v_t_start, v_t_stop, v_e_value, v_p_value, v_hsp_length, 
-		v_identities, v_similarities, v_pct_identity, v_pct_hsp_coverage, v_pct_coverage );
-
-	return v_pfeature_id;
-END;
-' LANGUAGE 'plpgsql';
-COMMENT ON FUNCTION ins_pblastfeature(integer, integer, integer, integer, integer, integer, real, real, integer, integer, integer) IS 'insert a blast feature';
--- -----------------------------------------------------------------------------
-
-
--- -----------------------------------------------------------------------------
---
 -- get_identical_seqs():
 --   purpose: retrieve a set of pseq_ids of "identical" sequences
 --   arguments: pseq_id
@@ -139,24 +68,72 @@ COMMENT ON FUNCTION ins_pblastfeature(integer, integer, integer, integer, intege
 --
 CREATE OR REPLACE FUNCTION get_identical_seqs(integer) RETURNS refcursor AS '
 DECLARE
-	v_pseq_id ALIAS FOR $1;
+  v_pseq_id ALIAS FOR $1;
 
-	ref refcursor;
-	pct_identity_min real;
-	pct_hsp_coverage_min real;
-	hsp_length_min integer;
+  ref refcursor;
+  pct_identity_min real;
+  pct_hsp_coverage_min real;
+  hsp_length_min integer;
 BEGIN
-	-- define selection criteria
-	pct_identity_min := 90.0;
-	pct_hsp_coverage_min := 90.0;
-	hsp_length_min := 50;
+  -- define selection criteria
+  pct_identity_min := 90.0;
+  pct_hsp_coverage_min := 90.0;
+  hsp_length_min := 50;
 
-	OPEN ref FOR SELECT t_pseq_id from unison.pblastfeature WHERE pseq_id=v_pseq_id and 
-	pct_identity>=pct_identity_min and pct_hsp_coverage >= pct_hsp_coverage_min and 
-	hsp_length >= hsp_length_min;
+  OPEN ref FOR SELECT t_pseq_id from unison.pblastfeature WHERE pseq_id=v_pseq_id and 
+  pct_identity>=pct_identity_min and pct_hsp_coverage >= pct_hsp_coverage_min and 
+  hsp_length >= hsp_length_min;
 
-	return ref;
+  return ref;
 END;
 ' LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION get_identical_seqs(integer) IS 'retrieve a set of nearly identical sequences';
+-- -----------------------------------------------------------------------------
+
+
+-- -----------------------------------------------------------------------------
+--
+-- pblastfeature_iu_trigger():
+--   purpose: calculate pct_identity, pct_hsp_coverage, and pct_coverage for
+--            inserts to the pblastfeature table
+--
+CREATE OR REPLACE FUNCTION pblastfeature_iu_trigger () RETURNS "trigger"
+    AS '
+  declare
+    q_seq_len integer;
+    t_seq_len integer;
+		shorter_len integer;
+		longer_len integer;
+  begin
+
+    -- get sequence lengths for derived values
+    select into q_seq_len len from pseq where pseq_id=new.pseq_id;
+    select into t_seq_len len from pseq where pseq_id=new.t_pseq_id;
+    IF    q_seq_len is null or q_seq_len = 0 THEN
+      RAISE EXCEPTION ''length for pseq_id=% is null or zero'',new.pseq_id;
+    ELSIF t_seq_len is null or t_seq_len = 0 THEN
+      RAISE EXCEPTION ''length for pseq_id=% is null or zero'',new.t_pseq_id;
+    END IF;
+
+    -- calculate derived values
+		IF q_seq_len > t_seq_len THEN
+			shorter_len := t_seq_len;
+			longer_len := q_seq_len;
+		ELSE
+			shorter_len := q_seq_len;
+			longer_len := t_seq_len;
+		END IF;
+    new.pct_identity := ( new.identities::real / new.hsp_length::real ) * 100.0;
+    new.pct_hsp_coverage := ( new.hsp_length::real / q_seq_len::real ) * 100.0;
+    new.pct_coverage := ( shorter_len::real / longer_len::real ) * 100.0;
+
+    return new;
+  end; '
+    LANGUAGE plpgsql;
+COMMENT ON FUNCTION pblastfeature_iu_trigger() IS 'calculate derived values for inserting into pblastfeature';
+
+CREATE TRIGGER pblastfeature_iu_trigger
+	BEFORE INSERT OR UPDATE ON pblastfeature
+		FOR EACH ROW
+			EXECUTE PROCEDURE pblastfeature_iu_trigger ();
 -- -----------------------------------------------------------------------------
