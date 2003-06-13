18aac94df4459868ba3594aed3770fccb862d2d7
cavs <cavs@localhost>
2003-06-13 00:26:50 +0000

    added  _get_pmodel_id_from_pseq_id()

--- perl5/Unison/papseq.pm
+++ perl5/Unison/papseq.pm
@@ -1,19 +1,20 @@
 =head1 NAME
 
 Unison::papseq -- Unison papseq table utilities
-S<$Id: papseq.pm,v 1.1 2003/06/12 22:29:26 cavs Exp $>
+S<$Id: papseq.pm,v 1.2 2003/06/12 22:31:01 cavs Exp $>
 
 =head1 SYNOPSIS
 
-use Unison;
+ use Unison;
 
-my $u = new Unison;
-
-(etc.)
+ my $u = new Unison;
+ 
+ $u->load_blast_report( $ARGV[0] );
 
 =head1 DESCRIPTION
 
-B<> is a
+B<Unison::papseq> is a module with methods for loading
+BLAST reports into the papseq table in the Unison database.
 
 =head1 ROUTINES AND METHODS
 
@@ -47,11 +48,14 @@ sub load_blast_report {
   while(my $sbjct = $report->nextSbjct) {
     my $t_pseq_id = $u->_get_pseq_id_from_name($sbjct->name());
     if ( !defined $t_pseq_id ) {
-      warn("Nno pseq_id defined for this target sequence: " . $sbjct->name() . " - skipping\n" );
+      warn("No pseq_id defined for this target sequence: " . $sbjct->name() . " - skipping\n" );
       next;
     }
+		# get the pmodel_id for this sequence
+    my $pmodel_id = $u->_get_pmodel_id_from_pseq_id($t_pseq_id);
+
     while(my $hsp = $sbjct->nextHSP) {
-      $u->insert_hsp( $q_pseq_id, $t_pseq_id, $hsp );
+      $u->insert_hsp( $q_pseq_id, $pmodel_id, $hsp );
     }
   }
 }
@@ -83,11 +87,12 @@ sub insert_hsp {
   } elsif ( ! defined $hsp or ( ref $hsp ne 'Bio::Tools::BPlite::HSP' ) ) {
     throw Unison::BadUsage( 'Unison::insert_hsp() requires Bio::Tools::BPlite::HSP object' );
   }
+  throw Unison::RuntimeError( 'Unison connection not open' ) if ! $u->is_open();
   
   my $sql_start = "insert into papseq " .
     "(pseq_id, start, stop, pmodel_id, mstart, mstop, len, ident, sim, " .
     "gaps,score,eval,pct_ident)";
-  my $sql = $sql_start . "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+  my $sql = $sql_start . "values (?,?,?,?,?,?,?,?,?,?,?,?,?)";
   my $sth = $u->prepare_cached($sql);
   my @values = ( $pseq_id, $hsp->query->start, $hsp->query->end, $pmodel_id, $hsp->hit->start,
     $hsp->hit->end,  $hsp->length, $hsp->match, $hsp->positive, $hsp->gaps, $hsp->score,
@@ -106,8 +111,8 @@ sub insert_hsp {
 =head2 _get_pseq_id_from_name()
 
  Name:      _get_pseq_id_from_name()
- Purpose:   retrieve pseq_id from a BLAST accession
- Arguments: accession
+ Purpose:   retrieve pseq_id from a FASTA header
+ Arguments: FASTA header
  Returns:   pseq_id (or undef if not found)
 
 =cut
@@ -136,6 +141,42 @@ sub _get_pseq_id_from_name {
   # store pseq_id (if available) in cache for the specified name
   $pseq_id_cache{ $name } = $pseq_id if defined $pseq_id;
   return $pseq_id;
+}
+
+
+#-------------------------------------------------------------------------------
+# _get_pmodel_id_from_pseq_id()
+#-------------------------------------------------------------------------------
+
+=head2 _get_pmodel_id_from_pseq_id()
+
+ Name:      _get_pmodel_id_from_pseq_id()
+ Purpose:   retrieve pmodel_id given a pseq_id
+ Arguments: pseq_id
+ Returns:   pmodel_id (or undef if not found)
+
+=cut
+
+my %pmodel_id_cache;
+sub _get_pmodel_id_from_pseq_id {
+  my ($u,$pseq_id) = @_;
+
+  # use cached value if available
+  return $pmodel_id_cache{ $pseq_id } if 
+    defined $pmodel_id_cache{ $pseq_id };
+
+  my $sql = "select pmodel_id from pmpseq where pseq_id=?";
+  my $sth = $u->prepare_cached($sql);
+  $sth->execute( $pseq_id );
+	my $retval = $sth->fetchrow_arrayref();
+	$sth->finish();
+
+  # store pmodel_id (if available) in cache for the specified pseq_id
+	if ( defined  $retval->[0] ) {
+		$pmodel_id_cache{ $pseq_id } = $retval->[0];
+		return $retval->[0];
+	}
+  return;
 }
 
 
