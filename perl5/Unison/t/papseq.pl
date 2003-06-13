650fb8927ccc4b067d3c54e075ac63a64230eda1
cavs <cavs@localhost>
2003-06-13 22:31:00 +0000

    removed unison parameters

--- perl5/Unison/t/papseq.pl
+++ perl5/Unison/t/papseq.pl
@@ -6,6 +6,6 @@ use Unison;
 
 $ENV{DEBUG}=1;
 die( "USAGE: papseq.pl <blast output>\n" ) if $#ARGV != 0;
-my $u = new Unison( 'username' => 'admin', 'dbname' => 'csb-dev','password' => $ENV{'PGPASSWD'} );
+my $u = new Unison();
 
 $u->load_blast_report( $ARGV[0] );
