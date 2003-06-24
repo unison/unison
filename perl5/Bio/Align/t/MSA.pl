7211360fa51ccc53733592e4ffbaeb25faf68050
cavs <cavs@localhost>
2003-06-24 23:49:15 +0000

    removed obsolete code

--- perl5/Bio/Align/t/MSA.pl
+++ perl5/Bio/Align/t/MSA.pl
@@ -3,31 +3,23 @@
 use warnings;
 use strict;
 use Bio::SeqIO;
-use Bio::Tools::Blast;
 use Bio::Align::MSA;
 use Bio::AlignIO;
-use Data::Dumper;
 
-
-die( "USAGE: cavs.pl <FASTA file> <BLAST output>\n" ) if $#ARGV!=1;
+die( "USAGE: MSA.pl <FASTA file> <BLAST output>\n" ) if $#ARGV!=1;
 
 $ENV{DEBUG} = 0;
 
-# open FASTA file to retrieve information about the query sequence
 my $in = Bio::SeqIO->new(-file=>$ARGV[0], '-format' => 'Fasta', );
 my $query = $in->next_seq();
-my $query_len = length( $query->seq() );
 my $msa = new Bio::Align::MSA( $query );
 
-# Use AlignIO.pm to create a SimpleAlign object from the blast report
+# Use AlignIO.pm to create SimpleAlign objects from the blast report
 my $str = Bio::AlignIO->new(-file=> $ARGV[1],'-format' => 'blast');
 my @alignme;
 while ( my $aln = $str->next_aln() ) {
-	print STDERR '-'x80,"\n";
-  # extract sequences and check values for the alignment column $pos
-	my $i=0;
   foreach my $seq ($aln->each_seq) {
-		push @alignme, $seq;
+    push @alignme, $seq;
   }
 }
 
