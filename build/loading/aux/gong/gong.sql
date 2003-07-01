f6fab578c2790f794b7425686b5b4a3f3ea9794c
rkh <rkh@localhost>
2003-07-01 18:59:55 +0000

    added term definitions & comments granted select on all tables to public added comments on tables and views

--- build/loading/aux/gong/gong.sql
+++ build/loading/aux/gong/gong.sql
@@ -1,4 +1,4 @@
--- $Id: gong.sql,v 1.1 2003/07/01 06:08:08 rkh Exp $
+-- $Id: gong.sql,v 1.2 2003/07/01 16:06:33 rkh Exp $
 -- GOng -- next generation GO tables
 
 -- This sql creates new tables based on the GO term data.  I find these
@@ -12,13 +12,10 @@
 
 
 
-drop schema GOng;
+drop schema GOng cascade;
 create schema GOng;
-grant usage on schema GOng to public;
-comment on schema gong is 'Gene Ontology next generation -- simplified GO access';
 
 set search_path = gong;
-
 
 create or replace function acc2id (text) returns integer
 language plpgsql as '
@@ -40,15 +37,12 @@ language plperl as 'return sprintf("GO:%06d",$_[0])';
 
 
 
-drop table is_a;
-drop table part_of;
-drop table node cascade;
-drop table alias;
-
 create table node (
 	go_id integer  primary key,	
 	is_obsolete boolean default FALSE,
-	alias_id integer )
+	alias_id integer,
+	definition text,
+	comment text )
 	without oids;
 
 create table is_a (
@@ -75,10 +69,11 @@ alter table node add constraint alias_id_exists
 	foreign key (alias_id) references alias(alias_id);
 
 
-
 -- populate nodes: pkey is go id; the term name is just another alias
-insert into node (go_id,is_obsolete)
-	select acc2id(acc),is_obsolete=1 from go.term where acc~'^GO:[0-9]';
+insert into node (go_id,is_obsolete,definition,comment)
+	select acc2id(acc),is_obsolete=1,D.term_definition,D.term_comment from go.term T
+	left join go.term_definition D on D.term_id=T.id
+	where acc~'^GO:[0-9]';
 -- insert aliases 
 insert into alias(go_id,alias)
 	select acc2id(acc),name from go.term where acc~'^GO:[0-9]';
@@ -89,6 +84,7 @@ update node set alias_id=(select alias_id from alias
 insert into alias (go_id,alias) 
 	select acc2id(acc),term_synonym as "alias" from go.term_synonym S
 	join go.term T on T.id=S.term_id;
+
 -- extract the is_a links
 insert into is_a select acc2id(T2.acc),acc2id(T1.acc) from go.term2term 
 	join go.term T1 on T1.id=term1_id  
@@ -99,3 +95,24 @@ insert into part_of select acc2id(T2.acc),acc2id(T1.acc) from go.term2term
 	join go.term T1 on T1.id=term1_id
 	join go.term T2 on T2.id=term2_id
 	where relationship_type_id=3;
+
+
+create view term as select N.go_id,A.alias,N.definition,is_obsolete from node N
+	left join alias A on A.alias_id=N.alias_id
+	order by is_obsolete,N.go_id;
+
+
+grant usage on schema GOng to public;
+comment on schema gong is 'Gene Ontology next generation -- simplified GO access';
+
+grant select on node to public;
+grant select on is_a to public;
+grant select on part_of to public;
+grant select on alias to public;
+grant select on term to public;
+
+comment on table node is 'GO nodes, indexed by their GO id (integer, wo/''GO:'')';
+comment on table alias is 'GO names and synonyms';
+comment on table is_a is 'GO is_a relationships';
+comment on table part_of is 'GO part_of relationships';
+comment on view term is 'simple view of GO nodes';
