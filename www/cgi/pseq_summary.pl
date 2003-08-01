136b32e42f093eda50d6b18597a3cfcd6a660193
rkh <rkh@localhost>
2003-08-01 20:45:20 +0000

    * finally got an initial version going!

--- www/cgi/pseq_summary.pl
+++ www/cgi/pseq_summary.pl
@@ -3,7 +3,6 @@
 use strict;
 use warnings;
 use CGI( -debug );
-use CGI::Carp qw(fatalsToBrowser);
 use Data::Dumper;
 BEGIN
   {
@@ -14,22 +13,37 @@ use lib $ENV{PWD}."/../perl5";
 use Unison::WWW::Page;
 use Unison::WWW::Table;
 
-
 my $p = new Unison::WWW::Page;
 my $u = $p->{unison};
 my $v = $p->Vars();
-$p->ensure_required_params(qw(pseq_id p2params_id));
 
+my $sql = qq/select O.origin,AO.alias,AO.descr from pseqalias SA
+			join paliasorigin AO on AO.palias_id=SA.palias_id
+			join porigin O on O.porigin_id=AO.porigin_id
+			where SA.pseq_id=$v->{pseq_id} and SA.iscurrent=true and O.ann_pref<=10000 
+			order by O.ann_pref/;
+my $ar = $u->selectall_arrayref($sql);
+my @f = qw( origin alias description );
 
-print $p->render("sequence summary for unison:$v->{pseq_id}",
-				 $p->start_form(-action=>'p2alignment.pl'),
-				 $p->submit(-value=>'align checked'),
-				 $p->hidden('pseq_id',$v->{pseq_id}),
-				 $p->hidden('p2params_id',$v->{p2params_id}),
-				 $p->group('Prospect2 Threadings',
+my $seq = $u->get_sequence_by_pseq_id($v->{pseq_id});
+$seq =~ s/.{60}/$&\n/g;
+
+print $p->render("Summary of Unison:$v->{pseq_id}",
+
+				 $p->group("Sequence",
+						   '<pre>', 
+						   '&gt;', $u->best_alias($v->{pseq_id}), "\n",
+						   $seq,
+						   '</pre>' ),
+
+				 $p->group("Aliases",
+						   'These are the aliases from the most reliable sources only; see also ',
+						   '<a href="pseq_paliases.pl?pseq_id=', $v->{pseq_id}, '">other aliases</a><p>',
 						   Unison::WWW::Table::render(\@f,$ar)),
-				 $p->submit(-value=>'align checked'),
-				 $p->end_form(),
-				 '<br><span class="sql">', '<b>SQL query:</b>', $sql, '</span>'
-				 );
 
+
+				 $p->group("Features",
+						   "<img src=\"graphic_features.sh?pseq_id=$v->{pseq_id}\">"),
+
+				 $p->sql($sql)
+				);
