3d97a46e5d251f6dd38f6f10d662aeefbebfe8d7
cavs <cavs@localhost>
2003-08-08 16:57:46 +0000

    added insert_hsp_swap() to handle swapping query-target info on HSPs

--- perl5/Unison/papseq.pm
+++ perl5/Unison/papseq.pm
@@ -1,7 +1,7 @@
 =head1 NAME
 
 Unison::papseq -- Unison papseq table utilities
-S<$Id: papseq.pm,v 1.3 2003/06/13 00:26:50 cavs Exp $>
+S<$Id: papseq.pm,v 1.4 2003/06/13 17:11:41 cavs Exp $>
 
 =head1 SYNOPSIS
 
@@ -47,12 +47,17 @@ sub load_blast_report {
   }
   while(my $sbjct = $report->nextSbjct) {
     my $t_pseq_id = $u->_get_pseq_id_from_name($sbjct->name());
-    if ( !defined $t_pseq_id ) {
-      warn("No pseq_id defined for this target sequence: " . $sbjct->name() . " - skipping\n" );
-      next;
-    }
+		throw Unison::RuntimeError( "No pseq_id defined for this target sequence: " 
+		  . $sbjct->name() ) if ( !defined $t_pseq_id );
+		if ( $q_pseq_id == $t_pseq_id ) {
+			print "Skipping this target ($t_pseq_id) because it is a self hit\n" if $ENV{'DEBUG'};
+			next;
+		}
     # get the pmodel_id for this sequence
     my $pmodel_id = $u->_get_pmodel_id_from_pseq_id($t_pseq_id);
+		if ( ! defined $pmodel_id ) {
+			throw Unison::RuntimeError( "Can't find pmodel_id for pseq_id=$t_pseq_id" );
+		}
 
     while(my $hsp = $sbjct->nextHSP) {
       $u->insert_hsp( $q_pseq_id, $pmodel_id, $hsp );
@@ -77,21 +82,51 @@ sub load_blast_report {
 sub insert_hsp {
   my($u,$pseq_id,$pmodel_id,$hsp) = @_;
 
+	$u->insert_hsp_swap($pseq_id,$pmodel_id,$hsp,0);
+
+  return;
+}
+
+
+#-------------------------------------------------------------------------------
+# insert_hsp_swap()
+#-------------------------------------------------------------------------------
+
+=head2 insert_hsp_swap()
+
+ Name:      insert_hsp_swap()
+ Purpose:   insert 1 Bio::Tools::BPlite::HSP allow for swapping of query and
+            target information.
+ Arguments: query pseq_id, target pmodel_id, Bio::Tools::BPlite::HSP, swap flag
+ Returns:   nada
+
+=cut
+
+sub insert_hsp_swap {
+  my($u,$pseq_id,$pmodel_id,$hsp,$swap) = @_;
+
   # check parameters!
   if      ( ! defined $u or ( ref $u ne 'Unison' ) ) {
     throw Unison::BadUsage( 'Unison::insert_hsp() requires Unison object' );
   } elsif ( ! defined $pseq_id ) {
     throw Unison::BadUsage( 'Unison::insert_hsp() requires query pseq_id as a parameter' );
   } elsif ( ! defined $pmodel_id ) {
-    throw Unison::BadUsage( 'Unison::insert_hsp() requires target pseq_id as a parameter' );
+    throw Unison::BadUsage( 'Unison::insert_hsp() requires pmodel_id as a parameter' );
   } elsif ( ! defined $hsp or ( ref $hsp ne 'Bio::Tools::BPlite::HSP' ) ) {
     throw Unison::BadUsage( 'Unison::insert_hsp() requires Bio::Tools::BPlite::HSP object' );
   }
   throw Unison::RuntimeError( 'Unison connection is not open' ) if ! $u->is_open();
   
-  my $sql_start = "insert into papseq " .
-    "(pseq_id, start, stop, pmodel_id, mstart, mstop, len, ident, sim, " .
-    "gaps,score,eval,pct_ident)";
+  my $sql_start;
+	if ( $swap ) {
+		$sql_start = "insert into papseq " .
+			"(pseq_id, mstart, mstop, pmodel_id, start, stop, len, ident, sim, " .
+			"gaps,score,eval,pct_ident)";
+	} else {
+		$sql_start = "insert into papseq " .
+			"(pseq_id, start, stop, pmodel_id, mstart, mstop, len, ident, sim, " .
+			"gaps,score,eval,pct_ident)";
+	}
   my $sql = $sql_start . "values (?,?,?,?,?,?,?,?,?,?,?,?,?)";
   my $sth = $u->prepare_cached($sql);
   my @values = ( $pseq_id, $hsp->query->start, $hsp->query->end, $pmodel_id, $hsp->hit->start,
