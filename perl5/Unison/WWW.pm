79234788de580aac91e01f0e7c54d9bc5672fb3c
rkh <rkh@localhost>
2003-08-18 23:25:34 +0000

    forgot the 1; (again)

--- perl5/Unison/WWW.pm
+++ perl5/Unison/WWW.pm
@@ -1,11 +1,13 @@
 package Unison::WWW;
 
-
 # prepend the full path of ../perl5/ to @INC
 # this facilitates tree relocation / dev trees in user directories
+# To be safe, it's probably best to load this last.
+
 BEGIN {
   if (exists $ENV{SCRIPT_FILENAME})
 	{ ($ENV{PWD}) = $ENV{SCRIPT_FILENAME} =~ m%^(.*/)%; }
 }
 use lib $ENV{PWD}."/../perl5";
 
+1;
