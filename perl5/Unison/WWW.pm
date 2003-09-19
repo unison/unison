32be644de1fd47418cbf5a760fa7e77fdadd6bf4
rkh <rkh@localhost>
2003-09-19 21:03:34 +0000

    *** empty log message ***

--- perl5/Unison/WWW.pm
+++ perl5/Unison/WWW.pm
@@ -5,9 +5,10 @@ package Unison::WWW;
 # To be safe, it's probably best to load this last.
 
 BEGIN {
-  if (exists $ENV{SCRIPT_FILENAME})
-	{ ($ENV{PWD}) = $ENV{SCRIPT_FILENAME} =~ m%^(.*/)%; }
+  $ENV{SCRIPT_FILENAME} = $0 unless (exists $ENV{SCRIPT_FILENAME});
+  my ($dir) = $ENV{SCRIPT_FILENAME} =~ m%^(.*\/)%;
+  $dir .= '/../perl5';
+  unshift(@INC, $dir) if (-d $dir);
 }
-use lib $ENV{PWD}."../perl5";
 
 1;
