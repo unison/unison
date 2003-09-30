b1429fcbab5f115c9e308b28c4dfdf5d9bcabe8b
cavs <cavs@localhost>
2003-09-30 22:48:45 +0000

    - added boolean flag to best_OA, best_OAD, best_palias_id, best_alias,best_annotation to allow origins with null ann_pref - kept backward compatible versions of the above methods - added lot'sa documentation

--- misc/sql-scraps/best_annotation.sql
+++ misc/sql-scraps/best_annotation.sql
@@ -1,22 +1,37 @@
+-- -----------------------------------------------------------------------------
+-- NAME: palias_OAD_fmt(integer)
+-- PURPOSE: returns text string of origin, alias, and descr for a given palias_id
+-- ARGUMENTS: palias_id
+-- RETURNS: text 
+--
 create or replace function unison.palias_OAD_fmt(integer)
 returns text language plpgsql as '
 DECLARE
 	a_id alias for $1;
 	rv text;
+	rec record;
 BEGIN
 	if a_id is null then
 		return null;
 	end if;
 
-	select into rv 
-		O.origin || '':'' || A.alias || '' ('' || A.descr || '')'' 
+	select into rv O.origin || '':'' || A.alias || '' ('' || coalesce(A.descr, ''No descr available'') || '')''
 		from paliasorigin A
 		join porigin O on O.porigin_id=A.porigin_id
 		where A.palias_id=a_id;
+
 	return rv;
 END;';
+-- -----------------------------------------------------------------------------
+comment on function unison.palias_OAD_fmt(integer)  is 'cannonical annotation format of given palias_id, like this: <origin>:<alias> (<description>)';
 
 
+-- -----------------------------------------------------------------------------
+-- NAME: palias_OA_fmt(integer)
+-- PURPOSE: returns text string of origin and alias for a given palias_id
+-- ARGUMENTS: palias_id
+-- RETURNS: text 
+--
 create or replace function unison.palias_OA_fmt(integer)
 returns text language plpgsql as '
 DECLARE
@@ -33,12 +48,21 @@ BEGIN
 		where A.palias_id=a_id;
 	return rv;
 END;';
+-- -----------------------------------------------------------------------------
+comment on function unison.palias_OA_fmt(integer)   is 'cannonical alias format of given palias_id, like this: <origin>:<alias>';
 
 
-create or replace function unison.best_palias_id(integer) 
+-- -----------------------------------------------------------------------------
+-- NAME: best_palias_id(integer,boolean)
+-- PURPOSE: return the palias_id of the best annotation (highest ann_pref value)
+-- ARGUMENTS: pseq_id, flag for whether to allow null ann_pref         
+-- RETURNS: text
+--
+create or replace function unison.best_palias_id(integer,boolean) 
 returns integer language plpgsql as '
 DECLARE
 	q_id alias for $1;
+  flag alias for $2;
 	a_id integer;
 	o_id integer;
 	rv text;
@@ -48,39 +72,140 @@ BEGIN
 		return a_id;
 	end if;
 
-	select into a_id QA.palias_id from pseqalias QA
-		join paliasorigin OA on OA.palias_id=QA.palias_id
-		join porigin O on O.porigin_id=OA.porigin_id
-		where QA.pseq_id=q_id and QA.iscurrent=TRUE and O.ann_pref is not null
-		order by O.ann_pref limit 1;
+	-- allow null ann_pref
+	if flag then
+		select into a_id QA.palias_id from pseqalias QA
+			join paliasorigin OA on OA.palias_id=QA.palias_id
+			join porigin O on O.porigin_id=OA.porigin_id
+			where QA.pseq_id=q_id and QA.iscurrent=TRUE 
+			order by O.ann_pref limit 1;
+	-- dont allow null ann_pref
+	else
+		select into a_id QA.palias_id from pseqalias QA
+			join paliasorigin OA on OA.palias_id=QA.palias_id
+			join porigin O on O.porigin_id=OA.porigin_id
+			where QA.pseq_id=q_id and QA.iscurrent=TRUE and O.ann_pref is not null
+			order by O.ann_pref limit 1;
+	end if;
+
 	if a_id is not null then
 		return a_id;
 	end if;
 
 	return NULL;
 END;';
+-- -----------------------------------------------------------------------------
+comment on function unison.best_palias_id(integer,boolean)  is '"best" palias_id of given pseq_id; flag for null ann_pref; see best_* functions';
 
 
+-- -----------------------------------------------------------------------------
+-- NAME: best_palias_id(integer)
+-- PURPOSE: backwardly form of best_palias_id(integer,boolean) use false for boolean
+-- ARGUMENTS: pseq_id
+-- RETURNS: text
+--
+create or replace function unison.best_palias_id(integer) 
+returns integer language plpgsql as 
+'BEGIN return best_palias_id($1,false); END;';
+-- -----------------------------------------------------------------------------
+comment on function unison.best_palias_id(integer)  is 'alias for best_palias_id(integer,false)';
+
+
+-- -----------------------------------------------------------------------------
+-- NAME: best_OA(integer,boolean)
+-- PURPOSE: return best origin and alias for a given pseq_id, use boolean
+--          to allow inclusion of null ann_pref origins
+-- ARGUMENTS: pseq_id, boolean flag for including origins with null ann_pref
+-- RETURNS: text
+--
+create or replace function unison.best_OA(integer,boolean) 
+returns text language plpgsql as
+'BEGIN return palias_OA_fmt( best_palias_id( $1, $2 ) ); END;';
+-- -----------------------------------------------------------------------------
+comment on function unison.best_OA(integer,boolean) is '"best" <origin:alias> for pseq_id';
+
+
+-- -----------------------------------------------------------------------------
+-- NAME: best_OA(integer)
+-- PURPOSE: backwardly form of best_OA(integer,boolean) use false for boolean
+-- ARGUMENTS: pseq_id
+-- RETURNS: text
+--
 create or replace function unison.best_OA(integer) 
 returns text language plpgsql as
-'BEGIN return palias_OA_fmt( best_palias_id( $1 ) ); END;';
+'BEGIN return best_OA( $1, false ); END;';
+-- -----------------------------------------------------------------------------
+comment on function unison.best_OA(integer) is 'alias for best_OA(integer,false)';
 
+
+-- -----------------------------------------------------------------------------
+-- NAME: best_OAD(integer,boolean)
+-- PURPOSE: return best origin, alias and descr for a given pseq_id, use boolean
+--          to allow inclusion of null ann_pref origins
+-- ARGUMENTS: pseq_id, boolean flag for including origins with null ann_pref
+-- RETURNS: text
+--
+create or replace function unison.best_OAD(integer,boolean) 
+returns text language plpgsql as
+'BEGIN return palias_OAD_fmt( best_palias_id( $1, $2 ) ); END;';
+comment on function unison.best_OAD(integer,boolean) is '"best" <origin:alias (description)> for pseq_id';
+-- -----------------------------------------------------------------------------
+
+
+-- -----------------------------------------------------------------------------
+-- NAME: best_OAD(integer)
+-- PURPOSE: alias for best_OAD(integer,false)
+-- ARGUMENTS: pseq_id
+-- RETURNS: text
+--
 create or replace function unison.best_OAD(integer) 
 returns text language plpgsql as
-'BEGIN return palias_OAD_fmt( best_palias_id( $1 ) ); END;';
+'BEGIN return best_OAD( $1, false ); END;';
+comment on function unison.best_OAD(integer) is 'alias for best_OAD(integer,false)';
+-- -----------------------------------------------------------------------------
 
+
+-- -----------------------------------------------------------------------------
+-- NAME: best_alias(integer,boolean)
+-- PURPOSE: alias for best_OA(integer,boolean)
+-- ARGUMENTS: pseq_id, boolean flag for including origins with null ann_pref
+-- RETURNS: text
+--
+create or replace function unison.best_alias(integer,boolean) 
+returns text language plpgsql as 'BEGIN	return best_OA( $1, $2 ); END;';
+-- -----------------------------------------------------------------------------
+comment on function unison.best_alias(integer,boolean) is 'same as best_OA';
+
+
+-- -----------------------------------------------------------------------------
+-- NAME: best_alias(integer)
+-- PURPOSE: alias for best_alias(integer,false)
+-- ARGUMENTS: pseq_id
+-- RETURNS: text
+--
 create or replace function unison.best_alias(integer) 
-returns text language plpgsql as 'BEGIN	return best_OA( $1 ); END;';
+returns text language plpgsql as 'BEGIN	return best_alias( $1, false ); END;';
+-- -----------------------------------------------------------------------------
+comment on function unison.best_alias(integer) is 'alias for best_alias(integer,false)';
 
+
+-- -----------------------------------------------------------------------------
+-- NAME: best_annotation(integer,boolean)
+-- PURPOSE: alias for best_OAD(integer,boolean)
+-- ARGUMENTS: pseq_id, boolean flag for including origins with null ann_pref
+-- RETURNS: text
+--
+create or replace function unison.best_annotation(integer,boolean) 
+returns text language plpgsql as 'BEGIN	return best_OAD( $1, $2 ); END;';
+comment on function unison.best_annotation(integer,boolean) is 'same as best_OAD';
+
+
+-- -----------------------------------------------------------------------------
+-- NAME: best_annotation(integer)
+-- PURPOSE: alias for best_annotation(integer,false)
+-- ARGUMENTS: pseq_id
+-- RETURNS: text
+--
 create or replace function unison.best_annotation(integer) 
-returns text language plpgsql as 'BEGIN	return best_OAD( $1 ); END;';
-
-
-
-comment on function unison.best_palias_id(integer)  is '"best" palias_id of given pseq_id; see best_* functions';
-comment on function unison.best_OA(integer)		    is '"best" <origin:alias> for pseq_id';
-comment on function unison.best_OAD(integer)	    is '"best" <origin:alias (description)> for pseq_id';
-comment on function unison.best_alias(integer)      is 'same as best_OA';
-comment on function unison.best_annotation(integer) is 'same as best_OAD';
-comment on function unison.palias_OAD_fmt(integer)  is 'cannonical annotation format of given palias_id, like this: <origin>:<alias> (<description>)';
-comment on function unison.palias_OA_fmt(integer)   is 'cannonical alias format of given palias_id, like this: <origin>:<alias>';
+returns text language plpgsql as 'BEGIN	return best_annotation( $1, false ); END;';
+comment on function unison.best_annotation(integer) is 'alias for best_annotation(integer,false)';
