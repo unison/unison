4b64a5c369f6b426bbc1753c325b66e74ce40754
cavs <cavs@localhost>
2003-10-16 18:23:07 +0000

    SQL script to build pfsignalp table

---
+++ misc/sql-scraps/create_pfsignalp.sql
@@ -0,0 +1,36 @@
+-- -----------------------------------------------------------------------------
+-- NAME: create_pfsignalp.sql
+-- PURPOSE: sql statements to create pfsignalp table
+--
+-- $Id: create_papseq.sql,v 1.1 2003/07/14 23:35:29 cavs Exp $
+-- -----------------------------------------------------------------------------
+
+DROP TABLE pfsignalp;
+CREATE TABLE pfsignalp (
+    pftype_id integer DEFAULT pftype_id_lookup('SignalP-2.0'::text),
+    siganchor_prob real
+)
+INHERITS (pfeature) WITHOUT OIDS;
+COMMENT ON TABLE pfsignalp IS 'signal sequence prediction from SignalP';
+COMMENT ON COLUMN pfsignalp.confidence IS 'signal sequence probability';
+COMMENT ON COLUMN pfsignalp.siganchor_prob IS 'signal anchor probability';
+
+-- permissions
+REVOKE ALL ON TABLE pfsignalp FROM PUBLIC;
+GRANT SELECT ON TABLE pfsignalp TO PUBLIC;
+GRANT INSERT,UPDATE ON TABLE pfsignalp TO loader;
+
+-- indices
+CREATE UNIQUE INDEX pfsignalp_sc_exists ON pfsignalp USING btree (pseq_id, "start", stop);
+CREATE INDEX pfsignalp_pseq_id_idx ON pfsignalp USING btree (pseq_id);
+CREATE INDEX pfsignalp_pftype_id_idx ON pfsignalp USING btree (pftype_id);
+CREATE INDEX pfsignalp_start_idx ON pfsignalp USING btree ("start");
+CREATE INDEX pfsignalp_stop_idx ON pfsignalp USING btree (stop);
+CREATE INDEX pfsignalp_quality_idx ON pfsignalp USING btree (confidence);
+CREATE INDEX pfsignalp_siganchor_idx ON pfsignalp USING btree (siganchor_prob);
+
+-- foreign keys
+ALTER TABLE ONLY pfsignalp
+    ADD CONSTRAINT pseq_id_exists FOREIGN KEY (pseq_id) REFERENCES pseq(pseq_id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY pfsignalp
+    ADD CONSTRAINT pftype_id_exists FOREIGN KEY (pftype_id) REFERENCES pftype(pftype_id) ON UPDATE CASCADE ON DELETE CASCADE;
