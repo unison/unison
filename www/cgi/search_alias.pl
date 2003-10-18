bdad7beb55f1be9cf4a73a6526fe98148dd810f3
rkh <rkh@localhost>
2003-10-18 00:06:54 +0000

    * now uses Unison:: palias code to do alias lookups

--- www/cgi/search_alias.pl
+++ www/cgi/search_alias.pl
@@ -6,12 +6,12 @@ use Unison::WWW;
 use Unison::WWW::Page;
 use Unison::WWW::Table;
 
-my $p = new Unison::WWW::Page;
+my $p = new Unison::WWW::Page();
 my $u = $p->{unison};
 my $v = $p->Vars();
 
 print $p->render("Sequence lookup by alias"
-				    . (defined $v->{seq} ? ": $v->{seq}" : ''),
+				 . (defined $v->{alias} ? ": $v->{alias}" : ''),
 
 				 $p->tip("use % for wildcard (but it's slower)"),
 				 $p->start_form(-method => 'GET',
@@ -30,29 +30,27 @@ print $p->render("Sequence lookup by alias"
 
 sub do_search {
   my $q = shift;
-  my $results;
-  my $sql = 'select pseq_id,canonical_oad_fmt(origin,alias,descr) as oad from v_palias';
-  if ($q =~ m/%/) {
-	$sql .= " where alias ilike ?";
-  } else {
-	$sql .= " where alias = ?";
-  }
-  $sql .= ' limit 1001';
+  my @pids = $u->get_pseq_id_from_alias( $q );
 
+  if ($#pids == -1) {
+	return( '<b>No results returned</b>'
+			. '<br>NOTE: Short regexp queries are silently ignored.');
+  }
+
+  if ($#pids >= 100) {
+	return('<b>Too many results returned (' .($#pids+1) 
+		   .'); please narrow your query (max is 100)</b>');
+  }
+
+  my $sth = $u->prepare('select best_oad(?)');
+  my @ar = map { [$_, $u->selectrow_array($sth,undef,$_) ] } @pids;
   my @fields = ( 'pseq_id', 'origin:alias (description)' );
-  my $ar = $u->selectall_arrayref($sql,undef,$q);
-
-  if ($#$ar > 1000) {
-	return "<b>Too many results returned ($#$ar); please narrow your query</b>";
-  }
-
+  my $ar = \@ar;
   for(my $i=0; $i<=$#$ar; $i++) {
 	$ar->[$i][0] = sprintf('<a href="pseq_summary.pl?pseq_id=%d">%d</a>',
 						   $ar->[$i][0],$ar->[$i][0]);
   }
 
-  $results = $p->group(sprintf("%d results for %s",$#$ar+1,$q),
-					   Unison::WWW::Table::render(\@fields,$ar));
-  $results .= $p->sql($sql);
-  return $results;
+  return $p->group(sprintf("%d results for %s",$#$ar+1,$q),
+				   Unison::WWW::Table::render(\@fields,$ar));
 }
