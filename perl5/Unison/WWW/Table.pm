595dc1679cd7ed18f66955ada916523191af97e4
rkh <rkh@localhost>
2003-11-03 18:08:18 +0000

    * group() returns array of items, not a scalar string (expected to be faster) * changes in render()

--- perl5/Unison/WWW/Table.pm
+++ perl5/Unison/WWW/Table.pm
@@ -47,43 +47,55 @@ sub render_th {
 
 
 sub render_compat {
+  # this code is among the worst I've written. I'm sorry.
+
   my $fr = shift;
   my $ar = shift;
   my $opts = shift;
-  my $rv = '';
 
-  $rv .= "<table class=\"uwtable\" border=\"0\" width=\"100%\">\n";
-  if ($#$ar > -1) {
-	if (defined $opts->{highlight_column}) {
-	  $rv .= '<tr>' . join('', map { '<th'
-									 . ($_==$opts->{highlight_column} ? ' class="highlighted"' : '').'>'
-									 . $fr->[$_] .'</th>'
-									   }
-						   0..$#$fr) . "</tr>\n";
-	  for(my $r=0; $r<=$#$ar; $r++) {
-		$rv .= "<tr>";
-		my @row = @{ $ar->[$r] };
-		for(my $c=0; $c<=$#row; $c++) {
-		  my $cl = $c==$opts->{highlight_column} ? 'class="highlighted"' : '';
-		  my $al = 'align="' . guess_alignment($row[$c]) . '"';
-		  $rv .= "<td $cl $al>$row[$c]</td>";
-		}
-		$rv .= "</tr>\n";
-	  }
-	} else {
-	  $rv .= "<tr>" . join('',map {'<th align="'.guess_alignment($_).'">'.$_.'</th>'} @$fr)."</tr>\n";
-	  $rv .= "<tr>" . join('',map {'<td>'.$_.'</td>'} @$_) . "</tr>\n" for @$ar; 
-	}
-  } else {
+  my $tbl_start = "<table class=\"uwtable\" border=\"0\" width=\"100%\">\n";
+  my $tbl_end = "</table>\n";
+
+  my $rv = $tbl_start;
+
+  if ($#$ar == -1) {
 	$rv .= sprintf("<tr><td bgcolor=\"$highlight_color\" colspan=\"%d\">query returned no data</td></tr>\n",$#$fr+1);
+	$rv .= $tbl_end;
+	return $rv;
   }
-  $rv .= "</table>\n";
+
+  if (not defined $opts->{highlight_column}) {
+	$rv .= "<tr>" . join('',map {'<th align="'.guess_alignment($_).'">'.$_.'</th>'} @$fr)."</tr>\n";
+	$rv .= "<tr>" . join('',map {'<td ailgn="'.guess_alignment($_).'">'.$_.'</td>'} @$_) ."</tr>\n" for @$ar; 
+	$rv .= $tbl_end;
+	return $rv;
+  }
+
+  # remainder is for the special case of column highlighting
+  $rv .= '<tr>' . join('', map { '<th'
+								   . ($_==$opts->{highlight_column} ? ' class="highlighted"' : '').'>'
+									 . $fr->[$_] .'</th>'
+								   }
+					   0..$#$fr) . "</tr>\n";
+  for(my $r=0; $r<=$#$ar; $r++) {
+	$rv .= "<tr>";
+	my @row = @{ $ar->[$r] };
+	for(my $c=0; $c<=$#row; $c++) {
+	  my $cl = $c==$opts->{highlight_column} ? 'class="highlighted"' : '';
+	  my $al = 'align="' . guess_alignment($row[$c]) . '"';
+	  $rv .= "<td $cl $al>$row[$c]</td>";
+	}
+	$rv .= "</tr>\n";
+  }
+
+  $rv .= $tbl_end;
   return $rv;
 }
 
+
 sub guess_alignment {
   my $s = shift;
-  return $s =~ m/[^-.\d]/ ? 'left' : 'right';
+  return $s =~ m/^[-.e\d]+$/ ? 'right' : 'left';
   }
 
 
