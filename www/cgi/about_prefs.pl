f77f49c2b3da80d4df65e411b32449d05aab5182
Reece Hart <reecehart@gmail.com>
2003-11-03 19:11:02 +0000

    initial

---
+++ www/cgi/about_prefs.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Unison::WWW;
+use Unison::WWW::Page;
+use Unison::WWW::Table;
+
+my $p = new Unison::WWW::Page;
+my $u = $p->{unison};
+my $P = $p->{userprefs};
+
+print $p->render("Unison User Preferences for $u->{username}",
+				 "<p>(user preferences are currently read-only)\n",
+				 "<pre>\n",
+				 (map { "$_: $P->{$_}\n" } sort keys %$P),
+				 "</pre>\n"
+				);
+
