e243199e1da0c9f82529ce58f225e093d6563cd4
rkh <rkh@localhost>
2003-11-03 19:17:03 +0000

    initial

---
+++ perl5/Unison/WWW/userprefs.pm
@@ -0,0 +1,32 @@
+package Unison;
+use strict;
+use warnings;
+
+
+# return user prefs as hashref
+sub get_userprefs {
+  my $self = shift;
+  my $userprefs;
+  $self->is_open()
+	|| croak("Unison connection not established");
+
+  my $sth = $self->prepare('select userprefs.* from userprefs  natural join pg_user where usename=?');
+
+  # try for this user
+  $userprefs = $self->selectrow_hashref($sth,undef,$self->{username});
+
+  # else use PUBLIC user's prefs
+  if (not defined $userprefs) {
+	$userprefs = $self->selectrow_hashref($sth,undef,'PUBLIC');
+  }
+
+  # ack! at least return a hashref
+  if (not defined $userprefs) {
+	$userprefs = {};
+  }
+
+  return $userprefs;
+  }
+
+
+1;
