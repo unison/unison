14f088880015b12dc386fcb3848eb88afa775766
cavs <cavs@localhost>
2004-01-27 22:38:34 +0000

    added more comments

--- misc/sql-scraps/blat.sql
+++ misc/sql-scraps/blat.sql
@@ -2,14 +2,13 @@
 -- Name: blat.sql
 -- Purpose: sql code for generating tables for storing blat results
 --
--- $Id$
+-- $Id: blat.sql,v 1.2 2004/01/27 21:51:52 cavs Exp $
 -- -----------------------------------------------------------------------------
 
 -- -----------------------------------------------------------------------------
 -- Name: genasm
 -- Purpose: store genome assembly information
--- -----------------------------------------------------------------------------
-
+--
 DROP TABLE genasm CASCADE;
 CREATE TABLE genasm (
     genasm_id serial NOT NULL,
@@ -30,12 +29,13 @@ ALTER TABLE ONLY genasm
 REVOKE ALL ON TABLE genasm FROM PUBLIC;
 GRANT SELECT ON TABLE genasm TO PUBLIC;
 GRANT INSERT,UPDATE ON TABLE genasm TO loader;
+-- -----------------------------------------------------------------------------
 
 
 -- -----------------------------------------------------------------------------
 -- Name: p2gblataln
 -- Purpose: store protein-to-dna BLAT alignments
--- -----------------------------------------------------------------------------
+-- 
 DROP TABLE p2gblataln CASCADE;
 CREATE TABLE p2gblataln (
     p2gblataln_id serial NOT NULL,
@@ -60,13 +60,13 @@ ALTER TABLE ONLY p2gblataln
 REVOKE ALL ON TABLE p2gblataln FROM PUBLIC;
 GRANT SELECT ON TABLE p2gblataln TO PUBLIC;
 GRANT INSERT,UPDATE,DELETE ON TABLE p2gblataln TO loader;
-
+-- -----------------------------------------------------------------------------
 
 
 -- -----------------------------------------------------------------------------
 -- Name: p2gblathsp
 -- Purpose: store protein-to-dna BLAT HSPs
--- -----------------------------------------------------------------------------
+-- 
 DROP TABLE p2gblathsp CASCADE;
 CREATE TABLE p2gblathsp (
     p2gblathsp_id serial NOT NULL,
@@ -80,6 +80,8 @@ CREATE TABLE p2gblathsp (
     pstop integer NOT NULL
 ) WITHOUT OIDS;
 COMMENT ON TABLE p2gblathsp IS 'protein to dna BLAT HSPs';
+COMMENT ON COLUMN p2gblathsp.chr IS 'chromosome (e.g. 1..22,M,U,X,Y for homo sapiens)';
+COMMENT ON COLUMN p2gblathsp.plus_strand IS 'located on genomic plus strand (t/f)';
 COMMENT ON COLUMN p2gblathsp.pstart IS 'start of HSP on protein (1-based)';
 COMMENT ON COLUMN p2gblathsp.pstop IS 'stop of HSP on protein (1-based)';
 COMMENT ON COLUMN p2gblathsp.gstart IS 'start of HSP on genome (1-based, +1 frame, gstop > gstart)';
@@ -99,33 +101,41 @@ ALTER TABLE ONLY p2gblathsp
 REVOKE ALL ON TABLE p2gblathsp FROM PUBLIC;
 GRANT SELECT ON TABLE p2gblathsp TO PUBLIC;
 GRANT INSERT,UPDATE,DELETE ON TABLE p2gblathsp TO loader;
+-- -----------------------------------------------------------------------------
 
 
 -- -----------------------------------------------------------------------------
 -- Name: p2gblatalnhsp
 -- Purpose: associates protein-to-genome BLAT HSPs with alignments
--- -----------------------------------------------------------------------------
+-- 
 DROP TABLE p2gblatalnhsp CASCADE;
 CREATE TABLE p2gblatalnhsp (
     p2gblataln_id integer NOT NULL,
     p2gblathsp_id integer NOT NULL
 ) WITHOUT OIDS;
+COMMENT ON TABLE p2gblatalnhsp IS 'groups protein-to-genome BLAT HSPs into alignments';
 CREATE UNIQUE INDEX p2gblatalnhsp_unq ON p2gblatalnhsp USING btree (p2gblataln_id,p2gblathsp_id);
 ALTER TABLE ONLY p2gblatalnhsp
     ADD CONSTRAINT p2gblathsp_id_exists FOREIGN KEY (p2gblathsp_id) REFERENCES p2gblathsp(p2gblathsp_id);
 ALTER TABLE ONLY p2gblatalnhsp
     ADD CONSTRAINT p2gblataln_id_exists FOREIGN KEY (p2gblataln_id) REFERENCES p2gblataln(p2gblataln_id);
 
+-- prevent updates - once defined, alignments don't change
+CREATE RULE no_update AS ON UPDATE TO p2gblatalnhsp DO NOTHING;
+
+REVOKE ALL ON TABLE p2gblathsp FROM PUBLIC;
+GRANT SELECT ON TABLE p2gblathsp TO PUBLIC;
+GRANT INSERT,DELETE ON TABLE p2gblathsp TO loader;
+-- -----------------------------------------------------------------------------
 
 
 -- -----------------------------------------------------------------------------
---
 -- split():
 --   purpose: analagous to Perl's split operator
 --   arguments: text to split, delimiter
 --   returns: array of split text 
 --   
---   origin: code found in a posting from joeconway.com
+--   origin: code found in a posting from Joe Conway
 --
 CREATE OR REPLACE FUNCTION split(text, text)
 RETURNS text[] AS '
@@ -152,6 +162,26 @@ RETURNS text[] AS '
     RETURN result_arr;
   END
 ' LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION split IS 'split text into an array - similar to Perl split';
+
+
+-- -----------------------------------------------------------------------------
+-- hsp_str_to_hsp_ids():
+--   purpose: deserialize a hsp_str into an array of p2gblathsp_ids
+--   argument: text serialization
+--   returns: array of p2gblathsp_ids
+--
+CREATE OR REPLACE FUNCTION hsp_str_to_hsp_ids(text) RETURNS int[] AS '
+DECLARE
+  hsp_str ALIAS FOR $1;
+  hsp_ids int[];
+BEGIN
+  SELECT INTO hsp_ids split(hsp_str,'':'');
+  RETURN hsp_ids;
+END;
+' LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION hsp_str_to_hsp_ids IS 'deserialize a hsp_str into an array of p2gblathsp_ids';
+-- -----------------------------------------------------------------------------
 
 
 -- -----------------------------------------------------------------------------
@@ -168,6 +198,7 @@ DECLARE
   low integer;
   high integer;
 BEGIN
+  -- get bounds of hsp_ids array for looping
   low  := replace(split_part(array_dims(hsp_ids),'':'',1),''['','''')::int;
   high := replace(split_part(array_dims(hsp_ids),'':'',2),'']'','''')::int;
 
@@ -179,28 +210,11 @@ BEGIN
   return hsp_str;
 END;
 ' LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION hsp_ids_to_hsp_str IS 'serialize an array of p2gblathsp_ids';
+-- -----------------------------------------------------------------------------
 
 
 -- -----------------------------------------------------------------------------
---
--- hsp_str_to_hsp_ids():
---   purpose: deserialize a hsp_str into an array of p2gblathsp_ids
---   argument: text serialization
---   returns: array of p2gblathsp_ids
---
-CREATE OR REPLACE FUNCTION hsp_str_to_hsp_ids(text) RETURNS int[] AS '
-DECLARE
-  hsp_str ALIAS FOR $1;
-  hsp_ids int[];
-BEGIN
-  SELECT INTO hsp_ids split(hsp_str,'':'');
-  RETURN hsp_ids;
-END;
-' LANGUAGE 'plpgsql';
-
-
--- -----------------------------------------------------------------------------
---
 -- ins_p2gblathsp():
 --   purpose: insert protein-to-genome BLAT HSPs
 --   arguments: genasm_id,chr,plus_strand,gstart,gstop,pseq_id,pstart,pstop
@@ -231,16 +245,17 @@ BEGIN
     pstop=v_pstop;
   IF v_p2gblathsp_id is null THEN
     SELECT INTO v_p2gblathsp_id nextval(''unison.p2gblathsp_p2gblathsp_id_seq''::text);
-    INSERT INTO p2gblathsp (genasm_id,chr,plus_strand,gstart,gstop,pseq_id,pstart,pstop,p2gblathsp_id) VALUES
-      (v_genasm_id,v_chr,v_plus_strand,v_gstart,v_gstop,v_pseq_id,v_pstart,v_pstop,v_p2gblathsp_id);
+    INSERT INTO p2gblathsp (p2gblathsp_id,genasm_id,chr,plus_strand,gstart,gstop,pseq_id,pstart,pstop) VALUES
+      (v_p2gblathsp_id,v_genasm_id,v_chr,v_plus_strand,v_gstart,v_gstop,v_pseq_id,v_pstart,v_pstop);
   END IF;
   return v_p2gblathsp_id;
 END;
 ' LANGUAGE 'plpgsql';
+COMMENT ON FUNCTION hsp_ids_to_hsp_str IS 'insert protein-to-genome BLAT HSPs';
+-- -----------------------------------------------------------------------------
 
 
 -- -----------------------------------------------------------------------------
---
 -- assign_p2gblataln():
 --   purpose: insert protein-to-genome BLAT alignments
 --   arguments: hsp_str, ident, pgap_cnt, pgap_bases, ggap_cnt, ggap_bases
@@ -262,7 +277,6 @@ DECLARE
 BEGIN
   -- check whether hsp_str is already in the database
   SELECT INTO v_p2gblataln_id p2gblataln_id FROM p2gblataln WHERE hsp_str=v_hsp_str;
-  RAISE WARNING ''v_p2gblataln_id: %'',v_p2gblataln_id;
   IF v_p2gblataln_id is null THEN
     SELECT INTO v_p2gblataln_id nextval(''unison.p2gblataln_p2gblataln_id_seq''::text);
     INSERT INTO p2gblataln (p2gblataln_id, hsp_str, ident, pgap_cnt, pgap_bases, ggap_cnt, ggap_bases) VALUES
@@ -281,4 +295,5 @@ BEGIN
   return v_p2gblataln_id;
 END;
 ' LANGUAGE 'plpgsql';
-
+COMMENT ON FUNCTION assign_p2gblataln IS 'insert protein-to-genome BLAT alignments';
+-- -----------------------------------------------------------------------------
