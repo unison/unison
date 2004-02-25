cb0e715adf81658017b1c661609198a8cdbd8c8e
rkh <rkh@localhost>
2004-02-25 20:46:25 +0000

    added exceptions, new pfeatures

--- www/cgi/pseq_history.pl
+++ www/cgi/pseq_history.pl
@@ -6,6 +6,7 @@ use Unison::WWW;
 use Unison::WWW::Page;
 use Unison::WWW::Table;
 use Unison::Exceptions;
+use Error qw(:try);
 
 
 my $p = new Unison::WWW::Page;
@@ -15,19 +16,20 @@ my $v = $p->Vars();
 my $sql = qq/select params,origin,modelset,ran_on from v_run_history where pseq_id=$v->{pseq_id}/;
 my $ar;
 
-$ar = $u->selectall_arrayref($sql);
 
-#try {
-#  $ar = $u->selectall_arrayref($sql);
-#} catch Unison::Exception with {
-#  $p->die('SQL Query Failed',
-#		  $sql,$_[0]);
-#};
+try {
+  $ar = $u->selectall_arrayref($sql);
+} catch Unison::Exception with {
+  $p->die('SQL Query Failed',
+		  $_[0],
+		  $p->sql($sql));
+};
 
 
 my @f = qw(params origin modelset ran_on);
 print $p->render("Run history for Unison:$v->{pseq_id}",
 				 $p->best_annotation($v->{pseq_id}),
+				 '<p>',
 				 $p->group("Run History",
 						   Unison::WWW::Table::render(\@f,$ar)),
 				 $p->sql($sql)
