ccd751a63d9505d3a936f319e9d05553088fcbd6
cavs <cavs@localhost>
2004-03-23 00:35:12 +0000

    script to update the tax_id in palias table

---
+++ misc/sql-scraps/assign_tax_id.sql
@@ -0,0 +1,26 @@
+UPDATE palias SET tax_id=gs2tax_id('HUMAN') 
+	WHERE porigin_id=porigin_id_lookup('Swiss-Prot'::text) AND tax_id IS NULL AND alias ~ '_HUMAN$'::text;
+
+UPDATE palias SET tax_id=gs2tax_id('MOUSE') 
+	WHERE porigin_id=porigin_id_lookup('Swiss-Prot'::text) AND tax_id IS NULL AND alias ~ '_MOUSE$'::text;
+
+UPDATE palias SET tax_id=gs2tax_id('RAT') 
+	WHERE porigin_id=porigin_id_lookup('Swiss-Prot'::text) AND tax_id IS NULL AND alias ~ '_RAT$'::text;
+
+UPDATE palias SET tax_id=gs2tax_id('HUMAN') 
+	WHERE porigin_id=porigin_id_lookup('Refseq'::text) AND descr ~ '[[]Homo sapiens[]]$'::text AND tax_id IS NULL;
+
+UPDATE palias SET tax_id=gs2tax_id('MOUSE') 
+      WHERE porigin_id=porigin_id_lookup('Refseq'::text) AND descr ~ '[[]Mus musculus[]]$'::text AND tax_id IS NULL;
+
+UPDATE palias SET tax_id=gs2tax_id('RAT') 
+      WHERE porigin_id=porigin_id_lookup('Refseq'::text) AND descr ~ '[[]Rattus norvegicus[]]$'::text AND tax_id IS NULL;
+
+UPDATE palias SET tax_id=gs2tax_id('HUMAN') 
+	WHERE porigin_id=porigin_id_lookup('SPDI'::text) AND descr ~ '^Human'::text AND tax_id IS NULL;
+
+UPDATE palias SET tax_id=gs2tax_id('MOUSE') 
+      WHERE porigin_id=porigin_id_lookup('SPDI'::text) AND descr ~ '^Mouse'::text AND tax_id IS NULL;
+
+UPDATE palias SET tax_id=gs2tax_id('RAT') 
+      WHERE porigin_id=porigin_id_lookup('SPDI'::text) AND descr ~ '^Rat'::text AND tax_id IS NULL;
