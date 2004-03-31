135d6900446b932043db1e525b61df0b7051ae01
cavs <cavs@localhost>
2004-03-31 17:56:01 +0000

    dropped v_paprospect_sf, v_paprospect_dm, v_paprospect_cf views. rewrote v_paprospect2_scop to use paprospect2_scop table

--- misc/sql-scraps/v_paprospect.sql
+++ misc/sql-scraps/v_paprospect.sql
@@ -1,51 +1,23 @@
--- new paprospect views
--- updated for name change (from p2thread) and rename of p2params_id->run_id
-
+-- -----------------------------------------------------------------------------
+-- NAME: v_paprospect_scop.sql
+-- PURPOSE: build v_paprospect2_scop view
+-- 
+-- $Id$
+-- -----------------------------------------------------------------------------
 
 SET search_path = unison;
 
-CREATE VIEW v_paprospect2_sf AS
-    SELECT t.pseq_id, t.run_id, b.name, t.raw, t.svm, t.mutation, t.pairwise,
-t.singleton, t.gap, des.descr AS "SCOP superfamily" FROM (((paprospect2 t
-JOIN p2template b ON ((t.pmodel_id = b.pmodel_id))) LEFT JOIN scop.cla ON
-((b.sunid = cla.sunid))) LEFT JOIN scop.des ON ((des.sunid = cla.sf)))
-ORDER BY t.svm DESC;
-
-REVOKE ALL ON TABLE v_paprospect2_sf FROM PUBLIC;
-GRANT SELECT ON TABLE v_paprospect2_sf TO PUBLIC;
-
-
-CREATE VIEW v_paprospect2_fa AS
-    SELECT t.pseq_id, t.run_id, b.name, t.raw, t.svm, t.mutation, t.pairwise,
-t.singleton, t.gap, des.descr AS "SCOP family" FROM (((paprospect2 t JOIN
-p2template b ON ((t.pmodel_id = b.pmodel_id))) LEFT JOIN scop.cla ON
-((b.sunid = cla.sunid))) LEFT JOIN scop.des ON ((des.sunid = cla.fa)))
-ORDER BY t.svm DESC;
-
-REVOKE ALL ON TABLE v_paprospect2_fa FROM PUBLIC;
-GRANT SELECT ON TABLE v_paprospect2_fa TO PUBLIC;
-
-
-CREATE VIEW v_paprospect2_cf AS
-    SELECT t.pseq_id, t.run_id, b.name, t.raw, t.svm, t.mutation, t.pairwise,
-t.singleton, t.gap, des.descr AS "SCOP fold" FROM (((paprospect2 t JOIN
-p2template b ON ((t.pmodel_id = b.pmodel_id))) LEFT JOIN scop.cla ON
-((b.sunid = cla.sunid))) LEFT JOIN scop.des ON ((des.sunid = cla.cf)))
-ORDER BY t.svm DESC;
-
-REVOKE ALL ON TABLE v_paprospect2_cf FROM PUBLIC;
-GRANT SELECT ON TABLE v_paprospect2_cf TO PUBLIC;
-
-
+DROP VIEW v_paprospect2_scop;
 CREATE VIEW v_paprospect2_scop AS
-SELECT t.pseq_id, t.run_id, b.name, t.raw, t.svm, t.mutation,
-t.pairwise, t.singleton, t.gap, ((((cfdes.descr || ' / '::text) ||
-sfdes.descr) || ' / '::text) || fades.descr) AS "SCOP fold class /
-superfamily / family" FROM (((((paprospect2 t JOIN p2template b ON
-((t.pmodel_id = b.pmodel_id))) LEFT JOIN scop.cla ON ((b.sunid =
-cla.sunid))) LEFT JOIN scop.des cfdes ON ((cfdes.sunid = cla.cf))) LEFT
-JOIN scop.des sfdes ON ((sfdes.sunid = cla.sf))) LEFT JOIN scop.des fades
-ON ((fades.sunid = cla.fa))) ORDER BY t.svm DESC;
+ SELECT t.pseq_id, t.params_id, m.acc, t."start", t.stop, t.nident, round(t.nident::numeric / (t.stop - t."start" + 1)::numeric * 100.0, 0) AS "%ide", t.raw::integer AS raw, round(t.svm::numeric, 1) AS svm, round(t.mutation::numeric, 0) AS mutation, round(t.pairwise::numeric, 0) AS pairwise, round(t.singleton::numeric, 0) AS singleton, round(t.gap::numeric, 0) AS gap, cla.cl AS clid, des1.descr AS clname, cla.sf AS sfid, des2.descr AS sfname, cla.dm AS dmid, des3.descr as dmname
+   FROM paprospect2 t
+   JOIN unison.pmprospect2_scop b ON t.pmodel_id = b.pmodel_id
+   JOIN unison.pmprospect2 m ON t.pmodel_id = m.pmodel_id
+   LEFT JOIN scop.cla ON b.sunid = cla.sunid
+   LEFT JOIN scop.des des1 ON cla.cl = des1.sunid
+   LEFT JOIN scop.des des2 ON cla.sf = des2.sunid
+   LEFT JOIN scop.des des3 ON cla.dm = des3.sunid
+  ORDER BY t.pseq_id ASC,t.svm DESC;
 
 REVOKE ALL ON TABLE v_paprospect2_scop FROM PUBLIC;
 GRANT SELECT ON TABLE v_paprospect2_scop TO PUBLIC;
