b88a0eb131b601f917924b6e09281bb1eddd1676
cavs <cavs@localhost>
2004-04-01 19:06:06 +0000

    renamed %ide to pct_ident - the % caused too many issues

--- misc/sql-scraps/v_paprospect.sql
+++ misc/sql-scraps/v_paprospect.sql
@@ -2,14 +2,14 @@
 -- NAME: v_paprospect_scop.sql
 -- PURPOSE: build v_paprospect2_scop view
 -- 
--- $Id$
+-- $Id: v_paprospect.sql,v 1.2 2004/03/31 17:56:01 cavs Exp $
 -- -----------------------------------------------------------------------------
 
 SET search_path = unison;
 
 DROP VIEW v_paprospect2_scop;
 CREATE VIEW v_paprospect2_scop AS
- SELECT t.pseq_id, t.params_id, m.acc, t."start", t.stop, t.nident, round(t.nident::numeric / (t.stop - t."start" + 1)::numeric * 100.0, 0) AS "%ide", t.raw::integer AS raw, round(t.svm::numeric, 1) AS svm, round(t.mutation::numeric, 0) AS mutation, round(t.pairwise::numeric, 0) AS pairwise, round(t.singleton::numeric, 0) AS singleton, round(t.gap::numeric, 0) AS gap, cla.cl AS clid, des1.descr AS clname, cla.sf AS sfid, des2.descr AS sfname, cla.dm AS dmid, des3.descr as dmname
+ SELECT t.pseq_id, t.params_id, m.acc, t."start", t.stop, t.nident, round(t.nident::numeric / (t.stop - t."start" + 1)::numeric * 100.0, 0) AS "pct_ident", t.raw::integer AS raw, round(t.svm::numeric, 1) AS svm, round(t.mutation::numeric, 0) AS mutation, round(t.pairwise::numeric, 0) AS pairwise, round(t.singleton::numeric, 0) AS singleton, round(t.gap::numeric, 0) AS gap, cla.cl AS clid, des1.descr AS clname, cla.sf AS sfid, des2.descr AS sfname, cla.dm AS dmid, des3.descr as dmname
    FROM paprospect2 t
    JOIN unison.pmprospect2_scop b ON t.pmodel_id = b.pmodel_id
    JOIN unison.pmprospect2 m ON t.pmodel_id = m.pmodel_id
