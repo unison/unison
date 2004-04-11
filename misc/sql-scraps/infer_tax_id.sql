d4d95b5c062686b1f7097abd33535fd9403a2047
rkh <rkh@localhost>
2004-04-11 00:42:20 +0000

    * added ncbi and ProAnno v1 origin handling

--- misc/sql-scraps/infer_tax_id.sql
+++ misc/sql-scraps/infer_tax_id.sql
@@ -5,6 +5,20 @@
 
 
 
+create or replace function _refseq_species(text) returns text
+immutable strict language sql as 
+'select compbio.plregex($1,''/^.+\\\\[(.+)\\\\]/$1/''::TEXT)';
+
+create or replace function _nr_species(text) returns text
+immutable strict language sql as 
+'select compbio.plregex($1,''/^.+\\\\[(.+)\\\\].*/$1/''::TEXT)';
+
+create or replace function _dblast_species(text) returns text
+immutable strict language sql as 
+'select compbio.plregex($1,''/^.* - (.+?)\\\\.?\\$/$1/''::TEXT)';
+
+
+
 create or replace function infer_tax_id(text,text,text) returns integer 
 immutable strict
 language sql as '
@@ -18,7 +32,7 @@ SELECT CASE
                 ''hum'',
                 ''Incyte'',
                 ''TNF6F'',
-                ''GGI'', ''GGI-subopt'',
+                ''GGI'', ''GGI-subopt'', ''ProAnno v1'',
                 ''Nick'')
     THEN
         gs2tax_id(''HUMAN'')
@@ -69,13 +83,28 @@ SELECT CASE
             WHEN ($3 ~ ''/species=mus'')        	THEN gs2tax_id(''MOUSE'')
         END)
 
+    WHEN ($1 = ''ncbi'')
+    THEN
+        (SELECT tax_id FROM tax.spspec WHERE UPPER(latin) = UPPER(_refseq_species($3)))
+
     WHEN ($1 = ''Refseq'')
     THEN
-        (SELECT tax_id FROM tax.spspec WHERE UPPER(latin) = UPPER(compbio.plregex($3,''/^.+\\\\[(.+)\\\\]/$1/''::TEXT)))
+        (SELECT tax_id FROM tax.spspec WHERE UPPER(latin) = UPPER(_refseq_species($3)))
+
+    WHEN ($1 = ''nr'')
+    THEN
+        (SELECT tax_id FROM tax.spspec WHERE UPPER(latin) = UPPER(_nr_species($3)))
 
     WHEN ($1 = ''dblast'')
     THEN
-		(SELECT tax_id FROM tax.spspec WHERE UPPER(latin) = UPPER(compbio.plregex($3,''/^.* - (.+)/$1/''::TEXT)))
+        (SELECT CASE
+	        WHEN ($2 ~ ''_'' AND gs2tax_id(substr($2,strpos($2,''_'')+1)) IS NOT NULL)
+							 					 	THEN gs2tax_id( substr($2,strpos($2,''_'')+1) )
+            WHEN ($3 ~ '' - human'')   		     	THEN gs2tax_id(''HUMAN'')
+            WHEN ($3 ~ '' - mouse'')   		     	THEN gs2tax_id(''MOUSE'')
+            WHEN ($3 ~ '' - rat'')   		     	THEN gs2tax_id(''RAT'')
+			ELSE (SELECT tax_id FROM tax.spspec WHERE UPPER(latin) = UPPER(_dblast_species($3)))
+        END)
 
 END;';
 
