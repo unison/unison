2a3469fb79a23c3a9d74d92901e0834bafe47286
cavs <cavs@localhost>
2004-04-19 19:20:47 +0000

    Added insert permission on pnote to public

--- misc/sql-scraps/create_pnote.sql
+++ misc/sql-scraps/create_pnote.sql
@@ -2,7 +2,7 @@
 -- NAME: create_pnote.sql
 -- PURPOSE: sql commands to build pnote tables and associated functions
 --
--- $Id$
+-- $Id: create_pnote.sql,v 1.1 2004/04/16 20:57:15 cavs Exp $
 -- -----------------------------------------------------------------------------
 
 CREATE TABLE pnote ( 
@@ -15,8 +15,8 @@ ALTER TABLE pnote
 	ADD constraint pseq_id_exists foreign key (pseq_id) REFERENCES pseq(pseq_id) ON UPDATE CASCADE ON DELETE CASCADE;
 CREATE INDEX pnote_pseq_id ON pnote USING btree (pseq_id);
 REVOKE ALL ON pnote FROM public;
-GRANT SELECT ON pnote TO public;
-GRANT INSERT,DELETE,UPDATE ON pnote TO loader;
+GRANT SELECT,INSERT ON pnote TO public;
+GRANT DELETE,UPDATE ON pnote TO loader;
 
 -- set the added and who fields automatically
 CREATE OR REPLACE FUNCTION pnote_iu_trigger () RETURNS "trigger" AS '
