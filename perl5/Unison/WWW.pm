e88cf0774409db38607610157f281e80c3b4ba32
rkh <rkh@localhost>
2004-04-21 21:13:49 +0000

    * added logging to /tmp/unison-<login>.log if it is a writable file

--- perl5/Unison/WWW.pm
+++ perl5/Unison/WWW.pm
@@ -3,11 +3,29 @@ use CBT::debug;
 CBT::debug::identify_file() if ($CBT::debug::trace_uses);
 
 
-# prepend the full path of ../perl5/ to @INC
-# this facilitates tree relocation / dev trees in user directories
-# To be safe, it's probably best to load this last.
-
 BEGIN {
+  # if this file exists and is writable, then we'll open it for logging.
+  # NOTE: the file must be writable by the web server, which DOES NOT run
+  # as remote user. Typically, do something like:
+  # $ touch /tmp/unison-rkh.log
+  # $ chmod a+w /tmp/unison-rkh.log
+  # to enable logging.
+  # THIS WILL SLOW THINGS DOWN... DON'T FORGET TO DELETE THE LOG!
+  my $log_fn = "/tmp/unison-$ENV{REMOTE_USER}.log";
+  if (-f $log_fn and -w $log_fn) {
+	close(STDERR);
+	if (not open(STDERR, ">>$log_fn")) {
+	  print("$log_fn: $!\n");
+	  exit(0);
+	}
+	$ENV{DEBUG} = 1;
+  }
+
+
+  # Prepend the full path of ../perl5/ to @INC
+  # This facilitates tree relocation / dev trees in user directories.
+  # To be safe, it's probably best to load Unison::WWW after non-Unison
+  # modules.
   $ENV{SCRIPT_FILENAME} = $0 unless (exists $ENV{SCRIPT_FILENAME});
   my ($dir) = $ENV{SCRIPT_FILENAME} =~ m%^(.*\/)%;
   $dir .= '/../perl5';
