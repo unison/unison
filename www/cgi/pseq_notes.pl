c3bce21468aed0e9906d9dcfba6ae476ce8aa532
Reece Hart <reecehart@gmail.com>
2004-04-21 22:50:22 +0000

    * sync with pnote column name change * highlight sort column * add readonly support

--- www/cgi/pseq_notes.pl
+++ www/cgi/pseq_notes.pl
@@ -15,29 +15,27 @@ my $sql;
 
 $p->ensure_required_params(qw(pseq_id));
 
-my $notify='';
+
 if ( defined $v->{action} && $v->{action} eq 'submit' ) {
   $sql = "insert into pnote (pseq_id,note) values ($v->{pseq_id},'$v->{note}')";
-  my $sth=$u->prepare($sql);
+  my $sth = $u->prepare($sql);
   $sth->execute();
   if ( defined $u->errstr ) {
-    $notify = "<FONT COLOR='red'>Unable to insert pnote: " . $u->errstr . "</FONT><P/>";
-  } else {
-    $notify = 'Inserted new note!';
+    $p->die('Unable to insert pnote: ',$u->errstr);
   }
 }
 
 
 $v->{sort} = 'added' if ! defined $v->{sort};
 my @cols = ( 
-  ['User',  'who'    ],
+  ['User',  'login'    ],
   ['Added', 'added'    ],
   ['Note',  ],
 );
-my $ob = { who => 'who', added => 'added desc', }->{$v->{sort}};
+my $ob = { login => 'login', added => 'added desc' }->{$v->{sort}};
 my @f = map { $_->[0] } @cols;
 my %colnum = map {$cols[$_]->[1] => $_} grep {defined $cols[$_]->[1]} 0..$#cols;
-$sql = qq/select who,added,note from pnote where pseq_id=$v->{pseq_id} order by $ob/;
+$sql = qq/select login,date_trunc('second',added) as "added",note from pnote where pseq_id=$v->{pseq_id} order by $ob/;
 my $ar;
 
 try {
@@ -53,29 +51,34 @@ my $hc;
 if (exists $colnum{$v->{sort}}) {
   $hc = $colnum{$v->{sort}};
 }
-                                                                                                                                                                          
+
 for(my $fi=0; $fi<=$#f; $fi++) {
   next if $fi == $hc;
   next unless defined $cols[$fi]->[1];
   $f[$fi] = sprintf("<a href=\"%s\">%s</a>",
-          $p->make_url({sort=>$cols[$fi]->[1]},qw(pseq_id params_id)),
+          $p->make_url({sort=>$cols[$fi]->[1]},qw(pseq_id)),
           $f[$fi]);
 }
 
 print $p->render("Notes for Unison:$v->{pseq_id}",
-         $p->best_annotation($v->{pseq_id}),
-         '<p>',
-         $notify,
-         $p->group("Notes",
-               Unison::WWW::Table::render(\@f,$ar)),
-         $p->sql($sql),
-         $p->group("Enter New Note",
-             "<FORM ACTION=",$p->make_url()," METHOD='POST'>" .
-             "<TEXTAREA NAME='note' ROWS='5' COLS='80'></TEXTAREA>" .
-             "<INPUT TYPE=HIDDEN NAME='pseq_id' VALUE='$v->{pseq_id}'>" .
-             "<INPUT TYPE=SUBMIT NAME='action' VALUE='submit'>" .
-             "</FORM>"),
-        );
+				 $p->best_annotation($v->{pseq_id}),
+				 '<p>',
+				 $p->group("Notes",
+						   Unison::WWW::Table::render(\@f,$ar,{highlight_column=>$hc})),
+				 '<p>',
+
+				 ( $p->{readonly}
+				   ? ''
+				   : $p->group("Enter New Note",
+							   "<FORM ACTION=",$p->make_url()," METHOD='POST'>" .
+							   "<TEXTAREA NAME='note' ROWS='5' COLS='60'></TEXTAREA>" .
+							   "<INPUT TYPE=HIDDEN NAME='pseq_id' VALUE='$v->{pseq_id}'>" .
+							   "<br><INPUT TYPE=SUBMIT NAME='action' VALUE='submit'>" .
+							   "</FORM>")
+				 ),
+
+				 $p->sql($sql),
+				);
 
 
 
