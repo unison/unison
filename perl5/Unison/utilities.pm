18751a356db83fa82f6c3712dffa087f4e755cb9
rkh <rkh@localhost>
2004-04-22 23:51:16 +0000

    merged utilities.pm (formerly deprecated.pm) with utils.pm

--- perl5/Unison/utilities.pm
+++ perl5/Unison/utilities.pm
@@ -1,19 +1,26 @@
 package Unison;
+use strict;
+use warnings;
+
 use CBT::debug;
 CBT::debug::identify_file() if ($CBT::debug::trace_uses);
 
-
-use strict;
-use warnings;
 use Carp qw(cluck);
 
 
 my %already_warned;
-
 sub warn_deprecated() {
   my @caller = caller(1);
   cluck("WARNING: deprecated function $caller[0] called\n") 
 	unless $already_warned{$caller[0]}++;
+}
+
+
+# convert an array of ranges to enumerated values
+# e.g., range_to_enum(qw(1 2 3..5)) returns (1,2,3,4,5)
+sub range_to_enum(@) {
+  my $rl = join(',',@_);
+  eval "$rl";
 }
 
 
