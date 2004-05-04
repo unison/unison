fef63cb830038c05fa13701e33147d8ab74a645e
rkh <rkh@localhost>
2004-05-04 21:19:54 +0000

    *** empty log message ***

--- www/cgi/t/test.sh
+++ www/cgi/t/test.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-# 
+
 # Simple test script which ensures that errors get sent to the browser
 # This should eventually be replaced by a more general test harness for
 # arbitrary scripts.
-#
+
+# A better approach might be to have a test harness which returns a
+# frameset which contains stdout in one frame and stderr in another.
+
 
 echo "Content-type: text/plain"
 echo
 
-exec `dirname $0`/about_env.pl  2>&1
+exec `dirname $0`/../about_env.pl  2>&1
