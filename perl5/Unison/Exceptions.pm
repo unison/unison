758168bdb22a628c19e90015c59ce09d24345381
rkh <rkh@localhost>
2004-05-04 04:53:45 +0000

    * added CBT::debug header * now provides try..catch..etc syntax to caller (only caller)

--- perl5/Unison/Exceptions.pm
+++ perl5/Unison/Exceptions.pm
@@ -1,15 +1,23 @@
-use Error qw(:try);
-
-
-package Unison::Exception;
 use CBT::debug;
 CBT::debug::identify_file() if ($CBT::debug::trace_uses);
+
+
+# Anyone using Unison::Exceptions will want the try/catch/...
+# syntax; import it for them just as 'use Error qw(:try)' would do
+package Unison::Exceptions;
+use base qw(Exporter);
+use Error qw(:try);
+@EXPORT = @Error::subs::EXPORT_OK;
+
+
+# Define a bunch of standard exceptions
+package Unison::Exception;
 use base 'CBT::Exception';
 
+our @EXCEPTIONS = qw(NotImplemented NotConnected ConnectionFailed BadUsage
+					RuntimeError DBIError);
 
-
-foreach my $subtype (qw(NotImplemented NotConnected ConnectionFailed
-                        BadUsage RuntimeError DBIError)) {
+foreach my $subtype (@EXCEPTIONS) {
   eval "package Unison::Exception::$subtype; use base 'Unison::Exception';";
 }
 
