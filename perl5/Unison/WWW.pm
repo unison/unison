236bdfdf1bb2af3e72f78b76c938358c5f4ecf2b
rkh <rkh@localhost>
2004-05-04 05:21:05 +0000

    * only open debug log if REMOTE_USER is set and log exists (previously   barfed when debugging from command line and R_U wasn't set

--- perl5/Unison/WWW.pm
+++ perl5/Unison/WWW.pm
@@ -5,7 +5,7 @@ CBT::debug::identify_file() if ($CBT::debug::trace_uses);
 use strict;
 use warnings;
 
-our ($RELEASE) = q$Name: bar $ =~ m/Name:\s+(\S*)\s+/;
+our ($RELEASE) = q$Name:  $ =~ m/Name:\s+(\S*)\s+/;
 
 
 BEGIN {
@@ -16,14 +16,16 @@ BEGIN {
   # $ chmod a+w /tmp/unison-rkh.log
   # to enable logging.
   # THIS WILL SLOW THINGS DOWN... DON'T FORGET TO DELETE THE LOG!
-  my $log_fn = "/tmp/unison-$ENV{REMOTE_USER}.log";
-  if (-f $log_fn and -w $log_fn) {
-	close(STDERR);
-	if (not open(STDERR, ">>$log_fn")) {
-	  print("$log_fn: $!\n");
-	  exit(0);
+  if (exists $ENV{REMOTE_USER}) {
+	my $log_fn = "/tmp/unison-$ENV{REMOTE_USER}.log";
+	if (-f $log_fn and -w $log_fn) {
+	  close(STDERR);
+	  if (not open(STDERR, ">>$log_fn")) {
+		print("$log_fn: $!\n");
+		exit(0);
+	  }
+	  $ENV{DEBUG} = 1;
 	}
-	$ENV{DEBUG} = 1;
   }
 }
 
