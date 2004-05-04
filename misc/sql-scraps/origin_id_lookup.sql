78df10babdea77c1f4577a910911b598deba6493
Reece Hart <reecehart@gmail.com>
2004-05-04 04:34:41 +0000

    * deprecated porigin_id_lookup

--- misc/sql-scraps/origin_id_lookup.sql
+++ misc/sql-scraps/origin_id_lookup.sql
@@ -1,9 +1,8 @@
 create or replace function porigin_id_lookup (text)
 returns integer
 language plpgsql as '
-DECLARE
-	rv integer;
 BEGIN
-	select into rv porigin_id from porigin where origin = $1;
-	return rv;
+	raise warning ''porigin_id_lookup(text) deprecated; use porigin_id(text) instead'';
+	return porigin_id($1);
 END;';
+
