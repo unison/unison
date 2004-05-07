4f34fd8f968ee6cbb7db10dbc89b23f90f0135ca
rkh <rkh@localhost>
2004-05-07 21:08:10 +0000

    * improved test for cases with multiple calls from identical locationes   (i.e., in loops) versus multiple calls from distinct locations.

--- perl5/Unison/t/deprecated.pl
+++ perl5/Unison/t/deprecated.pl
@@ -5,13 +5,63 @@ use warnings;
 use Unison::utilities;
 
 
-sub level2 {
+# A: single use, one level deep
+sub A1 {
   Unison::warn_deprecated();
 }
 
-sub level1 {
-  level2();
+
+# B: single use, two levels deep
+sub B2 {
+  Unison::warn_deprecated();
+}
+sub B1 {
+  B2()
 }
 
 
-level1();
+# C: multiple uses in one location
+sub C2 {
+  Unison::warn_deprecated();
+}
+sub C1 {
+  for(my $i=0; $i<=3; $i++) {
+	C2();
+  }
+}
+
+
+
+# D: multiple uses in one function
+sub D2 {
+  Unison::warn_deprecated();
+}
+sub D1 {
+  for(my $i=0; $i<=3; $i++) {
+	D2();
+	D2();
+  }
+}
+
+
+
+
+print(STDERR "# A1():\n");
+A1();
+
+print(STDERR "\n# B1():\n");
+B1();
+
+print(STDERR "\n# C1():\n");
+C1();
+
+print(STDERR "\n# D1():\n");
+D1();
+
+
+exit(0);
+
+
+
+
+
