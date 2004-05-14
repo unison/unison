8dcb00f7b4fba1420d70c6cce880400604584f85
rkh <rkh@localhost>
2004-05-14 20:34:05 +0000

    documentation added -- still incomplete

--- perl5/Unison.pm
+++ perl5/Unison.pm
@@ -1,7 +1,8 @@
 =head1 NAME
 
 Unison -- interface to the Unison database
-S<$Id: Unison.pm,v 1.1 2004/04/30 22:36:50 rkh Exp $>
+
+S<$Id: Unison.pm,v 1.10 2004/05/04 05:04:37 rkh Exp $>
 
 =head1 SYNOPSIS
 
@@ -9,18 +10,35 @@ use Unison;
 
 =head1 DESCRIPTION
 
-B<> is a
-
-=head1 ROUTINES & METHODS
-
-=cut
-
-
-=pod
+B<Unison::> is the perl API to the Unison database.  This module,
+B<Unison.pm>, loads the following commonly-used submodules in the Unison::
+API.  Please see these submodules for documention.
 
 =over
 
-=item
+=item Unison::utilities;
+
+=item Unison::DBI;
+
+=item Unison::Exceptions;
+
+=item Unison::pseq;
+
+=item Unison::porigin;
+
+=item Unison::paprospect2;
+
+=item Unison::pmprospect2;
+
+=item Unison::palias;
+
+=item Unison::papseq;
+
+=item Unison::params;
+
+=item Unison::run_history;
+
+=item Unison::userprefs;
 
 d28 1
 
@@ -34,7 +52,7 @@ CBT::debug::identify_file() if ($CBT::debug::trace_uses);
 use strict;
 use warnings;
 
-our ($RELEASE) = q$Name: foo $ =~ m/Name:\s+(\S*)\s+/;
+our ($RELEASE) = q$Name:  $ =~ m/Name:\s+(\S*)\s+/;
 if (q$Name:  $ =~ m/Name:\s+rel_(\S*)\s+/) { ($RELEASE = $1) =~ s/-/./g; }
 use Unison::common;
 use Unison::utilities;
@@ -55,15 +73,11 @@ use Unison::userprefs;
 =over
 =pod
 =back
-=head1 BUGS
-
-=head1 SEE ALSO
-
 
 =head1 AUTHOR
  Reece Hart, Ph.D.                     rkh@gene.com, http://www.gene.com/
  Genentech, Inc.                       650/225-6133 (voice), -5389 (fax)
- Bioinformatics Department             
+ Bioinformatics Department
  1 DNA Way, MS-93                      http://www.in-machina.com/~reece/
  South San Francisco, CA  94080-4990   reece@in-machina.com, GPG: 0x25EC91A0
  South San Francisco, CA  94080-4990    reece@harts.net, GPG: 0x25EC91A0
