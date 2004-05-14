9f9b4870331051945d8e67cf685b6e5e639c5933
rkh <rkh@localhost>
2004-05-14 20:33:19 +0000

    initial

---
+++ perl5/Unison/t/range_to_enum.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+
+use Unison::utilities;
+
+print("This should print 1,2,3,4,5,6,7,8,9,10: \n");
+print("result: ",
+	  join(",",Unison::range_to_enum("1..3","4,5,6..10")), "\n" );
+
