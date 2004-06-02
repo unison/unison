4153961073ac3fa139c7314fb4dfb3043e647d4f
Reece Hart <reecehart@gmail.com>
2004-06-02 23:06:04 +0000

    * moved process_stream and process_seq back into load-pseq

--- perl5/Unison/pseq.pm
+++ perl5/Unison/pseq.pm
@@ -1,7 +1,7 @@
 =head1 NAME
 
 Unison::pseq -- Unison pseq table utilities
-S<$Id: pseq.pm,v 1.15 2004/05/14 00:16:32 rkh Exp $>
+S<$Id: pseq.pm,v 1.16 2004/05/14 20:39:53 rkh Exp $>
 
 =head1 SYNOPSIS
 
@@ -28,9 +28,6 @@ CBT::debug::identify_file() if ($CBT::debug::trace_uses);
 
 use strict;
 use warnings;
-use vars qw( %alias %md5 );
-use Digest::MD5  qw(md5_hex);
-use Unison::utilities qw(clean_sequence);
 
 
 sub pseq_si_pseq_id {
@@ -212,178 +209,6 @@ return the pseq_id for a given sequence
 
 
 
-
-#-------------------------------------------------------------------------------
-# process_stream()
-#-------------------------------------------------------------------------------
-
-=head2 process_stream()
-
- NAME: process_stream
- PURPOSE: parse Bio::SeqIO stream and load sequences into Unison
- ARGUMENTS: Bio::SeqIO object, option hashref:
-'origin' => name of porigin (REQUIRED)
-  'start-after' => skip seqs until this accession
-  'sql-only' => boolean for sql output only (no loading in Unison)
-  'incl-subex' => boolean for whether to include subex gene predictions
-  'verbose' => boolean for whether to output more information
- RETURNS: hash with process info keys: nseen, nskipped, nadded, nfailed
-
-=cut
-
-sub process_stream {
-  my ($u,$in,$opts) = @_;
-  my %rv = ( nseen => 0, nskipped => 0, nfailed => 0, nadded => 0, naliases => 0 );
-  $opts->{porigin_id} = $u->porigin_si_porigin_id($opts->{origin});
-
-  # build a hash of tax_ids in tax.spspec only load tax_id if found in this
-  # hash.  hack to handle the fact that swissprot doesn't have all of the 
-  # tax_ids in their schema.
-  my %tax_id = map {$_->[0],1} @{$u->selectall_arrayref( 'select distinct tax_id from tax.spspec' )};
-
-  while ( my $bs = $in->next_seq() ) {
-	try {
-	  $u->process_seq($bs,$opts,\%rv,\%tax_id);
-	} catch Unison::Exception with {
-	  warn($_[0]);
-	};
-  }
-  return(\%rv);
-}
-
-
-#-------------------------------------------------------------------------------
-# process_seq()
-#-------------------------------------------------------------------------------
-
-=head2 process_seq()
-
- NAME: process_seq
- PURPOSE: parse Bio::Seq object and load seq and alia into Unison
- ARGUMENTS: Bio::Seq object, option hashref, process info hashref (keys:
-  nseen, nskipped, nadded), hashref of allowable tax_ids
- NOTES: sequences loaded into the "refseq" origin are loaded with sequence
-		version numbers; all others origins ignore sequence version, if any.
- RETURNS: nada
-
-=cut
-
-sub process_seq  {
-  my ($u,$bs,$opts,$rv,$tax_id_allowed) = @_;
-  my $id = $bs->display_id();
-  my $seq = $bs->seq();
-
-  (defined $seq)
-	|| throw Unison::Exception::RuntimeError("$id: sequence not defined");
-
-  my $tax_id;
-  my $descr = $bs->desc();
-  $seq = clean_sequence($seq);
-  my $md5 = &md5_hex($seq);
-  $rv->{nseen}++;
-
-  # description reformatting
-  $descr = '' unless defined $descr;
-  $descr =~ s/\s{2,}/ /g;
-  $descr =~ s/^\s+//;
-  $descr =~ s/\s+$//;
-
-  if ($opts->{origin} =~ m/spdi/i) {
-	$descr =~ s/\[(?:min|full)\]\s+//;
-	$descr =~ s/\# converted.+//;
-  } elsif ($opts->{origin} =~ m/refseq/i) {
-	my $v = $bs->seq_version();
-	if (defined $v) {
-	  $id .= ".$v";
-	}
-  }
-
-  # skip sequences in various conditions
-  my $skip;
-  if ($id !~ m/\w/) {
-    $skip = "doesn't look like a valid sequence id"; 
-  } elsif (defined $opts->{'start-after'}) {
-    $skip = "haven't reached $opts->{'start-after'} yet";
-    undef $opts->{'start-after'} if ($id eq $opts->{'start-after'});
-  } elsif (%alias and exists $alias{$id}) {
-    $skip = 'extant alias';
-  } elsif (length($seq) == 0) {
-    $skip = "zero-length";
-  } elsif (!$opts->{'incl-subex'} and $descr =~ m%/type=(\w+)% and $1 ne 'gene') {
-    $skip = "non-gene genescan transcript";
-  }
-  if (defined $skip) {
-    warn("# skipping $id: $skip ($descr)\n") if $opts->{verbose};
-    $rv->{nskipped}++;
-    return;
-  }
-
-  # @ids is the SET of ids to which we'll link this sequence
-  my @ids = ();
-
-  if ($opts->{origin} =~ m/SPDI/i) {
-    my %ids;
-    $ids{$1}++ if $descr =~ s/(PRO\d+)\s+//;
-    # Don't put UNQs or DNAs in database -- unqs aren't unique and dnas
-    # are proteins
-	# $ids{$1}++ if $descr =~ s/(UNQ\d+)\s+//;
-    # $ids{$1}++ if $descr =~ s/(DNA\d+)\s+//;
-    # $ids{$id}++;
-    @ids = sort keys %ids;
-  } else {
-    @ids = ( $id );
-  }
-
-  if ($opts->{'sql-only'}) {
-    printf("insert into pseq (seq,len) values ('$seq',%d);\n",
-		   length($seq)); return 1;
-  }
-
-  # select/insert sequences, then link aliases
-  my $pseq_id;
-  my $frommd5='';
-  if (%md5 and defined $md5{ $md5 }) {
-    $pseq_id = $md5{ $md5 };
-    $frommd5='*';
-  } else {
-	try {
-	  $pseq_id = $u->pseq_si_pseq_id( $seq );
-	} catch Unison::Exception with {
-	  $rv->{nfailed}++;
-	  throw Unison::Exception::RuntimeError( "Failed to load sequence $ids[0] ($descr)",
-											 $_[0] );
-	};
-    $md5{ $md5 } = $pseq_id;
-	$rv->{nadded}++;
-  }
-
-
-  # see if there's species info and make sure that the tax_id is in the set of allowable
-  # tax_ids (see description of process_stream method for more info).  if so, get the 
-  # tax_id to insert with the # alias
-  if (defined $bs->species && defined $bs->species->ncbi_taxid() && 
-	  defined $tax_id_allowed->{$bs->species->ncbi_taxid()} ) {
-    $tax_id = $bs->species->ncbi_taxid();
-  } else {
-    $tax_id = 'NULL';
-  }
-
-  foreach my $upd_id (@ids)  {
-	try {
-	  $u->add_palias($pseq_id,$opts->{porigin_id},$upd_id,$descr,$tax_id);
-	} catch Unison::Exception with {
-	  $rv->{nfailed}++;
-	  throw Unison::Exception::RuntimeError( "Failed to load one or more aliases for $pseq_id",
-											 $_[0] );
-	};
-	$rv->{naliases}++;
-  }
-
-  printf(STDERR "## added pseq_id=$pseq_id$frommd5, len=%d, aliases={@ids}, descr=%s\n",
-		 length($seq), $descr) if $opts->{verbose};
-
-  return;
-}
 
 
 ### DEPRECATED FUNCTIONS
