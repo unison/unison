42caf34a83bbcf1798248368368eca0b3c6a4f46
rkh <rkh@localhost>
2005-04-04 16:41:42 +0000

    initial

---
+++ perl5/Unison/t/unused/params_prospect_drop.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Unison;
+use Data::Dumper;
+
+my $u = new Unison( dbname=>'csb-dev' );
+
+while( my $id = shift ) {
+  my $o = $u->get_p2options_by_params_id( $id );
+  print(Dumper($o),"\n");
+}
