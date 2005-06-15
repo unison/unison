5df6de52eb9bc7333ef01ea071fbb7a9a3f244aa
Reece Hart <reecehart@gmail.com>
2005-06-15 20:17:34 +0000

    initial

---
+++ misc/sql-scraps/dv_pset_extrinsics.sql
@@ -0,0 +1,25 @@
+-- "extrinsics" means sets of things that I compute on (as opposed to
+-- "intrinsic" data loaded with the sequences themselves)
+
+CREATE OR REPLACE VIEW dv_pset_BLAST as
+SELECT pseq_id FROM pseqset where pset_id=pset_id('uniA')
+UNION SELECT A.pseq_id FROM v_current_annotations A WHERE A.porigin_id in (porigin_id('PDB'));
+
+CREATE OR REPLACE VIEW dv_pset_uptodate_cheap as
+SELECT 
+
+CREATE OR REPLACE VIEW dv_pset_uptodate_moderate as
+SELECT 
+
+CREATE OR REPLACE VIEW dv_pset_uptodate_expensive as
+SELECT 
+
+
+grant select on dv_pset_BLAST to PUBLIC;
+comment on view dv_pset_BLAST is 'defining view for pset BLAST';
+
+grant select on dv_pset_BLAST to PUBLIC;
+comment on view dv_pset_BLAST is 'defining view for pset BLAST';
+
+
+
