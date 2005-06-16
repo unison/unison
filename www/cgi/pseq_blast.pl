63f223a73671e6c74d7a5a57409f8fbec2b8ea0c
rkh <rkh@localhost>
2005-06-16 06:03:21 +0000

    uses new v_papseq and reformatted query for legibility

--- www/cgi/pseq_blast.pl
+++ www/cgi/pseq_blast.pl
@@ -15,12 +15,15 @@ my $p = new Unison::WWW::Page;
 my $u = $p->{unison};
 my $v = $p->Vars();
 
-my $sql = qq/select target,best_annotation(target),qstart||'-'||qstop,tstart||'-'||tstop,
-			len,ident,sim,gaps,eval,
-			pct_ident::int,pct_hsp_coverage::int,pct_coverage::int
-			from v_papseq where
-			q_pseq_id=$v->{pseq_id}
-			order by pct_ident desc,len desc,eval/;
+my $sql = <<EOSQL;
+SELECT t_pseq_id,best_annotation(t_pseq_id),
+  q_start||'-'||q_stop,t_start||'-'||t_stop,
+  len,ident,sim,gaps,eval,pct_ident::int,
+  pct_hsp_coverage::int,pct_coverage::int
+FROM v_papseq
+WHERE q_pseq_id=$v->{pseq_id}
+ORDER BY pct_ident desc,len desc,eval
+EOSQL
 
 my $ar = $u->selectall_arrayref($sql) ;
 splice(@$_,0,2,mk_palias_link($_->[0],$_->[1])) for @$ar;
