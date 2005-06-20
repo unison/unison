0d0cd2faf393c3e84add5810bff6d00688f79aee
Kiran Mukhyala <mukhyala@gmail.com>
2005-06-20 17:23:17 +0000

    calls ../t/output_test and formats results

---
+++ www/cgi/cgi-test.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+$Id$
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/../perl5", "$FindBin::Bin/../../perl5", "$FindBin::Bin/../../../perl5";
+
+use Unison;
+use Unison::WWW;
+use Unison::WWW::Page qw(infer_pseq_id);
+use Unison::WWW::Table;
+
+my $p = new Unison::WWW::Page();
+my $v = $p->Vars();
+
+$p->ensure_required_params( qw( pseq_id ) );
+$p->add_footer_lines('$Id$ ');
+
+my ($test_fh, $test_fn) = $p->tempfile(SUFFIX=>'.tst');
+my $cmd = "../t/output_test -q $v->{pseq_id} >> $test_fn";
+
+system( $cmd )
+  && $p->die("$cmd: $!\n");
+
+my @lines = split(/\n/,`cat $test_fn`);
+my (@f,@ar);
+foreach my $l (0..$#lines) {
+  $lines[$l] =~ s/\.+//;
+  $lines[$l] =~ s/://;
+  $lines[$l] =~ s/PASSED/\<FONT COLOR="green"\>PASSED\<\/FONT\>/;
+  $lines[$l] =~ s/FAILED/\<FONT COLOR="red"\>FAILED\<\/FONT\>/;
+
+  if($l == 0) {
+    @ar= (split(/\t+/,$lines[$l]));
+  }
+  else {
+    push @f,[(split(/\t+/,$lines[$l]))];
+  }
+}
+
+print $p->render("Unison : $v->{pseq_id}",
+		 $p->group("Test Results",
+			   Unison::WWW::Table::render(\@ar,\@f)),
+		);
+
+exit(0);
+
+
