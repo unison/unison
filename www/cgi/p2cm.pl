8521ce1bbc0d5cb18726cc8548c337911b3d0583
mukhyala <mukhyala@localhost>
2005-07-22 22:20:08 +0000

    suppot for threading with ssp

--- www/cgi/p2cm.pl
+++ www/cgi/p2cm.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use FindBin;
-use lib "$FindBin::Bin/../perl5", "$FindBin::Bin/../perl5-prereq", "$FindBin::Bin/../../perl5", "$FindBin::Bin/../../../perl5";
+use lib "$FindBin::Bin/../perl5", "$FindBin::Bin/../perl5-prereq", "$FindBin::Bin/../../perl5";
 
 use Error qw(:try);
 
@@ -13,9 +13,12 @@ use Unison::WWW;
 use Unison::WWW::Page;
 use Unison::Jmol;
 
+use Unison::Utilities::pfssp_psipred;
+
 use Bio::Prospect::Options;
 use Bio::Prospect::LocalClient;
 use Bio::Prospect::Exceptions;
+use Bio::Prospect::Align;
 
 my $p = new Unison::WWW::Page;
 my $u = $p->{unison};
@@ -37,10 +40,18 @@ if (not defined $po) {
 $po->{templates} = \@templates;
 $po->{"3d"} = 1;
 
+if ($po->{'phd'}) {
+  my $ssp = Unison::Utilities::pfssp_psipred::ssp_phd($po->{'phd'},$v->{pseq_id},$u);
+  if(not defined($ssp)) {
+    $p->die("no psipred with params_id=$v->{params_id}; \n");
+  }
+  $po->{'phd'} = $ssp;
+}
 try {
   my $pf = new Bio::Prospect::LocalClient( {options=>$po} );
 
-  my $thr = ($pf->thread( $seq ))[0];
+  my @threads = $pf->thread( $seq );
+  my $thr=$threads[0];
 
   if($v->{viewer} eq 'rasmol') {
     print("Content-type: application/x-rasmol\n",
@@ -62,12 +73,18 @@ try {
 	 );
   }
   elsif($v->{viewer} eq 'jmol') {
+
+    my $pa = new Bio::Prospect::Align( -debug=>0,-threads => \@threads);
+
       my $jmol = new Unison::Jmol(600,300);
       $p->add_html($jmol->script_header());
       print $p->render("Threading model for Unison:$v->{pseq_id}",
 		       $p->best_annotation($v->{pseq_id}),
 		       $jmol->initialize_inline($thr->output_jmol_script),
-		       "<center>Threading alignment : Identities are colored <font color=blue>blue</font>, similarities are colored <font color=cyan>cyan</font> and mismatches are colored <font color=red>red</font>. Insertions are shown in <font color=green>>number of insertions<</font>. Deletions are colored <font color=grey>grey</font>. All Cysteines are colored <font color=yellow>yellow</font> and conserved cysteines are spacefilled.</center>"
+		       "<center>Threading alignment : Identities are colored <font color=blue>blue</font>, similarities are colored <font color=cyan>cyan</font> and mismatches are colored <font color=red>red</font>. Insertions are shown in <font color=green>>number of insertions<</font>. Deletions are colored <font color=grey>grey</font>. All Cysteines are colored <font color=yellow>yellow</font> and conserved cysteines are spacefilled.</center>",
+		       '<p>',
+		       $p->group('Prospect2 Threading Alignment',
+		       '<b>', $pa->get_alignment(-format=>'html'), '</b>')
 		      );
 
     }
