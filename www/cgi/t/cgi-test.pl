13a1b79aec0ebf7ef473231ae5365909d8b54e53
mukhyala <mukhyala@localhost>
2005-07-28 18:29:59 +0000

    fix command line version, cleanup, add scripts to test

--- www/cgi/t/cgi-test.pl
+++ www/cgi/t/cgi-test.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 # cgi-test -- test Unison cgis
 # You must be sitting in the CGI directory you wish to test.
-# $Id: cgi-test.pl,v 1.6 2005/07/27 18:37:06 rkh Exp $
+# $Id: cgi-test.pl,v 1.7 2005/07/28 04:34:45 rkh Exp $
 
 use warnings;
 use strict;
@@ -20,7 +20,7 @@ my $usage = <<'EOU';
 #       -db <dbname>  # database name to connect to
 #       -q  <pseq_id> # pseq_id commonly used for testing
 #       -v            # verbose option to see the commnd line used for testing
-# $Id: cgi-test.pl,v 1.6 2005/07/27 18:37:06 rkh Exp $
+# $Id: cgi-test.pl,v 1.7 2005/07/28 04:34:45 rkh Exp $
 #------------------------------------------------------------------------------
 EOU
 
@@ -39,8 +39,8 @@ $ENV{PGUSER} = $opts{user} || 'PUBLIC';
 GetOptions(\%opts,
 	   'verbose|v',
 	   'query|q=i',
-#	   'dbname|db=s',
-#	   'host|h=s',
+	   'dbname|db=s',
+	   'host|h=s',
 	   'help'
 )  || die("$0: Incorrect Usage\n");
 
@@ -70,6 +70,8 @@ my @cgi_scripts =
    ['../about_contents.pl'],
    ['../about_credits.pl'],
    ['../about_env.pl'],
+   ['../about_legal.pl'],
+   ['../about_news.pl'],
    ['../about_prefs.pl'],
    ['../about_unison.pl'],
    ['../browse_sets.pl',"pset_id=-1234"],
@@ -85,7 +87,7 @@ my @cgi_scripts =
    ['../compare_scores.pl',"submit=vroom pmodelset_id=13 pcontrolset_id=500 params_id=1 score=raw Plot=Clustered"],
    ['../genome_features.pl',"genasm_id=1 chr=15 gstart=39562512 gstop=39591527"],
    ['../get_fasta.pl',"pseq_id=$pseq_id"],
-   ['../hmm_alignment.pl',"pseq_id=$pseq_id profiles=TNF params_id=15"],
+   ['../hmm_alignment.pl',"pseq_id=$pseq_id profiles=TNF params_id=22"],
    ['../p2alignment.pl',"pseq_id=76 params_id=1 templates=1jtzx"],
    ['../p2cm.pl',"pseq_id=$pseq_id viewer=rasmol params_id=1 templates=1jtzx"],
    ['../pseq_blast.pl',"pseq_id=$pseq_id"],
@@ -99,7 +101,8 @@ my @cgi_scripts =
    ['../pseq_paprospect2.pl',"pseq_id=$pseq_id"],
    ['../pseq_papssm.pl',"pseq_id=$pseq_id"],
    ['../pseq_patents.pl',"pseq_id=$pseq_id"],
-   ['../pseq_structure.pl',"pseq_id=$pseq_id"],
+   ['../pseq_structure.pl',"pseq_id=870 userfeatures=mysnp\@931 highlight=user:mysnp:cyan,HMM:Furin-like:blue"],
+   ['../emb_pseq_structure.pl',"pseq_id=98 userfeatures=Estrand\@164-174,mysnp\@170 highlight=user:Estrand:green,user:mysnp:cyan,HMM:TNF:blue"],
    ['../pseq_summary.pl',"pseq_id=$pseq_id"],
    ['../search_by_alias.pl',"alias=EGFR_HUMAN"],
    ['../search_by_properties.pl',"o_sel=RefSeq r_species=on r_species_sel=9606 r_age_sel=30d r_len=on r_len_min=100 r_len_max=400 r_sigp=on r_sigp_sel=0.6 al_hmm_eval=1e-10 al_pssm_eval=1e-10 al_prospect2=on al_prospect2_svm=9 al_prospect2_params_id=1 al_ms_sel=2 al_go_sel=5164 x_set_sel=5 submit=vroom"],
@@ -114,14 +117,13 @@ my @cgi_scripts =
 #  );
 
 
-my %dir_scripts = map {$_=>1} grep {not m%(?:CVS|t)$%} glob('./* ../*');
+my %dir_scripts = map {$_=>1} grep {not m%(?:CVS|t|~)$%} glob('./* ../*');
 
 
 my @badwords = ('Server Error', 'Object not found', 'DBIError', 'Exception', 'Error');
-#my @goodwords = ('html');
 my $npassed =  0;
 
-print('$Id: cgi-test.pl,v 1.6 2005/07/27 18:37:06 rkh Exp $ ', "\n\n");
+print('$Id: cgi-test.pl,v 1.7 2005/07/28 04:34:45 rkh Exp $ ', "\n\n");
 
 printf("%-30.30s\tstatus\t%7s\tmessage\n",'script','time');
 print('='x76,"\n");
@@ -129,13 +131,16 @@ foreach my $cgi (@cgi_scripts) {
   my $message = '';
   my $failed = 0;
   delete $ENV{QUERY_STRING};
+
   if (defined $cgi->[1]) {
-	($ENV{QUERY_STRING} = $cgi->[1]) =~ s/ /;/g;
+      ($ENV{QUERY_STRING} = $cgi->[1]) =~ s/ /;/g;
   }
   my $cmd = $cgi->[0];
-
+  if(not $opts{html}) {
+    $cmd .= " host=$opts{host} dbname=$opts{dbname} ";
+    $cmd .= $cgi->[1] if (defined $cgi->[1]);
+  }
   delete $dir_scripts{$cgi->[0]};
-
   printf( '%-30.30s', $cgi->[0] . ' ' . '.' x 30 );
 
   my $t0 = new Benchmark;
@@ -147,7 +152,7 @@ foreach my $cgi (@cgi_scripts) {
   } else {
 	my $word;
 	foreach $word (@badwords) {
-	  if ($output =~ /\b$word\b/i and $output !~ /$word.pm/) {
+	  if ($output =~ /\b$word\b/i and $output !~ /$word.pm/ and $output !~ /REMARK/) {
 		$message = $&;
 		if ( ($word eq 'Exception' and $output =~ m/Detail.+/)
 			 or ($word eq 'Error' and $output =~ m/Error:.+/) ) {
@@ -157,13 +162,6 @@ foreach my $cgi (@cgi_scripts) {
 		last;
 	  }
 	}
-	# this concept doesn't work because we output more than just html
-	#foreach $word (@goodwords) {
-	#  if (not $output =~ /$word/i) {
-	#	$failed++;
-	#	$message = "missing required word '$word'";
-	#  }
-	#}
   }
 
   my $t1 = new Benchmark;
