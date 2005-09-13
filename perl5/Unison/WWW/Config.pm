4ea228d1f3bce892f2d15ddca562d5c1f764966f
Reece Hart <reecehart@gmail.com>
2005-09-13 17:23:35 +0000

    initial

---
+++ perl5/Unison/WWW/Config.pm
@@ -0,0 +1,16 @@
+=head1 NAME
+
+Unison::WWW::Config -- Unison web configuration
+
+S<$Id: init.pm,v 1.1 2005/07/18 20:32:57 rkh Exp $>
+
+=cut
+
+package Unison::WWW::Config;
+
+use strict;
+use warnings;
+
+$ENV{PATH} = '/gne/compbio/i686-linux-2.6/bin:/usr/bin:/bin';
+
+1;
