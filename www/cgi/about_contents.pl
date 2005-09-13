f44497f1987b4e6d5b99db495a006ee5d419800f
Reece Hart <reecehart@gmail.com>
2005-09-13 17:26:31 +0000

    * added params and origin information

--- www/cgi/about_contents.pl
+++ www/cgi/about_contents.pl
@@ -10,16 +10,52 @@ use Unison::WWW;
 use Unison::WWW::Page;
 use Unison::WWW::Table;
 
+sub meta_group($);
+sub params_group($);
+sub porigin_group($);
 
 my $p = new Unison::WWW::Page;
-my $u = $p->{unison};
-
-my $sql = 'select * from meta order by 1';
-my $ar = $u->selectall_arrayref($sql);
-my @f = qw( key value );
 
 print $p->render("Contents and Meta Information",
-				 $p->group("Meta",
-						   Unison::WWW::Table::render(\@f,$ar)),
-				 $p->sql($sql)
+				 meta_group($p),
+				 porigin_group($p),
+				 params_group($p)
 				);
+
+exit(0);
+
+
+
+sub meta_group($) {
+  my $p = shift;
+  my $u = $p->{unison};
+  my $sql = 'select * from meta order by 1';
+  my $sth = $u->prepare( $sql );
+  my $ar = $u->selectall_arrayref($sth);
+  my @cols = @{ $sth->{NAME} };
+  return $p->group("Meta",
+				   Unison::WWW::Table::render(\@cols,$ar));
+}
+
+sub params_group($) {
+  my $p = shift;
+  my $u = $p->{unison};
+  my $sql = 'select name,descr,commandline from params order by name';
+  my $sth = $u->prepare( $sql );
+  my $ar = $u->selectall_arrayref($sth);
+  my @cols = @{ $sth->{NAME} };
+  return $p->group("Execution Parameters",
+				   Unison::WWW::Table::render(\@cols,$ar));
+}
+
+
+sub porigin_group($) {
+  my $p = shift;
+  my $u = $p->{unison};
+  my $sql = 'select origin,descr,ann_pref,url from porigin where ann_pref is not null order by ann_pref,origin';
+  my $sth = $u->prepare( $sql );
+  my $ar = $u->selectall_arrayref($sth);
+  my @cols = @{ $sth->{NAME} };
+  return $p->group("Data Sources",
+				   Unison::WWW::Table::render(\@cols,$ar));
+}
