23615ca00eb5a2c2b3b97469399589cec9678193
Reece Hart <reecehart@gmail.com>
2005-09-13 17:15:03 +0000

    added params.mk support

---
+++ build/loading/params.mk
@@ -0,0 +1,26 @@
+## params.mk -- Unison loading configuration and options
+## $Id$
+
+
+############################################################################
+#### PSEQ
+UNIPROT_DIR=${COMPBIO_PREFIX}/share/uniprot
+
+
+############################################################################
+#### PMHMM/PAHMM
+PFAM_DIR=${COMPBIO_PREFIX}/share/pfam-18.0
+PAHMM_PARAMS_ID:=26
+
+
+############################################################################
+#### PFREGEXP
+PFREGEXP_PARAMS_ID:=12
+ITxM_PMODELSET_ID:=11
+
+
+############################################################################
+#### PSPROTCOMP
+PSPROTCOMP_PARAMS_ID:=20
+PSPROTCOMP_SEQS_PER_RUN:=25
+
