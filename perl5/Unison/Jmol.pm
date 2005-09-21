9443ca75f33992b22fb3174b4bddcbe0057a5549
mukhyala <mukhyala@localhost>
2005-09-21 19:03:30 +0000

    relace stringio prints with .=

--- perl5/Unison/Jmol.pm
+++ perl5/Unison/Jmol.pm
@@ -28,27 +28,28 @@ sub script_header {
 }
 sub initialize{
     my ($self,$fn,$name,$pseq_structure,$structures_ar) = @_;
-    my $retval;
-    my $stringio = IO::String->new($retval);
+    
+    my $retval = '';
     my $select_chain = ":".uc(substr($name,4,1)).".*";
     my $jmol_menu_arr = $self->_get_jmol_menu_scripts($structures_ar,$select_chain) if(defined($structures_ar));
-    $stringio->print($pseq_structure->set_js_vars(1));
-    $stringio->print("<table border=1>");
-    $stringio->print("<tr><td colspan=3><script>jmolInitialize(\"../js/jmol/\");");
 
-    $stringio->print("jmolApplet([$self->{'width'}, $self->{'height'}],\"".$self->_load_script("../js/Jmol/pdb/all.ent/$fn",$select_chain,$name));
-    $stringio->print($self->highlights($select_chain)) if(defined($self->{'highlights'}));
-    $stringio->print("\");");
-    $stringio->print("</script></td></tr>");
+    $retval .= $pseq_structure->set_js_vars(1);
+    $retval .= "<table border=1>";
+    $retval .= "<tr><td colspan=3><script>jmolInitialize(\"../js/jmol/\");";
 
-    $stringio->print("<tr><td colspan=3><center><script>jmolMenu($jmol_menu_arr)</script></center></td></tr>");
-    $stringio->print("<tr><td width=33%><script>jmolCheckbox(\"select hetero ; wireframe 0.5;\",\"select not hetero; restrict selected;\",\"show hetero atoms\",false)</script></td>");
-    $stringio->print("<td width=33%><center><script>jmolButton(\"center $select_chain\", \"center\")</script></center></td>");
-    $stringio->print("<td width=33%><script>jmolCheckbox(\"select all; cartoon on;\",\"cartoon on; select $select_chain; restrict selected; center $select_chain;zoom 150;\",\"show all chains\")</script></td></tr>");
+    $retval .= "jmolApplet([$self->{'width'}, $self->{'height'}],\"".$self->_load_script("../js/Jmol/pdb/all.ent/$fn",$select_chain,$name);
+    $retval .= $self->highlights($select_chain) if(defined($self->{'highlights'}));
+    $retval .= "\");";
+    $retval .= "</script></td></tr>";
+
+    $retval .= "<tr><td colspan=3><center><script>jmolMenu($jmol_menu_arr)</script></center></td></tr>";
+    $retval .= "<tr><td width=33%><script>jmolCheckbox(\"select hetero ; wireframe 0.5;\",\"select not hetero; restrict selected;\",\"show hetero atoms\",false)</script></td>";
+    $retval .= "<td width=33%><center><script>jmolButton(\"center $select_chain\", \"center\")</script></center></td>";
+    $retval .= "<td width=33%><script>jmolCheckbox(\"select all; cartoon on;\",\"cartoon on; select $select_chain; restrict selected; center $select_chain;zoom 150;\",\"show all chains\")</script></td></tr>";
 
 
 
-    $stringio->print("</table>");
+    $retval .= "</table>";
     return( $retval );
 }
 
@@ -56,30 +57,30 @@ sub initialize_inline {
   my ($self,$coord,$script,$pseq_structure) = @_;
   my ($retval);
   my $c = "\"\"";
-  my $stringio = IO::String->new($retval);
-  $stringio->print($pseq_structure->set_js_vars(0)) if(defined($pseq_structure));
-  $stringio->print("<center><table border=\"1\" cellpadding=\"5\"><tr><td><script>jmolInitialize(\"../js/jmol/\");");
+  my $retval = '';
+  $retval .= $pseq_structure->set_js_vars(0) if(defined($pseq_structure));
+  $retval .= "<center><table border=\"1\" cellpadding=\"5\"><tr><td><script>jmolInitialize(\"../js/jmol/\");";
 
   foreach my $line (split(/\n/,$coord)) {
     if ($line =~ /^ATOM/) {
       $c .= "+\"$line\\n\"\n" if($line =~ /CA/);
     }
   }
-  $stringio->print("var myPdb = $c;");
-  $stringio->print("jmolAppletInline([$self->{'width'}, $self->{'height'}],myPdb,\"$script\");");
-  $stringio->print("</script></td>");
-  $stringio->print("</tr></table></center>");
+  $retval .= "var myPdb = $c;";
+  $retval .= "jmolAppletInline([$self->{'width'}, $self->{'height'}],myPdb,\"$script\");";
+  $retval .= "</script></td>";
+  $retval .= "</tr></table></center>";
   return( $retval );
 }
 
 sub load{
     my ($self,$fn,$chain) = @_;
-    my $retval;
-    my $stringio = IO::String->new($retval);
+    
+    my $retval = '';
     my $select_chain = ":".uc($chain).".*";
     my $name = substr($fn,3,4);
 	# set frank off;
-    $stringio->print("load ../js/Jmol/pdb/all.ent/$fn; spacefill off; wireframe off; cartoon on; color cartoon structure;select $select_chain; restrict selected; center $select_chain;zoom 150;set echo off;set echo top left;font echo 18 serif;color echo white; echo $name;");
+    $retval .= "load ../js/Jmol/pdb/all.ent/$fn; spacefill off; wireframe off; cartoon on; color cartoon structure;select $select_chain; restrict selected; center $select_chain;zoom 150;set echo off;set echo top left;font echo 18 serif;color echo white; echo $name;";
     return( $retval );
 }
 
@@ -91,16 +92,16 @@ sub pos_view {
       $colour =~ s/-/,/g;
       $colour = "[$colour]";
     }
-    my $retval;
-    my $stringio = IO::String->new($retval);
-    $stringio->print("spacefill off;");
-    $stringio->print("select within\(10.0, $pos$chain\);hbonds on;"); 
-    $stringio->print("select ".$pos.$chain.";");
-    $stringio->print("wireframe 0.5;");
-    $stringio->print("color $colour;");
-    $stringio->print("select $pos$chain and *.CA;");
-    $stringio->print("label $label; color label white;");
-    $stringio->print("center $pos$chain; zoom 400; select all;");
+    
+    my $retval = '';
+    $retval .= "spacefill off;";
+    $retval .= "select within\(10.0, $pos$chain\);hbonds on;"; 
+    $retval .= "select ".$pos.$chain.";";
+    $retval .= "wireframe 0.5;";
+    $retval .= "color $colour;";
+    $retval .= "select $pos$chain and *.CA;";
+    $retval .= "label $label; color label white;";
+    $retval .= "center $pos$chain; zoom 400; select all;";
     return $retval;
 
 }
@@ -149,10 +150,10 @@ sub changeStructureLink {
 
     my ($self,$script,$name) = @_;
 
-    my $retval;
-    my $stringio = IO::String->new($retval);
+    
+    my $retval = '';
 
-    $stringio->print("<script>jmolChangeStructureLink(\"$script\",\"$name\");</script>");
+    $retval .= "<script>jmolChangeStructureLink(\"$script\",\"$name\");</script>";
     return( $retval );
 }
 
@@ -187,17 +188,19 @@ sub _get_jmol_menu_scripts {
   my ($self,$ar,$select_chain) = @_;
   my $retval;
   my $stringio = IO::String->new($retval);
+
   $stringio->print("[");
   $stringio->print(map {"[\"".$self->_load_script("../js/Jmol/pdb/all.ent/pdb".substr($_->[1],0,4).".ent",$select_chain,$_->[1])."\",\"".substr($_->[13],0,48)." ($_->[1],eval=$_->[7],ident=$_->[9])\"],"} @$ar);
   $stringio->print("]");
+
   return $retval;
 }
 
 sub _load_script {
   my ($self,$fn,$select_chain,$name) = @_;
-  my $retval;
-  my $stringio = IO::String->new($retval);
-  $stringio->print("load $fn;set frank off;spacefill off; wireframe off; cartoon on; color cartoon structure; select $select_chain; restrict selected; center $select_chain;zoom 150;set echo off;set echo top left;font echo 18 serif;color echo white; echo $name;");
+
+  my $retval = '';
+  $retval .= "load $fn;set frank off;spacefill off; wireframe off; cartoon on; color cartoon structure; select $select_chain; restrict selected; center $select_chain;zoom 150;set echo off;set echo top left;font echo 18 serif;color echo white; echo $name;";
   return $retval;
 }
 
