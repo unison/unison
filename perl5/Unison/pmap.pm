c54a7d89e5dc33c44770b0a17b20a353414fff1c
mukhyala <mukhyala@localhost>
2005-11-21 19:22:36 +0000

    pmap (for protein genome alignements) related methods. (created during blat to pmap migration)

---
+++ perl5/Unison/pmap.pm
@@ -0,0 +1,70 @@
+=head1 NAME
+
+Unison::pmap -- PMAP-related functions for Unison
+
+S<$Id$>
+
+=head1 SYNOPSIS
+
+ use Unison;
+ use Unison::pmap;
+ my $u = new Unison(...);
+ $u->get_pmapaln_info( pseq_id params_id)
+
+=head1 DESCRIPTION
+
+B<Unison::pmap> provides PMAP-related methods to the B<Unison::>
+namespace.
+
+=cut
+
+
+package Unison;
+
+
+=pod
+
+=head1 ROUTINES AND METHODS
+
+=over
+
+=cut
+
+######################################################################
+## get_pmapaln_info
+
+=pod
+
+=item $u->get_pmapaln_info( B<pseq_id>, B<params_id> )
+
+returns an array of <genasm_id,chr,gstart,gstop,pmapaln_id> for a given
+B<pseq_id> B<prams_id> from the v_pmap view.
+
+=cut
+
+sub get_pmapaln_info {
+  my ($u, $pseq_id, $params_id) = @_;
+  my $sql = "select genasm_id,chr,gstart,gstop,aln_id from v_pmap where pseq_id=? and params_id=?";
+  print(STDERR $sql, ";\n\n") if $opts{verbose};
+  my $sth = $u->prepare_cached($sql);
+  return @{ $u->selectall_arrayref($sth,undef,$pseq_id,$params_id) };
+}
+
+
+=pod
+
+=head1 SEE ALSO
+
+=over
+
+=item * perldoc Unison
+
+=back
+
+=head1 AUTHOR
+
+see perldoc Unison for contact information
+
+=cut
+
+1;
