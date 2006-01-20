0a742f01f062d45716e9e27477435600b5571e1d
Reece Hart <reecehart@gmail.com>
2006-01-20 00:49:47 +0000

    consistency changes and .cvsignore updates

--- build/loading/local.mk
+++ build/loading/local.mk
@@ -1,32 +1,54 @@
 ## local.mk -- site-local definitions for Unison loading
-## $Id: local.mk,v 1.2 2005/09/13 15:05:14 rkh Exp $
+## $Id: local.mk,v 1.3 2005/10/09 20:13:00 rkh Exp $
 
 
 # Unison connection details
 export PGHOST:=csb
 export PGDATABASE:=csb-dev
-export PGUSER:=loader
-# NOTE: the corresponding PGPASSWORD must be passed in the environment 
+export PGUSER:=unison
+# NOTE: PGPASSWORD should be passed in the environment or in .pgpass
+# but people insoucient regarding security could set PGPASSWORD here.
 
 
 # Unison top-level directory
 UNISON_HOME:=${HOME}/unison
 
-PERL5LIB:=${UNISON_HOME}/perl5:${PERL5LIB}
-export PERL5LIB
-
+# where to find executables
 PATH:=${UNISON_HOME}/sbin:${UNISON_HOME}/bin:${UNISON_HOME}/misc
 PATH:=${PATH}:${COMPBIO_EPREFIX}/bin:${COMPBIO_PREFIX}/bin
 PATH:=${PATH}:/usr/pbs/bin:/usr/local/tools/bin:/usr/bin:/bin
 export PATH
 
-PROSPECT_PATH:=${COMPBIO_EPREFIX}/opt/prospect
-export PROSPECT_PATH
+# where to find Unison Perl5 modules
+PERL5LIB:=${UNISON_HOME}/perl5:${PERL5LIB}
+export PERL5LIB
 
-# WARNING: DEPENDENCY ON REECE'S HOME DIRECTORY HERE
-RENAME=~rkh/opt/bin/rerename
+# perl regexp-based renamer
+# this is only needed for PBS job submission
+RENAME=${UNISON_HOME}/sbin/rerename
 
-# Where to find various computational biology data required for
-# Unison loading
-UNIPROT_DIR=${COMPBIO_PREFIX}/share/uniprot
-PFAM_DIR=${COMPBIO_PREFIX}/share/pfam-18.0
+
+
+# Enable Genentech-specific loading
+# This will require a lot of code and data that are
+# not publicly available. 
+GNE:=0
+ifeq "$(shell /bin/hostname|cut -d. -f2-)" "gene.com"
+$(warning NOTICE: enabling Genentech-specific configuration)
+GNE:=1
+endif
+
+
+
+ifdef DEBUG
+$(warning UNISON_HOME=${UNISON_HOME} )
+$(warning PGUSER=${PGUSER} )
+$(warning PGDATABASE=${PGDATABASE} )
+$(warning PERL5LIB=${PERL5LIB} )
+$(warning PATH=${PATH} )
+$(warning QSUB=${QSUB} )
+endif
+
+ifndef PGPASSWORD
+$(warning PGPASSWORD isn't set ) 	#'
+endif
