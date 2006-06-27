1279928c3e81b448f6d4691f17dac32c57479ee2
Kiran Mukhyala <mukhyala@gmail.com>
2006-06-27 03:51:47 +0000

    new version brute force version

--- misc/sql-scraps/update_psets.sql
+++ misc/sql-scraps/update_psets.sql
@@ -24,73 +24,22 @@ BEGIN
 
 	RAISE DEBUG ''* % (pset_id=%)'', v_row.name, v_row.pset_id;
 
-	-- __newseqs may exist already, but there is no easy way to drop it preemptively
-	-- if it exists, an exception will be thrown
-	v_cmd:=''CREATE TEMP TABLE __newseqs AS SELECT DISTINCT pseq_id FROM (''||v_row.def||'') X'';
-    RAISE DEBUG ''  %'', v_cmd;
-    EXECUTE v_cmd;
-
-    v_cmd:=''DELETE FROM pseqset WHERE pset_id=''||v_row.pset_id||'' AND pseq_id NOT IN (SELECT pseq_id FROM __newseqs)'';
-    RAISE DEBUG ''  %'', v_cmd;
-    EXECUTE v_cmd;
+	v_cmd:=''delete from pseqset where pset_id=''||v_row.pset_id;
+	RAISE DEBUG ''  %'', v_cmd;
+	EXECUTE v_cmd;
 	GET DIAGNOSTICS v_nrows_del = ROW_COUNT;
 
-    v_cmd:=''INSERT INTO pseqset (pset_id,pseq_id) SELECT ''||v_row.pset_id||'',pseq_id FROM (SELECT pseq_id FROM __newseqs EXCEPT SELECT pseq_id FROM pseqset WHERE pset_id=''||v_pset_id||'') X'';
-    RAISE DEBUG ''  %'', v_cmd;
-    EXECUTE v_cmd;
+	v_cmd:=''INSERT INTO pseqset (pset_id,pseq_id) SELECT ''||v_row.pset_id||'',pseq_id FROM (''||v_row.def||'')X'';
+	RAISE DEBUG ''  %'', v_cmd;
+	EXECUTE v_cmd;
 	GET DIAGNOSTICS v_nrows_ins = ROW_COUNT;
 
-	EXECUTE ''DROP TABLE __newseqs'';
+	RAISE NOTICE ''  % (pset_id=%): % sequences deleted, % sequences added'',
+		v_row.name, v_row.pset_id, v_nrows_del, v_nrows_ins;
 
-	SELECT INTO v_nrows_tot COUNT(*) FROM pseqset WHERE pset_id = v_pset_id;
-
-	RAISE NOTICE ''  % (pset_id=%): % sequences deleted, % sequences added, % total'',
-		v_row.name, v_row.pset_id, v_nrows_del, v_nrows_ins, v_nrows_tot;
-
-	RETURN v_nrows_tot;
+	RETURN v_nrows_ins;
 END
 ';
 
-comment on function update_pset(integer) is 'update pseqset for a specified pset_id using the definition in pset.def';
+comment on function update_pset(integer) is 'update pseqset for a specified pset_id using the definition in pset_def';
 
-
-
-
-CREATE OR REPLACE FUNCTION update_psets_intrinsic ()
-RETURNS VOID
-LANGUAGE plpgsql
-AS '
-DECLARE
-	v_row record;
-	v_nseqs integer;
-BEGIN
-	FOR v_row IN SELECT pset_id,name FROM pset WHERE pset_id between 1 and 99 order by pset_id LOOP
-		SELECT INTO v_nseqs update_pset( v_row.pset_id );
-		RAISE NOTICE ''% (pset_id=%): % sequences'', v_row.name, v_row.pset_id, v_nseqs;
-    END LOOP;
-    RETURN;
-END;
-';
-
-comment on function update_psets_intrinsic () is 'update pseqset for set definitions based on origin, alias, description, and tax_id';
-
-
-
-
-CREATE OR REPLACE FUNCTION update_psets_extrinsic ()
-RETURNS VOID
-LANGUAGE plpgsql
-AS '
-DECLARE
-	v_row record;
-	v_nseqs integer;
-BEGIN
-	FOR v_row IN SELECT pset_id,name FROM pset WHERE pset_id between 100 and 199 order by pset_id LOOP
-		SELECT INTO v_nseqs update_pset( v_row.pset_id );
-		RAISE NOTICE ''% (pset_id=%): % sequences'', v_row.name, v_row.pset_id, v_nseqs;
-    END LOOP;
-    RETURN;
-END;
-';
-
-comment on function update_psets_extrinsic () is 'update pseqset for set definitions based on computed features';
