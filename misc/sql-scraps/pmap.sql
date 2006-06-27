8a6eff0816de1798ea1d542b0576b531c3697b1e
Kiran Mukhyala <mukhyala@gmail.com>
2006-06-27 03:54:01 +0000

    update

--- misc/sql-scraps/pmap.sql
+++ misc/sql-scraps/pmap.sql
@@ -2,7 +2,7 @@
 -- Name: pmap.sql
 -- Purpose: sql code for generating tables for storing PMAP results
 --
--- $Id: pmap.sql,v 1.2 2005/11/17 17:10:51 mukhyala Exp $
+-- $Id: pmap.sql,v 1.3 2006/01/24 18:48:51 mukhyala Exp $
 -- -----------------------------------------------------------------------------
 
 -- -----------------------------------------------------------------------------
@@ -13,7 +13,7 @@ DROP TABLE pmap_hsp CASCADE;
 CREATE TABLE pmap_hsp (
     hsp_id serial NOT NULL,
     chr text NOT NULL,
-    plus_strand boolean NOT NULL,
+    strand char NOT NULL,
     genasm_id integer NOT NULL,
     gstart integer NOT NULL,
     gstop integer NOT NULL,
@@ -31,9 +31,9 @@ GRANT SELECT ON TABLE pmap_hsp TO PUBLIC;
 REVOKE ALL ON TABLE pmap_hsp_hsp_id_seq FROM PUBLIC;
 GRANT INSERT,UPDATE ON TABLE pmap_hsp_hsp_id_seq TO loader;
 
-CREATE UNIQUE INDEX pmap_hsp_unq ON pmap_hsp USING btree (params_id,genasm_id, chr, plus_strand, gstart, gstop, pseq_id, pstart, pstop);
+CREATE UNIQUE INDEX pmap_hsp_unq ON pmap_hsp USING btree (params_id,genasm_id, chr, strand, gstart, gstop, pseq_id, pstart, pstop);
 CREATE INDEX pmap_hsp_p_lookup ON pmap_hsp USING btree (params_id,pseq_id, pstart, pstop);
-CREATE INDEX pmap_hsp_g_lookup ON pmap_hsp USING btree (params_id,genasm_id, chr, plus_strand, gstart, gstop);
+CREATE INDEX pmap_hsp_g_lookup ON pmap_hsp USING btree (params_id,genasm_id, chr, strand, gstart, gstop);
 
 
 ALTER TABLE ONLY pmap_hsp
@@ -48,7 +48,7 @@ ALTER TABLE ONLY pmap_hsp
 
 COMMENT ON TABLE pmap_hsp IS 'protein to dna PMAP HSPs';
 COMMENT ON COLUMN pmap_hsp.chr IS 'chromosome (e.g. 1..22,M,U,X,Y for homo sapiens)';
-COMMENT ON COLUMN pmap_hsp.plus_strand IS 'located on genomic plus strand (t/f)';
+COMMENT ON COLUMN pmap_hsp.strand IS 'genomic strand (+/-)';
 COMMENT ON COLUMN pmap_hsp.gstart IS 'start of HSP on genome (1-based, +1 frame, gstop > gstart)';
 COMMENT ON COLUMN pmap_hsp.gstop IS 'stop of HSP on genome (1-based, +1 frame, gstop > gstart)';
 COMMENT ON COLUMN pmap_hsp.pstart IS 'start of HSP on protein (1-based)';
@@ -120,7 +120,7 @@ GRANT INSERT,UPDATE,DELETE ON TABLE pmap_alnhsp TO loader;
 CREATE UNIQUE INDEX pmap_alnhsp_unq ON pmap_alnhsp USING btree (aln_id, hsp_id);
 
 ALTER TABLE ONLY pmap_alnhsp
-    ADD CONSTRAINT pmap_aln_id_exists FOREIGN KEY (aln_id) REFERENCES pmap_aln(aln_id) ON UPDATE CASCADE ON DELETE CASCADE;
+    ADD CONSTRAINT pmap_aln_id_exists FOREIGN KEY (aln_id) REFERENCES pmap_aln(aln_id) ON UPDATE CASCADE ON DELETE CASCADE INITIALLY DEFERRED;
 ALTER TABLE ONLY pmap_alnhsp
     ADD CONSTRAINT pmap_hsp_id_exists FOREIGN KEY (hsp_id) REFERENCES pmap_hsp(hsp_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
@@ -131,14 +131,14 @@ COMMENT ON TABLE pmap_alnhsp IS 'groups protein-to-genome PMAP HSPs into alignme
 -- -----------------------------------------------------------------------------
 -- ins_pmap_hsp():
 --   purpose: insert protein-to-genome PMAP HSPs
---   arguments: chr,plus_strand,gstart,gstop,pseq_id,pstart,pstop
+--   arguments: genasm_id,chr,strand,gstart,gstop,pseq_id,pstart,pstop
 --   returns: pmap_hsp_id
 --
-CREATE OR REPLACE FUNCTION ins_pmap_hsp(integer, text, boolean, integer, integer, integer, integer, integer, integer) RETURNS integer AS '
+CREATE OR REPLACE FUNCTION ins_pmap_hsp(integer, text, char, integer, integer, integer, integer, integer, integer) RETURNS integer AS '
 DECLARE
   v_genasm_id ALIAS FOR $1;	
   v_chr ALIAS FOR $2;
-  v_plus_strand ALIAS FOR $3;
+  v_strand ALIAS FOR $3;
   v_gstart ALIAS FOR $4;
   v_gstop ALIAS FOR $5;
   v_pseq_id ALIAS FOR $6;
@@ -151,7 +151,7 @@ BEGIN
   -- check whether HSP already loaded
   SELECT INTO v_hsp_id hsp_id FROM pmap_hsp WHERE 
     chr=v_chr AND
-    plus_strand=v_plus_strand AND
+    strand=v_strand AND
     gstart=v_gstart AND
     gstop=v_gstop AND
     pseq_id=v_pseq_id AND
@@ -161,8 +161,8 @@ BEGIN
     params_id=v_params_id;
   IF v_hsp_id is null THEN
     SELECT INTO v_hsp_id nextval(''pmap_hsp_hsp_id_seq''::text);
-    INSERT INTO pmap_hsp (hsp_id,genasm_id,chr,plus_strand,gstart,gstop,pseq_id,pstart,pstop,params_id) VALUES
-      (v_hsp_id,v_genasm_id,v_chr,v_plus_strand,v_gstart,v_gstop,v_pseq_id,v_pstart,v_pstop,v_params_id);	
+    INSERT INTO pmap_hsp (hsp_id,genasm_id,chr,strand,gstart,gstop,pseq_id,pstart,pstop,params_id) VALUES
+      (v_hsp_id,v_genasm_id,v_chr,v_strand,v_gstart,v_gstop,v_pseq_id,v_pstart,v_pstop,v_params_id);	
   END IF;
   RETURN v_hsp_id;
 END;
@@ -205,7 +205,7 @@ BEGIN
   IF v_aln_id is null THEN
 
     SELECT INTO v_aln_id nextval(''pmap_aln_aln_id_seq''::text);
-
+ 
    -- insert into pmap_alnhsp table (let the unique indices deal with redudancies
     SELECT INTO v_hsp_ids hsp_str_to_hsp_ids( v_hsp_str );
     low  := replace(split_part(array_dims(v_hsp_ids),'':'',1),''['','''')::int;
@@ -217,6 +217,8 @@ BEGIN
 
     INSERT INTO pmap_aln (aln_id, ident, pgap_cnt, pgap_bases, ggap_cnt, ggap_bases, params_id, genasm_id, pseq_id) VALUES
       (v_aln_id, v_ident, v_pgap_cnt, v_pgap_bases, v_ggap_cnt, v_ggap_bases, v_params_id, v_genasm_id, v_pseq_id);
+  ELSE
+	RAISE EXCEPTION ''the hsps already exists, aln_id is %'',v_aln_id;
   END IF;
 
   RETURN v_aln_id;
@@ -298,18 +300,18 @@ CREATE CONSTRAINT TRIGGER pseq_genasm_params_trigger
 -- 
 
 CREATE OR REPLACE VIEW PMAP_V AS
-	SELECT a.params_id, a.genasm_id, h.pseq_id, ah.aln_id, min(h.pstart) AS pstart, max(h.pstop) AS pstop, count(*) AS exons, sum(h.pstop - h.pstart + 1) AS aln_length, (sum(h.pstop - h.pstart + 1)::double precision / q.len::double precision * 100::double precision)::integer AS pct_cov, a.ident, ((a.ident / sum(h.pstop - h.pstart + 1))::double precision * 100::double precision)::integer AS pct_ident, h.chr, h.plus_strand, min(h.gstart) AS gstart, max(h.gstop) AS gstop
+	SELECT a.params_id, a.genasm_id, h.pseq_id, ah.aln_id, min(h.pstart) AS pstart, max(h.pstop) AS pstop, count(*) AS exons, sum(h.pstop - h.pstart + 1) AS aln_length, (sum(h.pstop - h.pstart + 1)::double precision / q.len::double precision * 100::double precision)::integer AS pct_cov, a.ident, ((a.ident / sum(h.pstop - h.pstart + 1))::double precision * 100::double precision)::integer AS pct_ident, h.chr, h.strand, min(h.gstart) AS gstart, max(h.gstop) AS gstop
    FROM pmap_hsp h
    JOIN pmap_alnhsp ah ON h.hsp_id = ah.hsp_id
    JOIN pmap_aln a ON ah.aln_id = a.aln_id
    JOIN pseq q ON h.pseq_id = q.pseq_id
-  GROUP BY a.params_id, a.genasm_id, h.pseq_id, ah.aln_id, h.chr, h.plus_strand, a.ident, q.len
+  GROUP BY a.params_id, a.genasm_id, h.pseq_id, ah.aln_id, h.chr, h.strand, a.ident, q.len
   ORDER BY h.pseq_id, (sum(h.pstop - h.pstart + 1)::double precision / q.len::double precision * 100::double precision)::integer DESC, (a.ident::double precision / sum(h.pstop - h.pstart + 1)::double precision * 100::double precision)::integer DESC;
 
 GRANT SELECT ON PMAP_V TO PUBLIC;
 
 CREATE OR REPLACE VIEW PMAP_ALN_V AS
-	SELECT a.params_id, ah.aln_id, h.pseq_id, h.pstart, h.pstop, h.gstart, h.gstop, a.ident, h.genasm_id, h.chr, h.plus_strand
+	SELECT a.params_id, ah.aln_id, h.pseq_id, h.pstart, h.pstop, h.gstart, h.gstop, a.ident, h.genasm_id, h.chr, h.strand
    FROM pmap_hsp h
    JOIN pmap_alnhsp ah ON h.hsp_id = ah.hsp_id
    JOIN pmap_aln a ON ah.aln_id = a.aln_id
