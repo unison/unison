1d1da3453c7c0ef35238a1047a79d0bd7452f66c
rkh <rkh@localhost>
2006-09-08 23:58:22 +0000

    working, but untested

--- www/cgi/browse_views.pl
+++ www/cgi/browse_views.pl
@@ -13,9 +13,9 @@ use Unison::WWW::Table;
 
 my $p = new Unison::WWW::Page;
 my $u = $p->{unison};
-$p->add_footer_lines('$Id: browse_views.pl,v 1.16 2006/08/16 21:21:19 mukhyala Exp $ ');
+$p->add_footer_lines('$Id: browse_views.pl,v 1.17 2006/08/17 17:27:05 rkh Exp $ ');
 
-my %cvs = %{ $u->selectall_hashref('select cv_id,name,descr,sql from canned_views where cv_id>0',
+my %cvs = %{ $u->selectall_hashref('select cv_id,name,descr,sql,order_by from canned_views',
 								   'cv_id') };
 
 print $p->render('Canned Views',
@@ -77,14 +77,14 @@ sub do_search {
   # column order
   # FIXME: genasm_id=3 and params_id=45 shouldn't be hardwired
   # consider 1-arg versions of these for most recent human alignment
-  $s_sql->columns('pseq_locus(X.pseq_id,3,45) as locus') if (exists $v->{a_locus});
-  $s_sql->columns('best_alias(pseq_locus_rep(X.pseq_id,3,45)) as locus_rep') if (exists $v->{a_locus_rep});
-  $s_sql->columns('best_annotation(X.pseq_id)') if (exists $v->{a_best_annotation});
+  $s_sql->columns('pseq_locus(X.pseq_id,3,45) as locus') if ($v->{a_locus});
+  $s_sql->columns('best_alias(pseq_locus_rep(X.pseq_id,3,45)) as locus_rep') if ($v->{a_locus_rep});
+  $s_sql->columns('best_annotation(X.pseq_id)') if ($v->{a_best_annotation});
   $s_sql->columns('X.*');
-  $s_sql->columns('Q.added::date') if (exists $v->{a_added});
-  $s_sql->columns("(select as_set(distinct 'UNQ'||unqid) from gg_unq_pro_dna_pseq_v where pseq_id=X.pseq_id group by unqid order by unqid) as unqs") if (exists $v->{a_unq});
-  $s_sql->columns("(select as_set(distinct 'PRO'||proid) from gg_unq_pro_dna_pseq_v where pseq_id=X.pseq_id group by proid order by proid) as pros") if (exists $v->{a_pro});
-  $s_sql->columns("(select as_set(distinct(chip||':'||probe_id)) from pseq_probe_v P where P.pseq_id=X.pseq_id) as probes") if (exists $v->{a_probes});
+  $s_sql->columns('Q.added::date') if ($v->{a_added});
+  $s_sql->columns("(select as_set(distinct 'UNQ'||unqid) from gg_unq_pro_dna_pseq_v where pseq_id=X.pseq_id group by unqid order by unqid) as unqs") if ($v->{a_unq});
+  $s_sql->columns("(select as_set(distinct 'PRO'||proid) from gg_unq_pro_dna_pseq_v where pseq_id=X.pseq_id group by proid order by proid) as pros") if ($v->{a_pro});
+  $s_sql->columns("(select as_set(distinct(chip||':'||probe_id)) from pseq_probe_v P where P.pseq_id=X.pseq_id) as probes") if ($v->{a_probes});
 
   if ($v->{a_locus_rep} eq 'order') {
 	$s_sql->order('locus_rep');
@@ -92,7 +92,10 @@ sub do_search {
 	$s_sql->distinct('locus_rep');
   }
 
-  my $sth = $u->prepare( "$s_sql" );
+  my $sql = "select * from ($s_sql) Y";
+  $sql .= " ORDER BY $cv->{order_by}" if (defined $cv->{order_by});
+
+  my $sth = $u->prepare( $sql );
   my $ar = $u->selectall_arrayref($sth);
   my @cols = @{ $sth->{NAME} };
 
@@ -134,12 +137,29 @@ sub do_search {
 		  "<b>$cv->{name} (view $cv->{cv_id})</b>: <i>$cv->{descr}</i>",
 		  $p->group(sprintf("%s; %d rows",$cv->{name}, $#$ar+1),
 					Unison::WWW::Table::render(\@colhdrs,$ar)),
-		  $p->sql("$s_sql")
+		  $p->sql("$sql")
 		);
 }
 
 
 
+
+my %defaults = 
+  (
+   cv_id => undef,
+   r_species => 'off',
+   r_species_sel => 9606,
+   r_age => 'off',
+   r_age_sel => 30,
+   a_added => 'off',
+   a_locus => 'off',
+   a_best_annotation => 'on',
+   a_probes => 'off',
+   a_locus_rep => 'no',
+   a_unq => 'no',
+   a_pro => 'no',
+   a_fam => 'no'
+  );
 
 sub build_form {
   my $p = shift;
@@ -147,10 +167,13 @@ sub build_form {
   my $v = $p->Vars();
 
   my @cvs = sort {$a<=>$b} keys %cvs;
+  # restrict menu to cv_id>0 unless a cv_id<0 is being displayed (usually power users)
+  @cvs = grep {$_>0} @cvs  unless (defined $v->{cv_id} and $v->{cv_id}<0);
+
   my %cvlabels = map {$_ => sprintf("%s (canned view %d)",$cvs{$_}->{name}, $_)} @cvs;
 
-  my %sl = map { $_->[0] => "$_->[1] ($_->[2])" } 
-	@{ $u->selectall_arrayref("select tax_id,common,latin from tax.spspec
+  my %sl = map { $_->[0] => "$_->[1] ($_->[3]; $_->[2])" } 
+	@{ $u->selectall_arrayref("select tax_id,common,latin,gs from tax.spspec
                                where tax_id in (7955,9606,10090,10116,10117,
                                                 9615,9685,7227)
                                order by gs") };
@@ -170,31 +193,31 @@ sub build_form {
 		  $p->popup_menu(-name=>'cv_id',
 						 -values => \@cvs,
 						 -labels => \%cvlabels,
-						 -default => $v->{cv_id}||undef
+						 -default => $v->{cv_id}
 						),
 		  '</td></tr>',
 		  '<tr><td colspan=2>where:</td></tr>',
 		  '<tr><td>',
 		  $p->checkbox(-name => 'r_species',
 					   -label => 'species is',
-					   -checked => $v->{r_species}||0),
+					   -checked => $v->{r_species}||$defaults{r_species}),
 		  '</td><td>',
 		  $p->popup_menu(-name => 'r_species_sel',
 						 -values => [sort keys %sl],
 						 -labels => \%sl,
-						 -default => $v->{r_species_sel}||9606
+						 -default => $v->{r_species_sel}||$defaults{r_species_sel}
 						),
 		  '</td></tr>',
 		  '<tr><td>',
 		  $p->checkbox(-name => 'r_age',
 					   -label => 'sequence is less than',
-					   -checked => 0),
+					   -checked => $v->{r_age}||$defaults{r_age}),
 		  '</td><td colspan=2>',
 		  $p->popup_menu(-name=>'r_age_sel',
-						 -values=>[qw(7d 14d 30d 60d 90d 180d 365d)],
-						 -default=>$v->{r_age_sel}||'30d'
+						 -values=>[qw(7 14 30 60 90 180 365)],
+						 -default=>$v->{r_age_sel}||$defaults{r_age_sel}
 						),
-		  ' old',
+		  ' days old',
 		  '</td></tr>',
 		  '</table>',
 
@@ -206,19 +229,19 @@ sub build_form {
 		  '<td valign="top">',
 		  $p->checkbox(-name => 'a_added',
 					   -label => 'date sequence added',
-					   -checked => $v->{a_added}),
+					   -checked => $v->{a_added}||$defaults{a_added}),
 		  '<br>',
 		  $p->checkbox(-name => 'a_locus',
 					   -label => 'Human genomic locus',
-					   -checked => $v->{a_locus}),
+					   -checked => $v->{a_locus}||$defaults{a_locus}),
 		  '<br>',
 		  $p->checkbox(-name => 'a_best_annotation',
 					   -label => '"best" annotation',
-					   -checked => $v->{a_best_annotation} ),
+					   -checked => $v->{a_best_annotation}||$defaults{a_best_annotation} ),
 		  '<br>',
 		  $p->checkbox(-name => 'a_probes',
-					   -label => 'microarray probs',
-					   -checked => $v->{a_probes}),
+					   -label => 'microarray probes',
+					   -checked => $v->{a_probes}||$defaults{a_probes}),
 		  '</td>',
 		  '<td style="vertical-align: top;">',
 		  'Genomic Representative:<br>',
@@ -229,7 +252,7 @@ sub build_form {
 									   'yes' => 'just the name, preserve results order',
 									   'order' => 'order (group) by representative',
 									   'distinct' => 'order hits by distinct representative' },
-						  -default => 'no',
+						  -default => $v->{a_locus_rep}||$defaults{a_locus_rep},
 						  -linebreak => 'true'
 						  ),
 		  '</td>',
@@ -237,11 +260,11 @@ sub build_form {
 		  'Genentech Annotations:<br>',
 		  $p->checkbox(-name => 'a_unq',
 					   -label => 'UNQ',
-					   -checked => $v->{a_unq}),
+					   -checked => $v->{a_unq}||$defaults{a_unq}),
 		  '<br>',
 		  $p->checkbox(-name => 'a_pro',
 					   -label => 'PRO',
-					   -checked => $v->{a_pro}),
+					   -checked => $v->{a_pro}||$defaults{a_pro}),
 		  '<br>',
 		  '</td>',
 		  '</tr>',
