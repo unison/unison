40e5de69a4a2bc1d964b77de1ddf3bd1f4027a90
Reece Hart <reecehart@gmail.com>
2007-03-13 00:28:45 +0000

    renamed unison-aux to unison_aux

--- build/loading/aux/cytoband/sql/cytoband-load.sql
+++ build/loading/aux/cytoband/sql/cytoband-load.sql
@@ -1,7 +1,7 @@
 BEGIN;
-TRUNCATE "unison-aux".cytoband_hg18;
-\copy "unison-aux".cytoband_hg18 FROM PSTDIN WITH NULL AS ''
+TRUNCATE "unison_aux".cytoband_hg18;
+\copy "unison_aux".cytoband_hg18 FROM PSTDIN WITH NULL AS ''
 COMMIT;
-ANALYZE "unison-aux".cytoband_hg18;
+ANALYZE "unison_aux".cytoband_hg18;
 
-SELECT COUNT(*) FROM "unison-aux".cytoband_hg18;
+SELECT COUNT(*) FROM "unison_aux".cytoband_hg18;
