d2f5b79764845dea3afdec46eedfbec54d7b8c69
Reece Hart <reecehart@gmail.com>
2007-04-17 19:58:28 +0000

    *** empty log message ***

--- perl5/Unison/Schema/Schema.pm
+++ perl5/Unison/Schema/Schema.pm
@@ -1,5 +1,11 @@
 package Unison;
 
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+
 sub get_table_info($) {
   # get_table_info -- aggregate FK-PK relationships by FK table
   # The primary use of this is to represent the PK dependencies for each
@@ -13,21 +19,27 @@ sub get_table_info($) {
 							FROM pgutils.foreign_keys
 				            ) );
   $sth->execute();
+
   my $nfks;
   while( my $row = $sth->fetchrow_hashref() ) {
 	$nfks++;
 
-	my $pkst = "$row->{pk_namespace}.$row->{pk_relation}";
-	$tables{$pkst} = Unison::Schema::Table->new( 'st' => $pkst ) unless exists $tables{$pkst};
-
 	my $fkst = "$row->{fk_namespace}.$row->{fk_relation}";
 	$tables{$fkst} = Unison::Schema::Table->new( 'st' => $fkst ) unless exists $tables{$fkst};
 
-	my %fk_data = ( (map { $_ => $row->{$_} } qw(fk_column pk_namespace pk_relation pk_column ud)), 
-					pkt => $tables{pkst} );
-	$tables{$fkst}->add_fk( \%fk_data );
+	my $pkst = "$row->{pk_namespace}.$row->{pk_relation}";
+	$tables{$pkst} = Unison::Schema::Table->new( 'st' => $pkst ) unless exists $tables{$pkst};
+
+	$tables{$fkst}->add_fk( {
+							 %$row,
+							 pkst => $pkst,
+							 pkt => $tables{$pkst}
+							} );
   }
   $sth->finish();
 
   return %tables;
 }
+
+
+1;
