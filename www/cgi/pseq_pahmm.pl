98640d6e97ad6a88d1fcb1003192d9fc47983d56
Kiran Mukhyala <mukhyala@gmail.com>
2007-09-05 22:39:30 +0000

    unison remodel changes

--- www/cgi/pseq_pahmm.pl
+++ www/cgi/pseq_pahmm.pl
@@ -7,6 +7,7 @@ use FindBin;
 use lib "$FindBin::Bin/../perl5", "$FindBin::Bin/../perl5-prereq", "$FindBin::Bin/../../perl5";
 
 use Unison::Exceptions;
+use Unison::pmodelset;
 use Unison::WWW;
 use Unison::WWW::Page qw(infer_pseq_id);
 use Unison::WWW::Table;
@@ -19,16 +20,25 @@ my $u = $p->{unison};
 my $v = $p->Vars();
 
 $p->ensure_required_params(qw(pseq_id));
-$p->add_footer_lines('$Id: pseq_pahmm.pl,v 1.20 2006/05/12 04:40:07 rkh Exp $ ');
+$p->add_footer_lines('$Id: pseq_pahmm.pl,v 1.21 2007/01/09 17:43:02 mukhyala Exp $ ');
 
 
 try {
   my @ps = $u->get_params_info_by_pftype('hmm');
   my %ps = map { $_->[0] => "$_->[1] (params $_->[0])" } @ps;
 
+  my @ms = $u->get_pmodelsets_hmm();
+  my %ms = map { $_->[0] => "$_->[1] (params $_->[0])" } @ms;
+
   if (not defined $v->{params_id}) {
-	$v->{params_id} = 
+	$v->{params_id} =
 	  $u->preferred_params_id_by_pftype('HMM')
+		|| $ps[0]->[0];
+  }
+
+  if (not defined $v->{pmodelset_id}) {
+	$v->{pmodelset_id} =
+	  $u->preferred_pmodelset_id_by_pftype('HMM')
 		|| $ps[0]->[0];
   }
 
@@ -36,12 +46,13 @@ try {
 
   my $js = <<EOJS;
 <script type="text/javascript" language="javascript">
-function update_emb_hmm_alignment(pseq_id, params_id, acc) {
+function update_emb_hmm_alignment(pseq_id, params_id, pmodelset_id, acc) {
 var emb_elem = document.getElementById('emb_hmm_alignment');
 if (emb_elem) {
   var emb_url = 'emb_hmm_alignment.pl?';
   emb_url += 'pseq_id='+pseq_id;
   emb_url += ';params_id='+params_id;
+  emb_url += ';pmodelset_id='+pmodelset_id;
   emb_url += ';profiles='+acc;
   emb_elem.setAttribute('src', emb_url);
   emb_elem.style.display = 'block';
@@ -61,6 +72,12 @@ EOJS
 													   -values => [map {$_->[0]} @ps],
 													   -labels => \%ps,
 													   -default => "$v->{params_id}"),
+		                        '&nbsp;',
+		                        'modelsets: ', $p->popup_menu(-name => 'pmodelset_id',
+													   -values => [map {$_->[0]} @ms],
+													   -labels => \%ms,
+													   -default => "$v->{pmodelset_id}"),
+		                        '&nbsp;',
 					$p->submit(-value=>'redisplay'),
 					$p->end_form(), "\n",
 
@@ -68,6 +85,7 @@ EOJS
 					$p->start_form(-action=>'hmm_alignment.pl', -method=>"GET"),
 					$p->hidden('pseq_id',$v->{pseq_id}),
 					$p->hidden('params_id',$v->{params_id}),
+					$p->hidden('pmodelset_id',$v->{pmodelset_id}),
 
 					$p->group("HMM alignments",
 							  Unison::WWW::Table::render($cref,$rref)),
@@ -97,8 +115,9 @@ sub _fetch($) {
 
   my $sql = <<EOSQL;
   SELECT start,stop,mstart,mstop,ends,score,eval,name,acc,descr
-    FROM pahmm_v
-   WHERE pseq_id=$v->{pseq_id} and params_id=$v->{params_id}
+    FROM pahmm_v p
+    JOIN pmsm_pmhmm m on p.pmodel_id=m.pmodel_id
+   WHERE pseq_id=$v->{pseq_id} and params_id=$v->{params_id} and m.pmodelset_id=$v->{pmodelset_id}
 ORDER BY eval
 EOSQL
   my $sth = $u->prepare($sql);
@@ -117,8 +136,8 @@ EOSQL
 
 	if ( not $p->is_public_instance() ) {
 	  my $ckbox = "<input type=\"checkbox\" name=\"profiles\" value=\"$r->{name}\">";
-	  my $aln = sprintf('<a href="javascript:update_emb_hmm_alignment(%d,%d,\'%s\')">show</a>', 
-						$v->{pseq_id}, $v->{params_id}, $r->{name});
+	  my $aln = sprintf('<a href="javascript:update_emb_hmm_alignment(%d,%d,%d,\'%s\')">show</a>', 
+						$v->{pseq_id}, $v->{params_id}, $v->{pmodelset_id}, $r->{name});
 	  push( @row, "$ckbox &nbsp; &nbsp; $aln" );
 	}
 
