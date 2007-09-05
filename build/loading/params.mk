f52ff3ce8086091dc737bab6cb367022bfea3101
mukhyala <mukhyala@localhost>
2007-09-05 22:09:48 +0000

    unison remodel changes

--- build/loading/params.mk
+++ build/loading/params.mk
@@ -1,5 +1,5 @@
 ## params.mk -- Unison loading configuration and options
-## $Id: params.mk,v 1.25 2007/02/01 22:00:01 mukhyala Exp $
+## $Id: params.mk,v 1.26 2007/04/30 05:05:53 rkh Exp $
 
 ## XXX: We should get more params_id from pftype(preferred_params_id).
 
@@ -31,88 +31,11 @@ COSMIC_DIR:=${RESEARCH_PREFIX}/data/public/mirrors/ftp.sanger.ac.uk/pub/CGP/cosm
 #### PAPROSPECT
 PROSPECT_PATH:=${RESEARCH_EPREFIX}/opt/prospect
 export PROSPECT_PATH
-PAPROSPECT_PARAMS_ID:=50
-PAPROSPECT_PMODELSET_ID:=20
-
 
 ############################################################################
 #### PAHMM
-PFAM_DIR=${RESEARCH_PREFIX}/data/public/pfam-21.0
-#OBSOLETE: PAHMM_PARAMS_ID:=43
-#OBSOLETE: PAHMM_ORIGIN_ID:=$(shell ${PSQL_DCMD} 'select model_origin_id from params where params_id=${PAHMM_PARAMS_ID}')
-
-
-############################################################################
-#### PAPSEQ
-PAPSEQ_PARAMS_ID:=3
-PAPSEQ_CMDLINE=$(shell ${PSQL_DCMD} 'select commandline from params where params_id=${PAPSEQ_PARAMS_ID}')
-
-
-############################################################################
-#### PFANTIGENIC
-PFANTIGENIC_PARAMS_ID:=4
-
-
-############################################################################
-#### PFBIGPI
-PFBIGPI_PARAMS_ID:=19
-
-
-############################################################################
-#### PFPEPCOIL
-PFPEPCOIL_PARAMS_ID:=37
-
-
-############################################################################
-#### PFREGEXP
-PFREGEXP_PARAMS_ID:=12
-PMREGEXP_PMODELSET_ID:=11
-
-############################################################################
-#### PFPSIPRED
-PFPSIPRED_PARAMS_ID=17
-
-
-############################################################################
-#### PFTMDETECT
-PFTMDETECT_PARAMS_ID:=9
-
-
-############################################################################
-#### PFTMHMM
-PFTMHMM_PARAMS_ID:=29
-
-
-############################################################################
-#### PFSIGNALP
-PFSIGNALP_PARAMS_ID:=28
-PFSIGNALP_SEQS_PER_RUN:=100
-
+PFAM_DIR=${RESEARCH_PREFIX}/data/public/pfam
 
 ############################################################################
 #### PMAP
-PMAP_PARAMS_ID:=48
 PMAP_GENASM_ID:=3
-
-
-############################################################################
-#### PSDISPROT
-DISPROT_PARAMS_ID:=39
-
-
-############################################################################
-#### PSPROTCOMP
-PSPROTCOMP_PARAMS_ID:=20
-PSPROTCOMP_SEQS_PER_RUN:=25
-
-############################################################################
-#### PROSITE
-PROSITE_ORIGIN:='Prosite 19.28'
-
-############################################################################
-#### SEG
-PFSEG_PARAMS_ID:=47
-
-############################################################################
-#### BLAST
-BLAST_PARAMS_ID:=3
