97d4984d7677ad30ef7cfe12bb6af878ea6e3522
mukhyala <mukhyala@localhost>
2008-03-18 00:33:05 +0000

    rename porigin to origin

--- build/loading/aux/ncbi/sql/homologene-load.sql
+++ build/loading/aux/ncbi/sql/homologene-load.sql
@@ -1,9 +1,9 @@
 BEGIN;
 TRUNCATE ncbi.homologene;
 \copy ncbi.homologene FROM PSTDIN
-UPDATE porigin SET last_updated = NOW() WHERE origin='HomoloGene';
+UPDATE origin SET last_updated = NOW() WHERE origin='HomoloGene';
 COMMIT;
 ANALYZE ncbi.homologene;
 
 SELECT COUNT(*) FROM ncbi.homologene;
-SELECT origin,last_updated FROM porigin WHERE origin='HomoloGene';
+SELECT origin,last_updated FROM origin WHERE origin='HomoloGene';
