c0cdc9a26122d0b2e984cd746f4615f28c7e4bf6
reece <reece@localhost>
2008-06-18 03:37:28 +0000

    moved all app info to PageInfo; NavBar translates to old menu format for now; all works

--- perl5/Unison/WWW/NavBar.pm
+++ perl5/Unison/WWW/NavBar.pm
@@ -24,7 +24,7 @@ use strict;
 use warnings;
 
 use Unison::WWW::PageInfo;
-
+use Data::Dumper;
 
 =pod
 
@@ -35,296 +35,170 @@ use Unison::WWW::PageInfo;
 =cut
 
 
-######################################################################
-## render
-
-=pod
-
-=item B<< $p->render( C<linkrefs>, C<index> ) >>
-
-renders a NavBar, which is provided as an array reference to array
-references, i.e., [ [name1,url1], [name2,url2], ... ].  C<index> is the
-integer index of the link which should be highlighted.
-
-=cut
-
-#sub render_navbar2 {
-#    ###
-#    my @lr      = shift;                            # link ref [[name,url],...]
-#    my $hilight = shift;                            # which to highlight
-#    my $rv      = "<table class=\"navbar\"><tr>";
-#    for ( my $i = 0 ; $i <= $#$lr ; $i++ ) {
-#        my $c = $rv .= sprintf(
-#            '<td%s><a href="%s">%s</a></td>',
-#            ( $i == $hilight - 1 ) ? ' class="hi"' : '',
-#            @{ $lr->[$i] }[ 1, 0 ]
-#        );
-#    }
-#    $rv .= '</tr></table>';
-#    return $rv;
-#}
+############################################################################
+## This code is among the ugliest, most fragile, and least maintainable
+## bits of flotsam I've ever created.  It should be jettisoned, except
+## that there's so little glamor in rewriting something that works.  Long
+## live inertia!
+##
+## Progress!  I've moved a lot of the code from Page.pm to NavBar.pm and
+## PageInfo.pm.  PageInfo.pm is the sole source of page info.  The code
+## below does contortions to translate PageInfo data into the array format 
+## used previously.  The overhaul is still in progress.
+############################################################################
 
 
-######################################################################
-## NAVBAR CODE
-## This navbar code is among the ugliest, most fragile, and least
-## maintainable bits of flotsam I've ever created.  It should be
-## jettisoned, except that there's so little glamor in rewriting something
-## that works.  Long live inertia!
-
-sub _nav_dump {
-    eval 'use Data::Dumper;  $Data::Dumper::Indent = 0;';
-    my $n = shift;
-    my $d = Dumper( \@_ );
-    $d =~ s/\],/],\n/g;
-    print( STDERR "$n: ", $#_ + 1, " items:\n", $d, "\n" );
-}
 
 sub render_navbar {
-    my $p 	    = shift;
-    my $v       = $p->Vars() || {};
-    my $pseq_id = exists $v->{pseq_id} ? "pseq_id=$v->{pseq_id}" : '';
-    my @navs =
-      ## format: @navs = ( menu, menu, ... );
-      ## where each menu is
-      ## [
-      ##   [ prd, pub, major_name, tooltip ],
-      ##   [ prd, pub, minor_name, tooltip, script, args ],
-      ##   [ prd, pub, minor_name, tooltip, script, args ],
-      ##   ...
-      ## ]
-      ## prd = production? 1=yes, 0=no (i.e., show ONLY in production)
-      ## pub = public? 1=yes, 0=no (i.e., show ONLY in public version)
-      (
-	   ## Search menu
-	   [
-		[
-		 1, 1, 'Search', 'Text- and Feature-based mining',
-		 'search_top.pl'
-		],
-		[
-		 1, 1, 'By Alias',
-		 'search for sequences by alias/name/accession',
-		 'search_alias.pl'
-		],
-		[
-		 1, 1,
-		 'By Properties',
-		 'mine for sequences based on properties',
-		 'search_properties.pl'
-		],
-	   ],
+  my $p = shift;
+  my $v = $p->Vars() || {};
+  my $pseq_id = exists $v->{pseq_id} ? "pseq_id=$v->{pseq_id}" : '';
+
+  my @navs =  __build_navs();
+  @navs = __format_tab_labels( @navs );
+  @navs =
+	__filter_navs( $p->is_prd_instance(), $p->is_public_instance(), @navs );
+  my ( $navi, $subnavi ) = _find_nav_ids($p,@navs);
+  $navi = -1 unless defined $navi;
+
+  my @nav = @{ $navs[$navi] };
+  shift @nav;			  # menu header is first item; subnav items remain
+
+  return(
+		 "<div class=\"nav\">\n"
+		 . "  <table class=\"navp\">\n"
+		 . _make_navrow( $navi, map { $_->[0] } @navs )
+		 . "  </table>\n"
+		 . "  <table class=\"navc\">\n" 
+		 . _make_navrow( $subnavi, @nav )
+		 . "  </table>\n"
+		 . "</div>\n"
+		)
+}
 
 
-	   ## Browse menu
-	   [
-		[
-		 1, 1, 'Browse',
-		 'browse curated queries and precomputed sequences sets',
-		 'browse_top.pl'
-		],
-		[
-		 1, 1, 'Views', 'browse dynamic queries of protein sequences',
-		 'browse_views.pl'
-		],
-		[
-		 1, 1, 'Sets', 'browse precomputed sets of proteins',
-		 'browse_sets.pl'
-		],
-	   ],
 
+######################################################################
+## INTERNAL FUNCTIONS
 
-	   ## Analyze menu
-	   [
-		[
-		 1, 1, 'Analyze',
-		 'display precomputed analyses for a single protein sequence',
-		 'pseq_top.pl'
-		],
-		[
-		 1, 1, 'Summary', 'summary of sequence information',
-		 'pseq_summary.pl', $pseq_id
-		],
-		[
-		 1, 1, 'Aliases', 'all aliases of this sequence',
-		 'pseq_paliases.pl', $pseq_id
-		],
-		[
-		 1, 0, 'Patents', 'patents on this sequence',
-		 'pseq_patents.pl', $pseq_id
-		],
-		[
-		 1, 1, 'Homologs', 'orthologs and paralogs of this sequence',
-		 'pseq_homologs.pl', $pseq_id
-		],
-		[
-		 1, 1, 'Functions', 'Protein Functions and References ',
-		 'pseq_functions.pl', $pseq_id
-		],
-		[
-		 1, 1, 'Features', 'sequences features',
-		 'pseq_features.pl', $pseq_id
-		],
-		[
-		 1, 1, 'Structure', 'structural features',
-		 'pseq_structure.pl', $pseq_id
-		],
-		[
-		 0, 1, 'BLAST', 'BLAST-related sequences',
-		 'pseq_blast.pl', $pseq_id
-		],
-		[
-		 0, 0, 'Prospect', 'Prospect threadings',
-		 'pseq_paprospect.pl', $pseq_id
-		],
-		[
-		 1, 1, 'HMM', 'Hidden Markov Model alignments',
-		 'pseq_pahmm.pl', $pseq_id
-		],
-		[ 0, 1, 'PSSM', 'PSSM alignments', 'pseq_papssm.pl', $pseq_id ],
-		[
-		 0, 1, 'Interactions', 'Protein-Protein Interactions',
-		 'pseq_intx.pl', $pseq_id
-		],
-		[ 1, 1, 'Loci', 'genomic localization', 'pseq_loci.pl', $pseq_id ],
-		[
-		 0, 0, 'Notes', 'user notes on this sequence',
-		 'pseq_notes.pl', $pseq_id
-		],
-		[ 1, 1, 'History', 'run history', 'pseq_history.pl', $pseq_id ],
-	   ],
+sub _nav_dump {
+  eval 'use Data::Dumper;  $Data::Dumper::Indent = 0;';
+  my $n = shift;
+  my $d = Dumper( \@_ );
+  $d =~ s/\],/],\n/g;
+  print( STDERR "$n: ", $#_ + 1, " items:\n", $d, "\n" );
+}
 
+sub __build_navs {
+  my @page_sets = 
+	(
+	 [
+	  'Search',
+	  'Text- and Feature-based mining',
+	  'search_top.pl',
+	  \@Unison::WWW::PageInfo::search_info
+	 ],
+	 [
+	  'Browse', 
+	  'browse curated queries and precomputed sequences sets',
+	  'browse_top.pl',
+	  \@Unison::WWW::PageInfo::search_info
+	 ],
+	 [
+	  'Analyze',
+	  'display precomputed analyses for a single protein sequence',
+	  'pseq_top.pl',
+	  \@Unison::WWW::PageInfo::analyze_info
+	 ],
+	 [
+	  'Tools',
+	  'Miscellaneous services',
+	  'tools_top.pl',
+	  \@Unison::WWW::PageInfo::tools_info ],
+	);
 
-	   ## Tools menu
-	   [									#
-		[
-		 1, 1, 'Tools', 'Miscellaneous services', 'tools_top.pl'
-		],
-		[
-		 1, 1, 'AliAn', 'Annotation of aliases',
-		 'alias_annotation.pl'
-		],
-		[
-		 1, 0, 'OnTarget',
-		 'compare a set of sequences to a set of models ',
-		 'on_target.pl'
-		],
-		[
-		 1, 0, 'Framework',
-		 'search for sequences matching a set of sequence regions',
-		 'search_framework.pl'
-		],
-		[ 0, 0, 'Scores', 'compare scoring systems',
-		  'compare_scores.pl' ],
-		[
-		 0, 0, 'Methods', 'compare threading methods',
-		 'compare_methods.pl'
-		]	   ],
+  my @navm;
+  foreach my $ps (@page_sets) {
+	my @snavs;
+	my $pub = 0;
+	my $prd = 0;
+	foreach my $t (@{$ps->[3]}) {
+	  $pub++ if $t->{pub};
+	  $prd++ if $t->{prd};
+	  push( @snavs, [
+					 $t->{prd}||0,
+					 $t->{pub}||0,
+					 $t->{name},
+					 $t->{brief},
+					 $t->{script},
+					 $t->{args}
+					] );
+	}
+	push( @navm, [
+				  [ $prd>0||0, $pub>0||0, $ps->[0], $ps->[1], $ps->[2] ],
+				  @snavs
+				 ]
+		);
+  }
 
-
-	   # empty list forces right-justification of subsequent menus
-	   [ [''] ],
-
-	   [									# About menu
-		[
-		 # consider '<img width="10px" height="10px" src="../av/favicon.gif" alt="u">'
-		 1, 1, 'About', ('Click for more information about Unison.'
-						 . sprintf("<br>host=%s; db=%s; user=%s",
-								   $v->{host}, $v->{dbname}, $v->{username})),
-		 'about_unison.pl'
-		],
-		[ 1, 1, 'About Unison', 'Unison overview', 'about_unison.pl' ],
-		[
-		 1, 1, 'Statistics', 'Unison summary statistics',
-		 'about_statistics.pl'
-		],
-		[ 1, 1, 'Origins', 'Unison data sources', 'about_origins.pl' ],
-		[
-		 1, 1, 'Params', 'Unison precomputed data types',
-		 'about_params.pl'
-		],
-		[ 0, 1, 'Env',   'environment info', 'about_env.pl' ],
-		[ 0, 1, 'Prefs', 'user prefs',       'about_prefs.pl' ],
-	   ],
-      );
-
-    @navs = __format_tab_labels(@navs);
-    @navs =
-      __filter_navs( $p->is_prd_instance(), $p->is_public_instance(), @navs );
-    my ( $navi, $subnavi ) = _find_nav_ids($p,@navs);
-    $navi = -1 unless defined $navi;
-
-    my @nav = @{ $navs[$navi] };
-    shift @nav;    # menu header is first item; subnav items remain
-
-    return(
-		"<div class=\"nav\">\n"
-      . "  <table class=\"navp\">\n"
-      . _make_navrow( $navi, map { $_->[0] } @navs )
-	  . "  </table>\n"
-	  . "  <table class=\"navc\">\n" 
-	  . _make_navrow( $subnavi, @nav )
-	  . "  </table>\n"
-	  . "</div>\n"
-	)
+  return @navm;
 }
 
 sub __format_tab_labels(@) {
-    my @navs = @_;
-    for ( my $i = 0 ; $i <= $#navs ; $i++ ) {
-        for ( my $j = 0 ; $j <= $#{ $navs[$i] } ; $j++ ) {
-            my @tooltip_tags = ();
-            if ( not $navs[$i]->[$j]->[1] ) {
-                $navs[$i]->[$j]->[2] = "<i>$navs[$i]->[$j]->[2]</i>"
-                  if defined $navs[$i]->[$j]->[2];
-                push( @tooltip_tags, 'public' );
-            }
-            if ( not $navs[$i]->[$j]->[0] ) {
-                $navs[$i]->[$j]->[2] = "<u>$navs[$i]->[$j]->[2]</u>"
-                  if defined $navs[$i]->[$j]->[2];
-                push( @tooltip_tags, 'production' );
-            }
-            if (@tooltip_tags) {
-                $navs[$i]->[$j]->[3] = '' unless $navs[$i]->[$j]->[3];
-                $navs[$i]->[$j]->[3] .=
-                  ( '<hr>NOTE: This tab contains data that will not appear in '
-                      . join( ' or ', @tooltip_tags )
-                      . ' versions of Unison.' );
-            }
-        }
-    }
-    return @navs;
+  my @navs = @_;
+  for ( my $i = 0 ; $i <= $#navs ; $i++ ) {
+	for ( my $j = 0 ; $j <= $#{ $navs[$i] } ; $j++ ) {
+	  my @tooltip_tags = ();
+	  if ( not $navs[$i]->[$j]->[1] ) {
+		$navs[$i]->[$j]->[2] = "<i>$navs[$i]->[$j]->[2]</i>"
+		  if defined $navs[$i]->[$j]->[2];
+		push( @tooltip_tags, 'public' );
+	  }
+	  if ( not $navs[$i]->[$j]->[0] ) {
+		$navs[$i]->[$j]->[2] = "<u>$navs[$i]->[$j]->[2]</u>"
+		  if defined $navs[$i]->[$j]->[2];
+		push( @tooltip_tags, 'production' );
+	  }
+	  if (@tooltip_tags) {
+		$navs[$i]->[$j]->[3] = '' unless $navs[$i]->[$j]->[3];
+		$navs[$i]->[$j]->[3] .=
+		  ( '<hr>NOTE: This tab contains data that will not appear in '
+			. join( ' or ', @tooltip_tags )
+			. ' versions of Unison.' );
+	  }
+	}
+  }
+  return @navs;
 }
 
 sub __filter_navs($$@) {
-    ## Purpose: remove development tabs from production environments, and remove
-    ## proprietary tabs from public environments.  The result is a modified navbar
-    ## array WITHOUT the prd and pub bits (array elems 0 and 1).
+  ## Purpose: remove development tabs from production environments, and remove
+  ## proprietary tabs from public environments.  The result is a modified navbar
+  ## array WITHOUT the prd and pub bits (array elems 0 and 1).
 
-    my ( $is_prd, $is_pub, @navs ) = @_;
-    for ( my $i = $#navs ; $i >= 0 ; $i-- ) {
-        if ( $navs[$i][0][0] eq '' ) {
+  my ( $is_prd, $is_pub, @navs ) = @_;
+  for ( my $i = $#navs ; $i >= 0 ; $i-- ) {
+	if ( $navs[$i][0][0] eq '' ) {
 
-            # menu break
-            next;
-        }
+	  # menu break
+	  next;
+	}
 
-        if (   ( $is_prd and not $navs[$i][0][0] )
-            or ( $is_pub and not $navs[$i][0][1] ) )
-        {
-            splice( @navs, $i, 1 );    # entire major menu is tossed
-            next;
-        }
+	if (   ( $is_prd and not $navs[$i][0][0] )
+		   or ( $is_pub and not $navs[$i][0][1] ) ) {
+	  splice( @navs, $i, 1 );				# entire major menu is tossed
+	  next;
+	}
 
-        # else...
-        @{ $navs[$i] } =
-          grep { ( ( not $is_prd or $_->[0] ) and ( not $is_pub or $_->[1] ) ) }
-          @{ $navs[$i] };
-        @{ $navs[$i] } = map { [ splice( @$_, 2 ) ] } @{ $navs[$i] };
-    }
+	# else...
+	@{ $navs[$i] } =
+	  grep { ( ( not $is_prd or $_->[0] ) and ( not $is_pub or $_->[1] ) ) }
+		@{ $navs[$i] };
+	@{ $navs[$i] } = map { [ splice( @$_, 2 ) ] } @{ $navs[$i] };
+  }
 
-    #_nav_dump("is_prd=$is_prd; is_pub=$is_pub; returned=",@navs);
-    return @navs;
+  #_nav_dump("is_prd=$is_prd; is_pub=$is_pub; returned=",@navs);
+  return @navs;
 }
 
 sub _find_nav_ids {
@@ -357,45 +231,45 @@ sub _find_nav_ids {
 }
 
 sub _make_navrow {
-    # A navrow is a tr, with 2 tds, each with 1 ul, each of which has >=0
-    # li entities. 
-    # $sel is which is selected, and may be undef
-    # @tu = array ref of [tab_label,tooltip,url,params]
-    my ( $sel, @tu ) = @_;
-    my $nav = "    <tr>\n      <td><ul>\n";
-	my $close_open = "      </ul></td>\n      <td class=\"right\"><ul>\n";
+  # A navrow is a tr, with 2 tds, each with 1 ul, each of which has >=0
+  # li entities. 
+  # $sel is which is selected, and may be undef
+  # @tu = array ref of [tab_label,tooltip,url,params]
+  my ( $sel, @tu ) = @_;
+  my $nav = "    <tr>\n      <td><ul>\n";
+  my $close_open = "      </ul></td>\n      <td class=\"right\"><ul>\n";
 
-    for ( my $i = 0 ; $i <= $#tu ; $i++ ) {
-        my ( $tab_label, $tooltip, $url, $params ) = @{ $tu[$i] };
+  for ( my $i = 0 ; $i <= $#tu ; $i++ ) {
+	my ( $tab_label, $tooltip, $url, $params ) = @{ $tu[$i] };
 		
-        if ( $tab_label eq '' ) {
-            $nav .= $close_open;
-			$close_open = '';
-            next;
-        }
+	if ( $tab_label eq '' ) {
+	  $nav .= $close_open;
+	  $close_open = '';
+	  next;
+	}
 
-        $url .= "?$params" if defined $params;
-        my $cl = '';
-        if ( defined $sel and $sel == $i ) {
-            $cl  = ' class="selected"';
-            $url = undef;
-        }
+	$url .= "?$params" if defined $params;
+	my $cl = '';
+	if ( defined $sel and $sel == $i ) {
+	  $cl  = ' class="selected"';
+	  $url = undef;
+	}
 
-		my $tt = ( (defined $tooltip and $tooltip =~ m/\w/)
-				   ? sprintf('tooltip="%s"',CGI::escapeHTML($tooltip))
-				   : '' );
-		$nav .= 
-			"\t<li$cl>"
-			. (defined $url 
-			   ? "<a $tt href=\"$url\">$tab_label</a>" 
-			   : "<span $tt>$tab_label</span>")
-			. "</li>\n"
-    }
+	my $tt = ( (defined $tooltip and $tooltip =~ m/\w/)
+			   ? sprintf('tooltip="%s"',CGI::escapeHTML($tooltip))
+			   : '' );
+	$nav .= 
+	  "\t<li$cl>"
+		. (defined $url 
+		   ? "<a $tt href=\"$url\">$tab_label</a>" 
+		   : "<span $tt>$tab_label</span>")
+		  . "</li>\n"
+		}
 
-    $nav .= $close_open;
-    $nav .= "      </ul></td>\n    </tr>\n";
+  $nav .= $close_open;
+  $nav .= "      </ul></td>\n    </tr>\n";
 
-    return $nav;
+  return $nav;
 }
 
 
---
+++ perl5/Unison/WWW/PageInfo.pm
@@ -0,0 +1,322 @@
+
+=head1 NAME
+
+Unison::WWW::AppInfo -- Unison application list
+
+S<$Id: Config.pm 1716 2007-11-05 05:27:08Z reece $>
+
+=cut
+
+
+package Unison::WWW::PageInfo;
+
+use strict;
+use warnings;
+
+use base 'Exporter';
+our @EXPORTS = qw();
+our @EXPORTS_OK = qw( render_app_list );
+
+
+sub render_app_list {
+  return( 
+		 qq(<div class="top">\n<dl>\n), 
+		 ( map { format_app_entry($_) } @_ ),
+		 qq(</dl>\n</div>\n)
+		)
+}
+
+sub format_app_entry {
+  my $ae = shift;
+  return <<EOHTML;
+<dt><a href="$ae->{script}">$ae->{name}</a> - $ae->{brief}
+<dd>$ae->{descr}
+EOHTML
+}
+
+
+
+1;
+
+
+# GOAL: (not yet implemented like this)
+# app = { script, name, brief, descr, pub, prd }
+# appset = { name, url, @app_names, pub, prd }
+# warn if @app_names is empty
+# main methods: render_appset, render_nav_bar
+
+
+# STYLE GUIDE:
+# {
+# 	name => # Short name for this app. Use: tabs and links
+#   tab => # tab text (default: name)
+# 	script => # basename for the script (in cgi)
+#    args => # 'pseq_id=$v->{pseq_id}', etc (optional)
+# 	brief => # 3-10 word description of results. Use: tooltips.
+#             # No verbs
+# 	descr => # paragraph description of app
+#    pub => 1 # show in public release (optional; default: no)
+#    prd => 1 # show in production release (optional; default: no)
+# },
+
+
+our @tools_info = 
+  (
+   {
+	name => 'AliAn',
+	script => 'alias_annotation.pl',
+	brief => 'Annotate a set of protein aliases',
+	pub => 1,
+	#prd => 1,
+	descr => q(AliAn takes a set of protein accessions and returns a variety of
+descriptive information about those accessions.  This is intend for people
+with data from mass spectroscopy, pull-downs, or other techniques that
+result in sets of protein accessions.),
+   },
+
+   { 
+	name => 'BabelFish',
+	script => 'babelfish.pl',
+	pub => 1,
+	#prd => 1,
+	brief => 'Translates protein identifiers',
+	descr => qq(
+Unison's Babel fish translates essentially any protein sequence
+accession/identifier into any other.  Source and destination identfiers
+may be from CCDS, Cosmic, Ensembl (most species), FANTOM, Genentech,
+Geneseq, HUGE, IPI, MGC, NCBI gi, NCBI RefSeq, PDB, ROUGE, RPS, STRING,
+Sugen kinases, UniProtKB/Swiss-Prot, UniProtKB/TrEMBL, and md5
+checksums. (This facility is named after <a class="extlink"
+target="_blank" href="http://en.wikipedia.org/wiki/Babel_fish">Douglas
+Adams' Babel fish</a>.),
+)
+   },
+
+   {
+	name => 'OnTarget',
+	script => 'on_target.pl',
+	prd => 1,
+	pub => 1,
+	brief => 'Accuracy assessments for various search methods',
+	descr => qq(Compare accuracy of several search methods on predefined
+	protein sequence sets.),
+   },
+
+   {
+	name => 'Framework Search',
+	script => 'search_framework.pl',
+	#prd => 1,
+	pub => 1,
+	brief => 'Antibody framework search',
+	descr => qq(),
+   },
+
+   {
+	name => 'Compare Scores',
+	script => 'compare_scores.pl',
+	#prd => 1,
+	#pub => 1,
+	brief => 'Compare sequence sets and analysis methods',
+	descr => qq(),
+   },
+
+   {
+	name => 'Compare Methods',
+	script => 'compare_methods.pl',
+	#prd => 1,
+	#pub => 1,
+	brief => 'Compare threading methods',
+	descr => qq(),
+   },
+  );
+
+
+our @search_info = 
+  (
+   {
+	name => 'Alias Search',
+	script => 'search_alias.pl',
+	brief => 'Search for proteins by alias',
+	descr => q(Search Unison by protein accession, identifier, or checksum.),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'Feature Search',
+	script => 'search_features.pl',
+	brief => 'Search for proteins by features',
+	descr => qq(),
+	pub => 1,
+   },
+
+   {
+	name => 'Property Search',
+	script => 'search_properties.pl',
+	brief => '',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+  );
+
+
+our @analyze_info = 
+  (
+   {
+	name => 'Summary',
+	script => 'pseq_summary.pl',
+	prd => 1,
+	pub => 1,
+	brief => 'Protein sequence summary',
+	descr => qq(Displays the most pertient and reliable information for a
+	specific protein sequence.),
+   },
+
+   {
+	name => 'Aliases',
+	script => 'pseq_paliases.pl',
+	prd => 1,
+	pub => 1,
+	brief => 'All aliases for a protein sequence',
+	descr => qq(Shows all aliases (accessions and identifiers) for a protein
+	sequence.),
+   },
+
+   {
+	name => 'Patents',
+	script => 'pseq_patents.pl',
+	brief => 'Patents "near" a given sequence',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'Features',
+	script => 'pseq_features.pl',
+	brief => 'Protein sequence features/domains/motifs',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'BLAST',
+	script => 'pseq_blast.pl',
+	brief => 'Precomputed BLAST results',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'PSSM',
+	script => 'pseq_papssm.pl',
+	brief => 'Precomputed Position-Specific Scoring Matrix alignments',
+	descr => qq(),
+	#prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'HMM',
+	script => 'pseq_pahmm.pl',
+	brief => 'Precomputed Hidden Markov Model alignments',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'Prospect',
+	script => 'pseq_paprospect.pl',
+	brief => 'Prospect Pro sequence threadings',
+	descr => qq(),
+	prd => 1,
+	#pub => 1,
+   },
+
+   {
+	name => 'Structures',
+	script => 'pseq_structure.pl',
+	brief => 'Structures and models',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'Functions',
+	script => 'pseq_functions.pl',
+	brief => 'Gene Ontology and GeneRIF annotations',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'Homologs',
+	script => 'pseq_homologs.pl',
+	brief => 'Homologs from HomoloGene',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'Interactions',
+	script => 'pseq_intx.pl',
+	brief => '',
+	descr => qq(),
+	#prd => 1,
+	#pub => 1,
+   },
+
+   {
+	name => 'Loci',
+	script => 'pseq_loci.pl',
+	brief => 'Protein-to-Genome alignments',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'History',
+	script => 'pseq_history.pl',
+	brief => 'Run history for sequence',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'Notes',
+	script => 'pseq_notes.pl',
+	brief => 'Sequence notes',
+	descr => qq(),
+   },
+
+  );
+
+our @browse_info = 
+  (
+   {
+	name => 'Sets',
+	script => 'browse_sets.pl',
+	brief => 'Precomputed sequence sets',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+
+   {
+	name => 'Views',
+	script => 'browse_views.pl',
+	brief => 'Dynamic sequence searches with predifined queries',
+	descr => qq(),
+	prd => 1,
+	pub => 1,
+   },
+  );
