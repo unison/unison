ffad8414848e7bfb7db64b758936364e1863e9b5
reece <reece@localhost>
2008-07-07 19:54:47 +0000

    "internal" changes for release process

--- build/release/pub-rules.pl
+++ build/release/pub-rules.pl
@@ -4,27 +4,43 @@
 # needs a little help in certain circumstances. This file contains
 # those exceptions.
 
+
+
+%schemas = map { $_ => 1 }
+  qw( pgutils  go gong marray ncbi pdb scop sst tax unison unison_aux );
+
+%ignore_tables = map { $_ => 1 }
+  qw(
+	  unison.p2gblataln
+	  unison.p2gblatalnhsp
+	  unison.p2gblathsp
+	  unison.paprospect
+	  unison.gg_famid_pseq_id_mv
+	  unison.pmprospect_scop_mv
+	  unison.known_bcl2_overlaps_mv
+   );
+
 %custom_table_predicates = (
     'unison.origin' => ['is_public = TRUE'],
     'unison.params' => ['is_public = TRUE'],
-
-    #   'unison.pseq' 	=> [ 'pseq_id < 1000'   ], ## testing
+    #'unison.pseq' 	=> ['pseq_id < 1000'  ], ## testing
 );
 
+
 %custom_copy_queries = (
-
     # The ER diagram looks like:
-    #   origin <-- pannotation <-- pseq_pannotation --> pseq
+    #   origin <-- pannotation <-- pseq_pannotation --> pseq <-- predictions
 
-    # restrict pseq rows to only those that occur in at least one public
-    # origin:
+	# We need to restrict pseq rows to only those that occur in at least
+    # one public origin.  The is public is implied by the pseq_annotation
+    # copy query, which eventually depends on the origin predicate above.
     'unison.pseq' => sub {
         my $qq  = $tables{'unison.pseq'}->pred_query();
-        my $saq = $tables{'unison.pseq_pannotation'}->copy_query();
+        my $qacq = $tables{'unison.pseq_pannotation'}->copy_query();
         $qq  =~ s/\n/ /g;
-        $saq =~ s/\n/ /g;
+        $qacq =~ s/\n/ /g;
         return (  "SELECT Q.* FROM ($qq) Q \n"
-                . "JOIN (SELECT DISTINCT pseq_id FROM ($saq) X) SACQ ON Q.pseq_id = SACQ.pseq_id"
+                . "JOIN (SELECT DISTINCT pseq_id FROM ($qacq) X) QACQ ON Q.pseq_id = QACQ.pseq_id"
         );
     },
 
@@ -33,6 +49,7 @@
     'unison.pftype' => 'SELECT * FROM unison.pftype',
 
     # don't restrict unison.params by anything other than its own predicates
+    # This allows rows in params for non-public params, but results won't appear
+    # in public releases.
     'unison.params' => sub { $tables{'unison.params'}->pred_query() },
 );
-
