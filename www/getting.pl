2ac3c04dba9500170a1247f8e5c7c6939a69f2df
Reece Hart <reecehart@gmail.com>
2008-07-24 02:53:07 +0000

    added AUTHORS file; fixed minor bugs and doc issues

--- www/getting.pl
+++ www/getting.pl
@@ -19,14 +19,20 @@ print $p->render('Getting It', <<EOBODY);
 
 <dt>Source code
 
-<dd>Unison is <a href="http://sourceforge.net/projects/unison-db/">hosted at
-SourceForge</a>.
+<dd>Unison is <a href="http://sourceforge.net/projects/unison-db/">hosted
+at SourceForge</a>.
+
 <p>The Unison source code contains raw the SQL schema, Perl modules,
-scripts, Makefiles for data loading, and a lot more.  Users may <a class="ext_link" target="_blank"
-href="http://unison-db.svn.sourceforge.net/viewvc/unison-db/trunk/">browse
-the subversion trunk</a> or <a class="ext_link" target="_blank"
+scripts, Makefiles for data loading, and a lot more.  Users may <a
+class="ext_link" target="_blank"
 href="http://sourceforge.net/svn/?group_id=140591">get a local copy</a> of
 the source code via subversion.
+
+<p>SourceForge has temporarily disabled web-based browsing of all
+repositories with viewvc.  The subversion repository remains available for
+local copies.  You can <a
+href="http://unison-db.svn.sourceforge.net/svnroot/unison-db/trunk/">browse
+the raw files (without viewvc)</a>.
 
 
 <dt>Public database dumps
