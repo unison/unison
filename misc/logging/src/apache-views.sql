a51cbdc30320abb01edb609a5cc4c19a42eb8590
reece <reece@localhost>
2009-03-13 16:20:29 +0000

    updated logging views to better identify bots

--- misc/logging/src/apache-views.sql
+++ misc/logging/src/apache-views.sql
@@ -9,6 +9,6 @@ SELECT src_bin,A.*
 FROM apache_log A
 JOIN src_bin B on A.src=B.src 
 WHERE req ~ '^GET ' AND req !~ '/(av/|js/|robots|styles/|tmp/)'
-AND src_bin !~ '\.gene\.com|host\d+\.hostmonster\.com|\.search\.msn\.com'
+AND src_bin !~ 'gene\.com|host\d+\.hostmonster\.com|search\.msn\.com|'
 AND agent !~ 'AISearchBot|Baiduspider|bot@bot.com|CCBot/|CazoodleBot/|discobot/|DotBot/|Exabot/|Exabot-Thumbnails|Gaisbot/|Gigabot/|Googlebot|GurujiBot/|LijitSpider/|MJ12bot/|NaverBot/|NaverBot/|OOZBOT/|Plonebot/|Semager/|SnapPreviewBot|Sogou develop spider|Sogou web spider/|Sosoimagespider|Speedy Spider|SurveyBot/|TurnitinBot/|Twiceler|Yahoo! Slurp|Yanga WorldSearch|YebolBot|librabot|msnbot|; obot|psbot/|robotgenius|YodaoBot/|YodaoBot-Image/|YoudaoBot/';
 
