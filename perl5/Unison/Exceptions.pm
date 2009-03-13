7379d0b4f04a452dda2156abef8411bb26dc78ef
Reece Hart <reecehart@gmail.com>
2009-03-13 17:18:00 +0000

    annotation/alias work: 1) disabled query preparation for certain queries because doing so results in suboptimal (seqscan) plans; 2) made schema changes to use text_pattern_ops for text columns that need LIKE/regexp searching (#746999); 3) made changes in params.pm, origin.pm, and pannotation.pm to be aware of (2); 4) obsoleted add_palias (deprecated in 2005)

--- perl5/Unison/Exceptions.pm
+++ perl5/Unison/Exceptions.pm
@@ -55,6 +55,7 @@ from Unison::Exceptions.
 use CBT::debug;
 CBT::debug::identify_file() if ($CBT::debug::trace_uses);
 
+
 # Anyone using Unison::Exceptions will want the try/catch/...
 # syntax; import it for them just as 'use Error qw(:try)' would do
 package Unison::Exceptions;
@@ -62,9 +63,9 @@ use base qw(Exporter);
 use Error qw(:try);
 @EXPORT = @Error::subs::EXPORT_OK;
 
-# Define some standard exceptions
+
+# Define some standard Unison exceptions
 package Unison::Exception;
-use base 'Unison::Exception';
 
 our @EXCEPTIONS = qw(NotImplemented NotConnected ConnectionFailed BadUsage
   RuntimeError DBIError);
@@ -72,6 +73,7 @@ our @EXCEPTIONS = qw(NotImplemented NotConnected ConnectionFailed BadUsage
 foreach my $subtype (@EXCEPTIONS) {
     eval "package Unison::Exception::$subtype; use base 'Unison::Exception';";
 }
+
 
 =pod
 
