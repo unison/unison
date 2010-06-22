2876e32292720cad19853a4a14f0c207a58da5e2
reece <reece@localhost>
2010-06-22 07:24:38 +0000

    collected minor updates; doc/INSTALL* needs unification/clarification/pruning

--- www/license.pl
+++ www/license.pl
@@ -20,30 +20,30 @@ print $p->render('License', <<EOBODY);
 Source code, documentation, configuration, and other primary source files
 are released under the Apache License 2.0.
 
-<pre style="background: #ddd; margin: 10px 50px 10px 50px;">
-   Unison Protein Database &mdash; http://unison-db.org/
+<pre style="background: #ddd; margin: 10px 50px 10px 50px; padding: 5px;">
+Unison Protein Database &mdash; http://unison-db.org/
 
-   Copyright 2009 Reece Hart
-   Copyright 2002-2008 Reece Hart, Genentech, Inc.
+Copyright 2009 Reece Hart
+Copyright 2002-2008 Reece Hart, Genentech, Inc.
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 </pre>
 
 
 Any content not covered by the Apache License above is released under the 
 Creative Commons License 3.0.
 
-<div style="background: #ddd; margin: 10px 50px 10px 50px;">
+<div style="background: #ddd; margin: 10px 50px 10px 50px; padding: 5px;">
    <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img
    alt="Creative Commons License" style="border-width:0"
    src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />This
